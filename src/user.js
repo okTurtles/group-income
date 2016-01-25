63bcd93100fa2eef057d0d7a602cfbd791d21048
SGrondin <github@simongrondin.name>
2016-01-24 23:45:24 -0500

    2 step registration

--- src/user.js
+++ src/user.js
@@ -2,6 +2,7 @@
 
 var Joi = require('joi')
 var bcrypt = require('bcrypt')
+var email = require('./helpers/email')
 var uuid = require('node-uuid')
 var hash = Promise.promisify(bcrypt.hash)
 
@@ -33,29 +34,66 @@ module.exports = function (server, Sequelize, db) {
           name: Joi.string().min(3).max(50).required(),
           email: Joi.string().email().required(),
           password: Joi.string().min(7).max(50).required(),
-          phone: Joi.string().min(7).max(14).required(),
+          phone: Joi.string().min(7).max(14),
           contriGL: Joi.number().integer().required(),
-          contriRL: Joi.number().integer().required()
+          contriRL: Joi.number().integer().required(),
+          payPaypal: Joi.string(),
+          payBitcoin: Joi.string(),
+          payVenmo: Joi.string(),
+          payInstructions: Joi.string()
         }
       }
     },
     method: 'POST',
     path: '/user/',
     handler: function (request, reply) {
-      var savedUser = null
-
       hash(request.payload.password, 8)
       .then(function (hashed) {
+        request.payload.verification = uuid.v4()
         request.payload.password = hashed
         return db.User.create(request.payload)
       })
       .then(function (user) {
+        // TODO: Don't send the link before going to production
+        var link = process.env.DOMAIN + '/user/' + user.dataValues.verification + '/verify'
+        email.send(user.dataValues.email, 'Please verify your Group Income account', 'Click this link: ' + link)
+        reply({user: user.dataValues, link: link})
+      })
+      .catch(function (err) {
+        logger(err)
+        reply(err)
+      })
+    }
+  })
+
+  server.route({
+    config: {
+      validate: {
+        params: {
+          verification: Joi.string().min(36).max(36).required()
+        }
+      }
+    },
+    method: 'POST',
+    path: '/user/{verification}/verify',
+    handler: function (request, reply) {
+      var savedUser = null
+      var savedSession = null
+
+      // TODO: Session.create and User.update need to be wrapped together in a transaction
+      db.User.findOne({where: {verification: request.params.verification}})
+      .then(function (user) {
+        if (user == null) return Promise.reject(new Error('Invalid verification link'))
         savedUser = user.dataValues
-        return db.Session.findOne({where: {logout: null, user: savedUser.id}})
+        return db.Session.create({id: uuid.v4(), user: savedUser.id, logout: null})
       })
       .then(function (session) {
-        request.cookieAuth.set(session.dataValues)
-        reply({user: savedUser, session: session.dataValues})
+        savedSession = session.dataValues
+        return db.User.update({verification: null}, {where: {id: savedUser.id}})
+      })
+      .then(function () {
+        request.cookieAuth.set(savedSession)
+        reply({user: savedUser, session: savedSession})
       })
       .catch(function (err) {
         logger(err)
@@ -66,18 +104,19 @@ module.exports = function (server, Sequelize, db) {
 
   db.User = db.define('User', {
     name: {type: Sequelize.STRING, unique: true, allowNull: false},
-    email: {type: Sequelize.STRING, allowNull: false},
+    email: {type: Sequelize.STRING, unique: true, allowNull: false},
     password: {type: Sequelize.STRING, allowNull: false},
-    phone: {type: Sequelize.STRING, allowNull: false},
+    phone: {type: Sequelize.STRING, allowNull: true},
+    verification: {type: Sequelize.STRING, allowNull: true},
     contriGL: {type: Sequelize.INTEGER, allowNull: false},
-    contriRL: {type: Sequelize.INTEGER, allowNull: false}
+    contriRL: {type: Sequelize.INTEGER, allowNull: false},
+    payPaypal: {type: Sequelize.STRING, allowNull: true},
+    payBitcoin: {type: Sequelize.STRING, allowNull: true},
+    payVenmo: {type: Sequelize.STRING, allowNull: true},
+    payInstructions: {type: Sequelize.STRING, allowNull: true}
+
   }, {
-    freezeTableName: true,
-    hooks: {
-      afterCreate: function (instance) {
-        return db.Session.create({id: uuid.v4(), user: instance.id, logout: null})
-      }
-    }
+    freezeTableName: true
   })
 
   return db.User.sync()
