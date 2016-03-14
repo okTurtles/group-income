edd63c0ee5bd69bd8e830f57025f3dbbf566d8c2
Cayman Nava <caymannava@gmail.com>
2016-03-13 21:47:46 -0700

    add Button and Input components

---
+++ frontend/simple/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <button class="btn">{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GIButton',
+  props: {
+    label: String
+  }
+}
+</script>
+
+<style>
+.btn {
+  background-color: rgba(161, 206, 41, 1);
+  color: rgba(255,255,255,1);
+  border-radius: .5rem;
+  font-size: 1.8rem;
+  font-weight: 700;
+  border: none;
+  padding: 1.8rem;
+}
+</style>
---
+++ frontend/simple/components/Input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <input class="text-input"
+           name="{{ name }}"
+           placeholder="{{ placeholder }}"
+           v-bind:class="{'with-prefix-label': prefix }">
+    <label for="{{ name }}">{{ label }} 
+      <span v-if="prefix"
+            class="prefix-label">
+        {{prefix}}
+      </span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GIInput',
+  props: {
+    name: String,
+    label: String,
+    placeholder: String,
+    prefix: String
+  }
+}
+</script>
+
+<style>
+label {
+  display: block;
+  position: relative;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  margin-top: .5rem;
+  margin-bottom: 1.5rem;
+  font-weight: bold;
+}
+.prefix-label {
+  position: absolute;
+  left: 0;
+  top: -3.4rem;
+  font-size: 1.8rem;
+}
+.text-input {
+  width: 27rem;
+  border: none;
+  padding: 1.5rem 1.5rem .5rem 0;
+  border-bottom: .2rem dashed rgba(204, 204, 204, 1);
+  font-size: 1.8rem;
+}
+.text-input.with-prefix-label {
+  padding: 1.5rem 1.5rem .5rem 1.5rem;
+}
+input:focus {
+  border-bottom: 2px solid rgba(161, 206, 41, 1);
+  outline: none;
+}
+</style>
