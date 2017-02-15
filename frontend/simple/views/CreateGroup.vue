f99a318b476038b9e4bf6e2d0eedb20bd6a71c23
Vijayee Kulkaa <vijayee.kulkaa@hushmail.com>
2017-02-15 18:13:24 -0500

    Review Changes: Smaller fonts | Thin Sliders

--- frontend/simple/views/CreateGroup.vue
+++ frontend/simple/views/CreateGroup.vue
@@ -8,20 +8,20 @@
         @submit.prevent="submit">
           <div class="columns is-multiline">
             <div class="column is-one-third center create-group">
-              <p class="title is-1"><i18n>Describe your group</i18n></p>
+              <p class="title is-2"><i18n>Describe your group</i18n></p>
               <div class="box">
-                <p class="title"><i18n>What is your Group's Name?</i18n></p>
+                <p class="title is-3"><i18n>What is your Group's Name?</i18n></p>
                 <input type="text" v-validate data-vv-as="Group Name" data-vv-rules="required" name="groupName" v-model="groupName" class="input">
                 <span v-if="errors.has('groupName')" data-control='groupName' class="help is-danger">{{ errors.first('groupName') }}</span>
               </div>
               <div class="box">
-                <p class="title"><i18n>What are your shared values?</i18n></p>
+                <p class="title is-3"><i18n>What are your shared values?</i18n></p>
                 <textarea class="textarea" v-validate data-vv-as="Shared Values" data-vv-rules="required" name="sharedValues" v-model="sharedValues"></textarea>
                 <span v-if="errors.has('sharedValues')" data-control='sharedValues' class="help is-danger">{{ errors.first('sharedValues') }}</span>
               </div>
               <div class="box">
-                <p class="title"><i18n>What percentage of members are required to change the rules?</i18n></p>
-                <p class="title percentage">{{changePercentage}}%</p>
+                <p class="title is-3"><i18n>What percentage of members are required to change the rules?</i18n></p>
+                <p class="title is-3 percentage">{{changePercentage}}%</p>
                 <input type="range" v-validate data-vv-as="Percentage to change rules" data-vv-rules="between:1,100"  min="0" max="100" name="changePercentage" v-model="changePercentage">
                 <span v-if="errors.has('changePercentage')" data-control='changePercentage' class="help is-danger">The Percentage to change rules field must be between 1% and 100%</span>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
@@ -29,27 +29,27 @@
               </div>
             </div>
             <div class="column is-one-third center create-group">
-              <p class="title is-1"><i18n>Member relationships</i18n></p>
+              <p class="title is-2"><i18n>Member relationships</i18n></p>
               <div class="box">
-                <p class="title"><i18n>Is your group open to new members?</i18n></p>
-                <p class="title" v-show="openMembership">Yes</p>
-                <p class="title" v-show="!openMembership">No</p>
+                <p class="title is-3"><i18n>Is your group open to new members?</i18n></p>
+                <p class="title is-3" v-show="openMembership">Yes</p>
+                <p class="title is-3" v-show="!openMembership">No</p>
                 <label class="switch">
                   <input type="checkbox" name="openMembership" v-model="openMembership">
                   <div class="slider round"></div>
                 </label>
               </div>
               <div class="box">
-                <p class="title"><i18n>How many members should it take to approve a new member?</i18n></p>
-                <p class="title">{{memberApprovalPercentage}}%</p>
+                <p class="title is-3"><i18n>How many members should it take to approve a new member?</i18n></p>
+                <p class="title is-3">{{memberApprovalPercentage}}%</p>
                 <input type="range" min="0" max="100" data-vv-as="Member Approval Percentage" v-validate data-vv-rules="between:1,100" name="memberApprovalPercentage" v-model="memberApprovalPercentage">
                 <span v-if="errors.has('memberApprovalPercentage')" data-control='memberApprovalPercentage' class="help is-danger">The Member Approval Percentage field must be between 1% and 100%.</span>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
                 <input v-if="dev" type="text" v-model="memberApprovalPercentage" name="proxyMemberApprovalPercentage" style="opacity:0; position: absolute;">
               </div>
               <div class="box">
-                <p class="title"><i18n>How many members should it take to remove a member?</i18n></p>
-                <p class="title">{{memberRemovalPercentage}}%</p>
+                <p class="title is-3"><i18n>How many members should it take to remove a member?</i18n></p>
+                <p class="title is-3">{{memberRemovalPercentage}}%</p>
                 <input type="range" min="0" max="100" data-vv-as="Member Removal Percentage" v-validate data-vv-rules="between:1,100" name="memberRemovalPercentage" v-model="memberRemovalPercentage">
                 <span v-if="errors.has('memberRemovalPercentage')" data-control='memberRemovalPercentage' class="help is-danger">The Member Removal Percentage field must be between 1% and 100%.</span>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
@@ -57,9 +57,9 @@
               </div>
             </div>
             <div class="column is-one-third center create-group">
-              <p class="title is-1"><i18n>Resource allocation</i18n></p>
+              <p class="title is-2"><i18n>Resource allocation</i18n></p>
               <div class="box">
-                <p class="title"><i18n>How much income will your group seek to provide</i18n></p>
+                <p class="title is-3"><i18n>How much income will your group seek to provide</i18n></p>
                 <p class="control has-icon">
                   <input type="text" data-vv-as="Income Provided" v-validate data-vv-rules="decimal:2" name="incomeProvided" v-model="incomeProvided" class="input">
                   <span class="icon">
@@ -69,7 +69,7 @@
                 <span v-if="errors.has('incomeProvided')" data-control="incomeProvided" class="help is-danger">The Income Provided field must be a numeric currency amount and may contain 2 decimal points.</span>
               </div>
               <div class="box" >
-                <p class="title"><i18n>How transparent should your group be about who contributes?</i18n></p>
+                <p class="title is-3"><i18n>How transparent should your group be about who contributes?</i18n></p>
                 <p class="select">
                   <select v-validate data-vv-rules="required" data-vv-as="Conrtibution Privacy" name="contributionPrivacy" v-model="contributionPrivacy">
                     <option value="">Select an option</option>
