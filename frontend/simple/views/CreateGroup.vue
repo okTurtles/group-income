5b26d9e0dd29da2c43e13f77610999cf1adb8315
Dana Woodman <dana@danawoodman.com>
2017-04-13 12:36:19 -0700

    Do some cleanup to the create group view.
    - Move a view specific style into component scoped styles
    - Format input JSX so it is more legible
    - Use `const` instead of `let` where possible
    - Use new function syntax for object

--- frontend/simple/views/CreateGroup.vue
+++ frontend/simple/views/CreateGroup.vue
@@ -6,26 +6,57 @@
       <div class="column is-1"></div>
       <div class="column is-10" >
         <form ref="form"
-        name="CreateGroupForm"
-        @submit.prevent="submit">
+          name="CreateGroupForm"
+          @submit.prevent="submit"
+        >
           <div class="columns is-multiline">
             <div class="column is-one-third has-text-centered create-group">
               <p class="title is-4"><i18n>Describe your group</i18n></p>
               <div class="box">
                 <p class="title is-5"><i18n>What is your Group's Name?</i18n></p>
-                <input type="text" v-validate data-vv-as="Group Name" data-vv-rules="required" name="groupName" v-model="groupName" class="input">
-                <i18n v-if="errors.has('groupName')" data-control='groupName' class="help is-danger">{{errors.first('groupName')}}</i18n>
+                <input
+                  type="text"
+                  v-validate
+                  data-vv-as="Group Name"
+                  data-vv-rules="required"
+                  name="groupName"
+                  v-model="groupName"
+                  class="input"
+                >
+                <i18n v-if="errors.has('groupName')" data-control='groupName' class="help is-danger">
+                  {{ errors.first('groupName') }}
+                </i18n>
               </div>
               <div class="box">
                 <p class="title is-5"><i18n>What are your shared values?</i18n></p>
-                <textarea class="textarea" v-validate data-vv-as="Shared Values" data-vv-rules="required" name="sharedValues" v-model="sharedValues"></textarea>
-                <i18n v-if="errors.has('sharedValues')" data-control='sharedValues' class="help is-danger">{{errors.first('sharedValues')}}</i18n>
+                <textarea
+                  class="textarea"
+                  v-validate
+                  data-vv-as="Shared Values"
+                  data-vv-rules="required"
+                  name="sharedValues"
+                  v-model="sharedValues">
+                </textarea>
+                <i18n v-if="errors.has('sharedValues')" data-control='sharedValues' class="help is-danger">
+                  {{ errors.first('sharedValues') }}
+                </i18n>
               </div>
               <div class="box">
                 <p class="title is-5"><i18n>What percentage of members are required to change the rules?</i18n></p>
                 <p class="title is-3 percentage">{{changePercentage}}%</p>
-                <input type="range" v-validate data-vv-as="Percentage to change rules" data-vv-rules="between:1,100"  min="0" max="100" name="changePercentage" v-model="changePercentage">
-                <i18n v-if="errors.has('changePercentage')" data-control='changePercentage' class="help is-danger">The Percentage to change rules field must be between 1% and 100%</i18n>
+                <input
+                  type="range"
+                  v-validate
+                  data-vv-as="Percentage to change rules"
+                  data-vv-rules="between:1,100"
+                  min="0"
+                  max="100"
+                  name="changePercentage"
+                  v-model="changePercentage"
+                >
+                <i18n v-if="errors.has('changePercentage')" data-control='changePercentage' class="help is-danger">
+                  The Percentage to change rules field must be between 1% and 100%
+                </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
                 <input v-if="dev" type="text" v-model="changePercentage" name="proxyChangePercentage" style="opacity:0; position: absolute;">
               </div>
@@ -35,18 +66,52 @@
               <div class="box">
                 <p class="title is-5"><i18n>How many members should it take to approve a new member?</i18n></p>
                 <p class="title is-3">{{memberApprovalPercentage}}%</p>
-                <input type="range" min="0" max="100" data-vv-as="Member Approval Percentage" v-validate data-vv-rules="between:1,100" name="memberApprovalPercentage" v-model="memberApprovalPercentage">
-                <i18n v-if="errors.has('memberApprovalPercentage')" data-control='memberApprovalPercentage' class="help is-danger">The Member Approval Percentage field must be between 1% and 100%.</i18n>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  data-vv-as="Member Approval Percentage"
+                  v-validate
+                  data-vv-rules="between:1,100"
+                  name="memberApprovalPercentage"
+                  v-model="memberApprovalPercentage"
+                >
+                <i18n v-if="errors.has('memberApprovalPercentage')" data-control='memberApprovalPercentage' class="help is-danger">
+                  The Member Approval Percentage field must be between 1% and 100%.
+                </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
-                <input v-if="dev" type="text" v-model="memberApprovalPercentage" name="proxyMemberApprovalPercentage" style="opacity:0; position: absolute;">
+                <input
+                  v-if="dev"
+                  type="text"
+                  v-model="memberApprovalPercentage"
+                  name="proxyMemberApprovalPercentage"
+                  style="opacity:0; position: absolute;"
+                >
               </div>
               <div class="box">
                 <p class="title is-5"><i18n>How many members should it take to remove a member?</i18n></p>
                 <p class="title is-3">{{memberRemovalPercentage}}%</p>
-                <input type="range" min="0" max="100" data-vv-as="Member Removal Percentage" v-validate data-vv-rules="between:1,100" name="memberRemovalPercentage" v-model="memberRemovalPercentage">
-                <i18n v-if="errors.has('memberRemovalPercentage')" data-control='memberRemovalPercentage' class="help is-danger">The Member Removal Percentage field must be between 1% and 100%.</i18n>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  data-vv-as="Member Removal Percentage"
+                  v-validate
+                  data-vv-rules="between:1,100"
+                  name="memberRemovalPercentage"
+                  v-model="memberRemovalPercentage"
+                >
+                <i18n v-if="errors.has('memberRemovalPercentage')" data-control='memberRemovalPercentage' class="help is-danger">
+                  The Member Removal Percentage field must be between 1% and 100%.
+                </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
-                <input v-if="dev" type="text" v-model="memberRemovalPercentage" name="proxyMemberRemovalPercentage" style="opacity:0; position: absolute;">
+                <input
+                  v-if="dev"
+                  type="text"
+                  v-model="memberRemovalPercentage"
+                  name="proxyMemberRemovalPercentage"
+                  style="opacity:0; position: absolute;"
+                >
               </div>
             </div>
             <div class="column is-one-third has-text-centered create-group">
@@ -69,26 +134,49 @@
                       data-vv-as="Income Provided"
                       v-validate data-vv-rules="decimal:2"
                       v-model="incomeProvided"
-                    >
+                    />
                   </p>
                 </div>
-                <i18n v-if="errors.has('incomeProvided')" data-control="incomeProvided" class="help is-danger">The Income Provided field must be a numeric currency amount and may contain 2 decimal points.</i18n>
+                <i18n v-if="errors.has('incomeProvided')" data-control="incomeProvided" class="help is-danger">
+                  The Income Provided field must be a numeric currency amount and may contain 2 decimal points.
+                </i18n>
               </div>
               <div class="box" >
                 <p class="title is-5"><i18n>How transparent should your group be about who contributes?</i18n></p>
                 <p class="select">
-                  <select v-validate data-vv-rules="required" data-vv-as="Contribution Privacy" name="contributionPrivacy" v-model="contributionPrivacy">
+                  <select
+                    v-validate
+                    data-vv-rules="required"
+                    data-vv-as="Contribution Privacy"
+                    name="contributionPrivacy"
+                    v-model="contributionPrivacy"
+                  >
                     <option value="">Select an option</option>
                     <option value="Very Private">Very Private</option>
                   </select>
                 </p>
-                <i18n v-if="errors.has('contributionPrivacy')" data-control="contributionPrivacy" class="help is-danger">{{errors.first('contributionPrivacy')}}</i18n>
+                <i18n v-if="errors.has('contributionPrivacy')" data-control="contributionPrivacy" class="help is-danger">
+                  {{ errors.first('contributionPrivacy') }}
+                </i18n>
               </div>
               <div class="has-text-centered button-box">
                 <div id="errorMsg" v-if="errorMsg" class="help is-danger">{{errorMsg}}</div>
                 <div id="successMsg" v-if="created" class="created"><i18n>Success</i18n></div>
-                <button class="button is-success is-large" v-if="!created" type="submit"><i18n>Create Group</i18n></button>
-                <a class="button is-warning is-large" id="nextButton" v-if="created" v-on:click="next"><i18n>Next: Invite Group Members</i18n></a>
+                <button
+                  class="button is-success is-large"
+                  v-if="!created"
+                  type="submit"
+                >
+                  <i18n>Create Group</i18n>
+                </button>
+                <a
+                  class="button is-warning is-large"
+                  id="nextButton"
+                  v-if="created"
+                  v-on:click="next"
+                >
+                  <i18n>Next: Invite Group Members</i18n>
+                </a>
               </div>
             </div>
           </div>
@@ -98,6 +186,7 @@
     </div>
   </section>
 </template>
+
 <script>
 import Vue from 'vue'
 import backend from '../js/backend'
@@ -106,20 +195,21 @@ import L from '../js/translations'
 export default {
   name: 'CreateGroupView',
   methods: {
-    next: function () {
+    next () {
       this.$router.push({path: '/invite'})
     },
-    submit: async function () {
+    async submit () {
       let success
       try {
         success = await this.$validator.validateAll()
       } catch (ex) {
-        let firsErr = document.querySelector('span.help.is-danger')
-        let control = firsErr.dataset.control
-        let ctrlEl = document.querySelector(`input[name="${control}"], textarea[name="${control}"]`)
+        const firsErr = document.querySelector('span.help.is-danger')
+        const control = firsErr.dataset.control
+        const ctrlEl = document.querySelector(`input[name="${control}"], textarea[name="${control}"]`)
         ctrlEl.focus()
         ctrlEl.scrollIntoView()
       }
+
       if (success) {
         try {
           this.errorMsg = null
@@ -148,7 +238,7 @@ export default {
       }
     }
   },
-  data: function () {
+  data () {
     return {
       groupName: null,
       sharedValues: null,
@@ -165,3 +255,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.button-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20%;
+  margin: 30px 0 0 0;
+}
+</style>
