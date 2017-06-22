37fdecc46f19e19cd1137dfad8a2def8fe46861e
David Ernst <git@dsernst.com>
2017-06-22 10:34:01 -0700

    Add <i18n> to GroupDashboard strings

--- frontend/simple/views/GroupDashboard.vue
+++ frontend/simple/views/GroupDashboard.vue
@@ -20,12 +20,12 @@ export default {
     <p class='is-pulled-right'>
       <a class="button is-primary" @click.prevent="invite(group.id)">
         <span class="icon is-small"><i class='fa fa-plus mr-sm'></i></span>
-        <span>Invite members</span>
+        <span><i18n>Invite members</i18n></span>
       </a>
     </p>
     <h1 class="title is-1 mb-none">{{ group.groupName }}</h1>
-    <p class="mb-lg"><small>SHARED VALUES:</small> {{ group.sharedValues }}</p>
-    <h5 class="subtitle is-4">Members</h5>
+    <p class="mb-lg"><small><i18n>SHARED VALUES:</i18n></small> {{ group.sharedValues }}</p>
+    <h5 class="subtitle is-4"><i18n>Members</i18n></h5>
     <ul>
       <li v-for="member in group.members">{{ member }}</li>
     </ul>
