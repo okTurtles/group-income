4619f42f9ba9478205635eb06be29de138e42469
Dana Woodman <dana@danawoodman.com>
2017-06-21 21:54:57 -0700

    Add styling utility classes.
    Reduce duplication and allow for more rapid UI development by adding styling utilties.
    
    Move Sass variables to their own file.

--- frontend/simple/views/Home.vue
+++ frontend/simple/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="section full-screen">
-      <div id="HomeLogo" class="centered" style="text-align: center">
+      <div id="HomeLogo" class="mx-auto has-text-centered">
         <img style="width: 160px; height: 160px" src="images/group-income-icon-transparent.png">
         <br>
-        <div style="display: block; margin-top: 45px" class="title foco-font"><i18n>Welcome to GroupIncome</i18n></div>
+        <div class="display-block mt-xl title foco-font"><i18n>Welcome to GroupIncome</i18n></div>
         <div v-if="!$store.state.loggedIn"><a v-on:click="forwardToLogin"><i18n>Login</i18n></a> or  <router-link to="signup"><i18n>Sign Up</i18n></router-link> <i18n>to continue</i18n></div>
       </div>
     </section>
