9cf846685330a07c005d746855c15aecd4b0e10b
David Ernst <git@dsernst.com>
2017-06-23 15:39:40 -0700

    Move GroupSettings out into its own component file

---
+++ frontend/simple/components/GroupSettings.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: 'GroupSettings',
+  props: {
+    group: Object
+  }
+}
+</script>
+
+<template>
+  <section>
+    <!-- Group Settings -->
+    <h3 class="title is-3"><i18n>Current Settings</i18n></h3>
+    <div class="columns has-text-centered">
+      <div class="column is-3">
+        <p class="settings-number">{{ group.changePercentage }}%</p>
+        <p class="settings-label">Change Rules</p>
+        <a href="#">Propose change</a>
+      </div>
+      <div class="column is-3">
+        <p class="settings-number">{{ group.memberApprovalPercentage }}%</p>
+        <p class="settings-label">Add Member</p>
+        <a href="#">Propose change</a>
+      </div>
+      <div class="column is-3">
+        <p class="settings-number">{{ group.memberRemovalPercentage }}%</p>
+        <p class="settings-label">Remove Member</p>
+        <a href="#">Propose change</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+  h3.title
+    margin-top: 30px
+
+  .settings-number
+    font-family: HelveticaNeue-Bold
+    font-size: 72px
+    color: #616161
+    line-height: 64px
+
+  .settings-label
+    font-family: HelveticaNeue-Light
+    font-size: 24px
+    color: #616161
+</style>
--- frontend/simple/views/GroupDashboard.vue
+++ frontend/simple/views/GroupDashboard.vue
@@ -1,5 +1,6 @@
 <script>
 import GroupMembers from '../components/GroupMembers.vue'
+import GroupSettings from '../components/GroupSettings.vue'
 
 export default {
   name: 'GroupDashboard',
@@ -9,7 +10,8 @@ export default {
     }
   },
   components: {
-    GroupMembers
+    GroupMembers,
+    GroupSettings
   }
 }
 </script>
@@ -35,25 +37,7 @@ export default {
         <h3 class="title is-3"><i18n>Support History</i18n></h3>
         <p>Your group is still in its first month.</p>
 
-        <!-- Group Settings -->
-        <h3 class="title is-3"><i18n>Current Settings</i18n></h3>
-        <div class="columns has-text-centered">
-          <div class="column is-3">
-            <p class="settings-number">{{ group.changePercentage }}%</p>
-            <p class="settings-label">Change Rules</p>
-            <a href="#">Propose change</a>
-          </div>
-          <div class="column is-3">
-            <p class="settings-number">{{ group.memberApprovalPercentage }}%</p>
-            <p class="settings-label">Add Member</p>
-            <a href="#">Propose change</a>
-          </div>
-          <div class="column is-3">
-            <p class="settings-number">{{ group.memberRemovalPercentage }}%</p>
-            <p class="settings-label">Remove Member</p>
-            <a href="#">Propose change</a>
-          </div>
-        </div>
+        <group-settings :group="group" />
 
       </div>
     </div>
@@ -79,15 +63,4 @@ export default {
     font-size: 48px
     color: #616161
     line-height: 45px
-
-  .settings-number
-    font-family: HelveticaNeue-Bold
-    font-size: 72px
-    color: #616161
-    line-height: 64px
-
-  .settings-label
-    font-family: HelveticaNeue-Light
-    font-size: 24px
-    color: #616161
 </style>
