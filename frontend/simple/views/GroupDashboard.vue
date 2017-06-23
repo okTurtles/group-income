af2242e4adade20472e5a7b4ea798e07d007be30
David Ernst <git@dsernst.com>
2017-06-23 14:51:02 -0700

    /dashboard: Style min-income and add group-settings values

--- frontend/simple/views/GroupDashboard.vue
+++ frontend/simple/views/GroupDashboard.vue
@@ -22,9 +22,9 @@ export default {
         <p>YOUR GROUPS</p>
       </div>
       <div class="column">
-        <div class='is-pulled-right'>
-          <p>Min Income</p>
-          <p>${{ group.incomeProvided}}</p>
+        <div class='is-pulled-right has-text-right'>
+          <p class="min-income-label">Min Income</p>
+          <p class="min-income">${{ group.incomeProvided}}</p>
           <a href="">Propose change</a>
         </div>
         <h1 class="title is-1">{{ group.groupName }}</h1>
@@ -41,11 +41,27 @@ export default {
 
         <!-- Support History -->
         <h3 class="title is-3"><i18n>Support History</i18n></h3>
-        <a href="#">View full history</a>
+        <p>Your group was just started. This will update each month!</p>
 
         <!-- Group Settings -->
         <h3 class="title is-3"><i18n>Current Settings</i18n></h3>
-
+        <div class="columns has-text-centered">
+          <div class="column is-3">
+            <p class="settings-number">{{ group.changePercentage }}%</p>
+            <p class="settings-label">Change Rules</p>
+            <a href="#">Propose change</a>
+          </div>
+          <div class="column is-3">
+            <p class="settings-number">{{ group.memberApprovalPercentage }}%</p>
+            <p class="settings-label">Add Member</p>
+            <a href="#">Propose change</a>
+          </div>
+          <div class="column is-3">
+            <p class="settings-number">{{ group.memberRemovalPercentage }}%</p>
+            <p class="settings-label">Remove Member</p>
+            <a href="#">Propose change</a>
+          </div>
+        </div>
 
       </div>
     </div>
@@ -58,6 +74,20 @@ export default {
     font-size: 52px
     color: #616161
 
+  h3.title
+    margin-top: 30px
+
+  .min-income-label
+    font-family: HelveticaNeue-Light
+    font-size: 16px
+    color: #928E8E
+
+  .min-income
+    font-family: HelveticaNeue-Bold
+    font-size: 48px
+    color: #616161
+    line-height: 45px
+
   .invite-button
     border-radius: 999px
     background-color: #ECECEC
@@ -66,4 +96,15 @@ export default {
     width: 63px
     .icon i
       font-size: 36px
+
+  .settings-number
+    font-family: HelveticaNeue-Bold
+    font-size: 72px
+    color: #616161
+    line-height: 64px
+
+  .settings-label
+    font-family: HelveticaNeue-Light
+    font-size: 24px
+    color: #616161
 </style>
