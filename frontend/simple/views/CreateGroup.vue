25aa0f65cfb21239b9201cc2a4cd8dd0fe5d9f6a
Aniko Fejes <oranjje@gmail.com>
2017-11-07 17:05:43 +0100

    moved creategroup local state under form/ephemeral keys for #297

--- frontend/simple/views/CreateGroup.vue
+++ frontend/simple/views/CreateGroup.vue
@@ -20,8 +20,8 @@
                   v-validate
                   data-vv-as="Group Name"
                   data-vv-rules="required"
-                  name="groupName"
-                  v-model="groupName"
+                  name="form.groupName"
+                  v-model="form.groupName"
                   class="input"
                 >
                 <i18n v-if="errors.has('groupName')" data-control='groupName' class="help is-danger">
@@ -35,8 +35,8 @@
                   v-validate
                   data-vv-as="Shared Values"
                   data-vv-rules="required"
-                  name="sharedValues"
-                  v-model="sharedValues">
+                  name="form.sharedValues"
+                  v-model="form.sharedValues">
                 </textarea>
                 <i18n v-if="errors.has('sharedValues')" data-control='sharedValues' class="help is-danger">
                   {{ errors.first('sharedValues') }}
@@ -44,7 +44,7 @@
               </div>
               <div class="box">
                 <p class="title is-5"><i18n>What percentage of members are required to change the rules?</i18n></p>
-                <p class="title is-3 percentage">{{ changePercentage }}%</p>
+                <p class="title is-3 percentage">{{ form.changePercentage }}%</p>
                 <input
                   type="range"
                   v-validate
@@ -52,21 +52,21 @@
                   data-vv-rules="between:1,100"
                   min="0"
                   max="100"
-                  name="changePercentage"
-                  v-model="changePercentage"
+                  name="form.changePercentage"
+                  v-model="form.changePercentage"
                 >
                 <i18n v-if="errors.has('changePercentage')" data-control='changePercentage' class="help is-danger">
                   The Percentage to change rules field must be between 1% and 100%
                 </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
-                <input v-if="dev" type="text" v-model="changePercentage" name="proxyChangePercentage" style="opacity:0; position: absolute;">
+                <input v-if="ephemeral.dev" type="text" v-model="form.changePercentage" name="proxyChangePercentage" style="opacity:0; position: absolute;">
               </div>
             </div>
             <div class="column is-one-third has-text-centered create-group">
               <p class="title is-4"><i18n>Member relationships</i18n></p>
               <div class="box">
                 <p class="title is-5"><i18n>How many members should it take to approve a new member?</i18n></p>
-                <p class="title is-3">{{ memberApprovalPercentage }}%</p>
+                <p class="title is-3">{{ form.memberApprovalPercentage }}%</p>
                 <input
                   type="range"
                   min="0"
@@ -74,24 +74,24 @@
                   data-vv-as="Member Approval Percentage"
                   v-validate
                   data-vv-rules="between:1,100"
-                  name="memberApprovalPercentage"
-                  v-model="memberApprovalPercentage"
+                  name="form.memberApprovalPercentage"
+                  v-model="form.memberApprovalPercentage"
                 >
                 <i18n v-if="errors.has('memberApprovalPercentage')" data-control='memberApprovalPercentage' class="help is-danger">
                   The Member Approval Percentage field must be between 1% and 100%.
                 </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
                 <input
-                  v-if="dev"
+                  v-if="ephemeral.dev"
                   type="text"
-                  v-model="memberApprovalPercentage"
+                  v-model="form.memberApprovalPercentage"
                   name="proxyMemberApprovalPercentage"
                   style="opacity:0; position: absolute;"
                 >
               </div>
               <div class="box">
                 <p class="title is-5"><i18n>How many members should it take to remove a member?</i18n></p>
-                <p class="title is-3">{{ memberRemovalPercentage }}%</p>
+                <p class="title is-3">{{ form.memberRemovalPercentage }}%</p>
                 <input
                   type="range"
                   min="0"
@@ -99,17 +99,17 @@
                   data-vv-as="Member Removal Percentage"
                   v-validate
                   data-vv-rules="between:1,100"
-                  name="memberRemovalPercentage"
-                  v-model="memberRemovalPercentage"
+                  name="form.memberRemovalPercentage"
+                  v-model="form.memberRemovalPercentage"
                 >
                 <i18n v-if="errors.has('memberRemovalPercentage')" data-control='memberRemovalPercentage' class="help is-danger">
                   The Member Removal Percentage field must be between 1% and 100%.
                 </i18n>
                 <!-- this is used for nightmare and only rendered in 'development' mode. Without this nightmare has trouble triggering the events for vue that updates the model when values are set -->
                 <input
-                  v-if="dev"
+                  v-if="ephemeral.dev"
                   type="text"
-                  v-model="memberRemovalPercentage"
+                  v-model="form.memberRemovalPercentage"
                   name="proxyMemberRemovalPercentage"
                   style="opacity:0; position: absolute;"
                 >
@@ -135,7 +135,7 @@
                       placeholder="Amount"
                       data-vv-as="Income Provided"
                       v-validate data-vv-rules="required|decimal:2"
-                      v-model="incomeProvided"
+                      v-model="form.incomeProvided"
                     />
                   </p>
                 </div>
@@ -151,7 +151,7 @@
                     data-vv-rules="required"
                     data-vv-as="Contribution Privacy"
                     name="contributionPrivacy"
-                    v-model="contributionPrivacy"
+                    v-model="form.contributionPrivacy"
                   >
                     <option value="">Select an option</option>
                     <option value="Very Private">Very Private</option>
@@ -212,13 +212,13 @@ export default {
         this.errorMsg = null
         const entry = new contracts.GroupContract({
           authorizations: [Events.CanModifyAuths.dummyAuth()],
-          groupName: this.groupName,
-          sharedValues: this.sharedValues,
-          changePercentage: this.changePercentage,
-          memberApprovalPercentage: this.memberApprovalPercentage,
-          memberRemovalPercentage: this.memberRemovalPercentage,
-          incomeProvided: this.incomeProvided,
-          contributionPrivacy: this.contributionPrivacy,
+          groupName: this.form.groupName,
+          sharedValues: this.form.sharedValues,
+          changePercentage: this.form.changePercentage,
+          memberApprovalPercentage: this.form.memberApprovalPercentage,
+          memberRemovalPercentage: this.form.memberRemovalPercentage,
+          incomeProvided: this.form.incomeProvided,
+          contributionPrivacy: this.form.contributionPrivacy,
           founderUsername: this.$store.state.loggedIn.name,
           founderIdentityContractId: this.$store.state.loggedIn.identityContractId
         })
@@ -235,23 +235,26 @@ export default {
         await this.$store.dispatch('syncContractWithServer', hash)
       } catch (error) {
         console.error(error)
-        this.errorMsg = L('Failed to Create Group')
+        this.form.errorMsg = L('Failed to Create Group')
       }
     }
   },
   data () {
-    // TODO: wrap this in a 'form' parameter (#297)
     return {
-      groupName: null,
-      sharedValues: null,
-      changePercentage: 80,
-      memberApprovalPercentage: 80,
-      memberRemovalPercentage: 80,
-      incomeProvided: null,
-      contributionPrivacy: 'Very Private',
-      errorMsg: null,
-      // this determines whether or not to render proxy components for nightmare
-      dev: process.env.NODE_ENV === 'development'
+      form: {
+        groupName: null,
+        sharedValues: null,
+        changePercentage: 80,
+        memberApprovalPercentage: 80,
+        memberRemovalPercentage: 80,
+        incomeProvided: null,
+        contributionPrivacy: 'Very Private',
+        errorMsg: null
+      },
+      ephemeral: {
+        // this determines whether or not to render proxy components for nightmare
+        dev: process.env.NODE_ENV === 'development'
+      }
     }
   }
 }
