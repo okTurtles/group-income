2a74df69fef7dc0cc4b7ade4f924caffd35690a8
Aniko Fejes <oranjje@gmail.com>
2017-11-13 23:27:07 +0100

    invite step now checks if user exists

--- frontend/simple/components/CreateGroup/CreateGroupInvitees.vue
+++ frontend/simple/components/CreateGroup/CreateGroupInvitees.vue
@@ -5,20 +5,72 @@
       type="text"
       name="invitee"
       class="input"
-      v-model="currentInvitee"
+      v-model="searchUser"
     >
     <button
       @click.prevent="addInvitee()"
     >
       <i18n>Add</i18n>
     </button>
-    <ul>
-      <li v-for="invitee in invitees">{{ invitee }}</li>
-    </ul>
+
     <p><i18n>Who would you like to include in your group?</i18n></p>
+
+    <p v-if="userErrorMsg" class="help is-danger">{{ userErrorMsg }}</p>
+
+    <table
+            class="table is-bordered is-striped is-narrow"
+            v-if="invitees.length"
+    >
+      <thead>
+      <tr>
+        <th class="table-header"><i18n>Initial Invitees</i18n></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(invitee, index) in invitees" class="member">
+        <td>
+          <div class="media">
+            <div class="media-left">
+              <p class="image is-64x64">
+                <!-- TODO: use responsive figure:
+              http://bulma.io/documentation/elements/image/ -->
+                <!-- TODO: ideally these would be loaded from cache -->
+                <img :src="invitee.state.attributes.picture" width="64" height="64">
+              </p>
+            </div>
+            <div class="media-content">
+              <strong>{{invitee.state.attributes.name}}</strong>
+            </div>
+            <div class="media-right">
+              <button class="delete" @click="remove(index)"></button>
+            </div>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
+<style lang="scss" scoped>
+.table-header {
+  background-color: #fafafa;
+}
+.media {
+  align-items: center;
+}
+.add-form {
+  margin-bottom: 2rem;
+}
+.notification-icon {
+  margin-right: 1rem;
+}
+</style>
 <script>
+import { latestContractState } from '../../js/state'
+import { namespace } from '../../js/backend/hapi'
+import L from '../../js/translations'
+
 export default {
   name: 'CreateGroupInvitees',
   props: {
@@ -27,14 +79,35 @@ export default {
   data: function () {
     return {
       invitees: this.value,
-      currentInvitee: ''
+      searchUser: '',
+      userErrorMsg: ''
     }
   },
   methods: {
-    addInvitee: function () {
-      this.invitees.push(this.currentInvitee)
-      this.currentInvitee = ''
-      this.$emit('input', this.invitees)
+    async addInvitee () {
+      if (!this.searchUser) return
+
+      if (this.searchUser === this.$store.state.loggedIn.name) {
+        this.userErrorMsg = L('Invalid User: Cannot Invite One\'s self')
+        return
+      } else {
+        this.userErrorMsg = ''
+      }
+
+      try {
+        const contractId = await namespace.lookup(this.searchUser)
+        console.log('contractId:', contractId)
+        const state = await latestContractState(contractId)
+        if (!this.invitees.find(invitee => invitee.state.attributes.name === this.searchUser)) {
+          this.invitees.push({ state, contractId })
+        }
+        this.searchUser = null
+        this.userErrorMsg = ''
+        this.$emit('input', this.invitees)
+      } catch (err) {
+        console.log(err)
+        this.userErrorMsg = L('Invalid User')
+      }
     }
   }
 }
