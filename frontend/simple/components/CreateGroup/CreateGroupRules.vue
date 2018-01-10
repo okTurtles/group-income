f25f8319fc1e285baeb47561e7ce49988e82fb26
Aniko Fejes <oranjje@gmail.com>
2018-01-10 13:52:55 +0100

    circle sliders with majority warning

--- frontend/simple/components/CreateGroup/CreateGroupRules.vue
+++ frontend/simple/components/CreateGroup/CreateGroupRules.vue
@@ -4,100 +4,151 @@
     <p class="content"><i18n>What percentage approval is necessary to adjust the group rules?</i18n></p>
     <div class="columns">
       <div class="column">
-        <div class="field" v-if="form.editChange">
-          <div class="control">
-            <input
-              class="input is-large"
-              :class="{ 'is-danger': v.changePercentage.$error }"
-              type="number"
-              min="1"
-              max="100"
-              name="changePercentage"
-              :value="group.changePercentage"
-              @input="update"
-            />
-          </div>
+        <div class="rule">
+          <p class="percent">{{ group.changePercentage }}%</p>
+          <circle-slider
+            class="circle-slider"
+            :value="group.changePercentage"
+            @input="value => update('changePercentage', value)"
+            :side="160"
+            :circleWidth="1"
+            :progressWidth="2"
+            :knobRadius="5"
+            :progressColor="changeColor"
+            :knobColor="changeColor"
+            circleColor="#69707a"
+          >
+          </circle-slider>
+          <p class="title is-6"><i18n>Change Rules</i18n></p>
         </div>
-        <p class="percent" v-else>{{ group.changePercentage }}%</p>
-        <p class="title is-5"><i18n>Change Rules</i18n></p>
-        <button id="changeRulesToggle" class="button is-link is-6 subtitle" @click="toggle('editChange')"><i18n>Change</i18n></button>
       </div>
       <div class="column">
-        <div class="field" v-if="form.editApprove">
-          <div class="control">
-            <input
-              class="input is-large"
-              :class="{ 'is-danger': v.memberApprovalPercentage.$error }"
-              type="number"
-              min="1"
-              max="100"
-              name="memberApprovalPercentage"
-              :value="group.memberApprovalPercentage"
-              @input="update"
-            />
-          </div>
+        <div class="rule">
+          <p class="percent">{{ group.memberApprovalPercentage }}%</p>
+          <circle-slider
+            class="circle-slider"
+            :value="group.memberApprovalPercentage"
+            @input="value => update('memberApprovalPercentage', value)"
+            :side="160"
+            :circleWidth="1"
+            :progressWidth="2"
+            :knobRadius="5"
+            :progressColor="approveColor"
+            :knobColor="approveColor"
+            circleColor="#69707a"
+          >
+          </circle-slider>
+          <p class="title is-6"><i18n>Add Member</i18n></p>
         </div>
-        <p class="percent" v-else>{{ group.memberApprovalPercentage }}%</p>
-        <p class="title is-5"><i18n>Add Member</i18n></p>
-        <button id="approveToggle" class="button is-link is-6 subtitle" @click="toggle('editApprove')"><i18n>Change</i18n></button>
       </div>
       <div class="column">
-        <div class="field" v-if="form.editRemove">
-          <div class="control">
-            <input
-              class="input is-large"
-              :class="{ 'is-danger': v.memberRemovalPercentage.$error }"
-              type="number"
-              min="1"
-              max="100"
-              name="memberRemovalPercentage"
-              :value="group.memberRemovalPercentage"
-              @input="update"
-            />
-          </div>
+        <div class="rule">
+          <p class="percent">{{ group.memberRemovalPercentage }}%</p>
+          <circle-slider
+            class="circle-slider"
+            :value="group.memberRemovalPercentage"
+            @input="value => update('memberRemovalPercentage', value)"
+            :side="160"
+            :circleWidth="1"
+            :progressWidth="2"
+            :knobRadius="5"
+            :progressColor="removeColor"
+            :knobColor="removeColor"
+            circleColor="#69707a"
+          >
+          </circle-slider>
+          <p class="title is-6"><i18n>Remove Member</i18n></p>
         </div>
-        <p class="percent" v-else>{{ group.memberRemovalPercentage }}%</p>
-        <p class="title is-5"><i18n>Remove Member</i18n></p>
-        <button id="removeToggle" class="button is-link is-6 subtitle" @click="toggle('editRemove')"><i18n>Change</i18n></button>
       </div>
     </div>
+    <transition name="open">
+      <div class="message is-warning" v-if="!superMajority">
+        <div class="message-body">
+          The percentage value you are choosing is most likely too low
+          for a decision that can have a potentially significant impact
+          on a person’s life. Please consider using a supermajority threshold.
+          <a href="https://groupincome.org/2016/09/deprecating-mays-theorem/#when-majority-rule-can-harm">
+            Read more on our blog about the dangers of majority rule.
+          </a>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <style>
+  .rule {
+    position: relative;
+    width: 160px;
+    height: 140px;
+    margin: 0 auto;
+    padding-top: 40px;
+  }
   .percent {
     font-size: 3rem;
     font-weight: bold;
     line-height: 3.4rem;
-    margin-bottom: 0.75rem;
+  }
+  .circle-slider {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .message {
+    margin-top: 2rem;
+  }
+  #rulesStep .message-body {
+    border-color: #f68b39;
+  }
+  /* message transition */
+  .open-enter-active, .open-leave-active {
+    transition: max-height .3s ease;
+    max-height: 150px;
+    overflow: hidden;
+  }
+  .open-enter, .open-leave-to {
+    max-height: 0;
   }
 </style>
 <script>
+import { CircleSlider } from 'vue-circle-slider'
+
+const SUPERMAJORITY = 67
+const OKCOLOR = '#78c848'
+const WARNCOLOR = '#f68b39'
+
 export default {
   name: 'CreateGroupRules',
   props: {
     group: {type: Object},
     v: {type: Object}
   },
+  components: {
+    CircleSlider
+  },
   methods: {
-    update (e) {
-      this.v[e.target.name].$touch()
+    update (prop, value) {
+      this.v[prop].$touch()
       this.$emit('input', {
         data: {
-          [e.target.name]: parseInt(e.target.value)
+          [prop]: value
         }
       })
-    },
-    toggle (field) {
-      this.form[field] = !this.form[field]
     }
   },
-  data () {
-    return {
-      form: {
-        editChange: false,
-        editApprove: false,
-        editRemove: false
-      }
+  computed: {
+    changeColor: function () {
+      return this.group.changePercentage >= SUPERMAJORITY ? OKCOLOR : WARNCOLOR
+    },
+    approveColor: function () {
+      return this.group.memberApprovalPercentage >= SUPERMAJORITY ? OKCOLOR : WARNCOLOR
+    },
+    removeColor: function () {
+      return this.group.memberRemovalPercentage >= SUPERMAJORITY ? OKCOLOR : WARNCOLOR
+    },
+    superMajority: function () {
+      return this.group.changePercentage >= SUPERMAJORITY &&
+             this.group.memberApprovalPercentage >= SUPERMAJORITY &&
+             this.group.memberRemovalPercentage >= SUPERMAJORITY
     }
   }
 }
