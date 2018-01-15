510da52fa26f5082d8a64c28b87938648ffac6e9
Aniko Fejes <oranjje@gmail.com>
2018-01-15 16:22:44 +0100

    try-catch instead of confusing promise-catch

--- frontend/simple/main.js
+++ frontend/simple/main.js
@@ -10,16 +10,16 @@ import store from './js/state'
 async function loadLastUser () {
   let user = await db.loadCurrentUser()
   if (user) {
-    let identityContractId = await namespace.lookup(user).catch((err) => {
+    try {
+      let identityContractId = await namespace.lookup(user)
+      await store.dispatch('login', {name: user, identityContractId})
+    } catch (err) {
       console.log('lookup failed!')
       store.dispatch('logout')
       if (err.status === 404) {
         console.warn(`It looks like the local user does not exist anymore on the server 😱 If this is unexpected, contact us at https://gitter.im/okTurtles/group-income`)
         db.clearUser(user)
       }
-    })
-    if (identityContractId) {
-      await store.dispatch('login', {name: user, identityContractId})
     }
   }
   /* eslint-disable no-new */
