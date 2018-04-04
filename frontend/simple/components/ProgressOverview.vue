4856ad60dd828720e2c3b83f4605540d60f5f9ae
sandrina-p <sandrin4pereir4@gmail.com>
2018-04-05 00:49:43 +0100

    feat: Create ProgressOverview.vue placeholder

---
+++ frontend/simple/components/ProgressOverview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="notification is-centered po-container">
+    Progress Overview on the way.
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'ProgressOverview',
+    props: {
+      who: String,
+      what: String,
+      change: String,
+      votesCount: Number
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .po-container {
+    margin: 3rem 0;
+  }
+</style>
