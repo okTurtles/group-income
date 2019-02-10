58ba1995aee692bb555d722c4b583c21ef9c765f
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-02-10 16:14:41 -0200

    Fix lazy loading

--- frontend/lazyLoadedView.js
+++ frontend/lazyLoadedView.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
-import Modal from './views/components/Modal/Modal.vue'
-import LoginModal from './views/containers/LoginModal.vue'
-import SignUp from './views/containers/SignUp.vue'
 
-Vue.component('Modal', Modal)
-Vue.component('LoginModal', LoginModal)
-Vue.component('SignUp', SignUp)
+Vue.component('Modal', () => import('../../containers/Modal.vue'))
+Vue.component('LoginModal', () => import('../../containers/LoginModal.vue'))
+Vue.component('SignUp', () => import('../../containers/SignUp.vue'))
