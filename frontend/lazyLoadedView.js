2da36ad0ba620866a762a535d850950d1837a719
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-02-11 18:55:57 -0200

    Lazy load the missing modal containers

--- frontend/lazyLoadedView.js
+++ frontend/lazyLoadedView.js
@@ -2,3 +2,7 @@ import Vue from 'vue'
 
 Vue.component('LoginModal', () => import('./views/containers/LoginModal.vue'))
 Vue.component('SignUp', () => import('./views/containers/SignUp.vue'))
+Vue.component('Mincome', () => import('./views/containers/proposals-form/Mincome.vue'))
+Vue.component('RuleChangeRule', () => import('./views/containers/proposals-form/RuleChangeRule.vue'))
+Vue.component('RuleAddMember', () => import('./views/containers/proposals-form/RuleAddMember.vue'))
+Vue.component('RuleRemoveMember', () => import('./views/containers/proposals-form/RuleRemoveMember.vue'))
