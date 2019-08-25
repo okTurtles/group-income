2db825c48dbce6bbd8b3d1cba892bae6f7587e40
sandrina-p <sandrin4pereir4@gmail.com>
2019-08-25 13:58:38 +0100

    Make Cypress read the same babelrc as the rest of the project (a.k.a. support FlowJS)

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -11,7 +11,13 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+const browserify = require('@cypress/browserify-preprocessor')
+
+module.exports = (on) => {
+  // Make cypress share the same babelrc config as the rest of the project
+  // https://github.com/cypress-io/cypress-browserify-preprocessor#modifying-default-options
+  const options = browserify.defaultOptions
+  options.browserifyOptions.transform[1][1].babelrc = true
+
+  on('file:preprocessor', browserify(options))
 }
