4d675c27ac0163fe487951f370991f97c248eda3
sandrina-p <sandrin4pereir4@gmail.com>
2019-09-21 11:15:42 +0100

    Solve TODOs at sidebar GroupMembers.vue

--- frontend/views/containers/sidebar/GroupMembers.vue
+++ frontend/views/containers/sidebar/GroupMembers.vue
@@ -12,14 +12,10 @@
       i18n Add
 
   ul.c-group-list
-    // TODO: Check why removing member condition can fail here
-    // TODO: remove v-if in v-for loop and reorder list member by active user limited to 10
     li.c-group-member(
-      v-for='(member, username, index) in groupMembers'
-      v-if='index < 10 && member'
+      v-for='(member, username, index) in firstTenMembers'
       :class='member.pending && "is-pending"'
       :key='username'
-      data-test='member'
     )
       user-image(:username='username')
 
@@ -110,6 +106,14 @@ export default {
     ]),
     currentUserName () {
       return this.$store.state.loggedIn.username
+    },
+    firstTenMembers () {
+      const usernames = Object.keys(this.groupMembers).slice(0, 9)
+
+      return usernames.reduce((acc, cur) => ({
+        ...acc,
+        [cur]: this.groupMembers[cur]
+      }), {})
     }
   }
 }
