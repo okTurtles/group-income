09c03d889184c6e82e6a178df9049dabc9c2615f
SebinSong <songxevin@gmail.com>
2019-10-05 13:57:54 +1300

    restore GroupWelcome.vue

---
+++ frontend/views/pages/GroupWelcome.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+main.c-splash
+  group-welcome
+</template>
+
+<script>
+import GroupWelcome from '@components/GroupWelcome.vue'
+export default {
+  name: 'GroupWelcomePage',
+  components: {
+    GroupWelcome
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.c-splash {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
