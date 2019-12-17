405d1407a7195880a0be395df389d6a26525588a
sandrina-p <sandrin4pereir4@gmail.com>
2019-12-17 17:00:28 +0000

    minor improvement on ourIncomeToReceive

--- frontend/views/containers/GroupPledgesGraph.vue
+++ frontend/views/containers/GroupPledgesGraph.vue
@@ -106,7 +106,7 @@ export default {
       const surplus = Math.max(0, pledgeTotal - othersIncomeNeeded - ourIncomeNeeded)
       let ourIncomeToReceive = ourIncomeNeeded
 
-      if (neededPledges > 0) {
+      if (doWeNeedPledge && neededPledges > 0) {
         incomeDistribution.push({ name: this.ourUsername, amount: ourIncomeAmount })
 
         ourIncomeToReceive = distributeIncome(incomeDistribution, mincome)
