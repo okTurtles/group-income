5bfc61530c7b8afada5a959c45d56a91d6ab5545
Pierre Schweiger <schweiger.pierre@gmail.com>
2020-01-19 20:51:14 +0800

    Fix title internationalisation

--- frontend/views/components/CalloutCard.vue
+++ frontend/views/components/CalloutCard.vue
@@ -2,7 +2,7 @@
   section.c-calloutCard(:class='{ card: isCard }')
     component.c-svg(:is='svg')
     div
-      i18n.is-title-3.c-title(tag='h3' v-if='title') {{ title }}
+      h3.is-title-3.c-title(v-if='title') {{ title }}
       slot
 </template>
 
