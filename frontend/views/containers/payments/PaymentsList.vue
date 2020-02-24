207cb67fd0bba5e9399356298ed50082026c2ac6
Pierre Schweiger <schweiger.pierre@gmail.com>
2020-02-24 12:22:05 +0800

    Refactor part 2

--- frontend/views/containers/payments/PaymentsList.vue
+++ frontend/views/containers/payments/PaymentsList.vue
@@ -18,7 +18,7 @@ table.table.table-in-card.c-payments
         .c-user-month(:class='index === 0 ? "has-text-1" : "c-status-danger c-status"') {{ randomMonth() }}
       td.c-payments-amount
         .c-payments-amount-info(v-if='index === 0 && (!needsIncome && paymentsType === "todo")')
-          i18n.c-partial-info.has-text-1(
+          i18n.c-payments-amount-text.has-text-1(
             tag='span'
             :args='{partial_amount: `<strong class="has-text-0">$20</strong>`, partial_total: payment.amountFormatted}'
           ) {partial_amount} out of {partial_total}
@@ -50,68 +50,24 @@ table.table.table-in-card.c-payments
       td
         .c-actions
           .c-actions-month(:class='!(index !== 0 && paymentsType === "todo") ? "has-text-1" : "c-status-danger c-status"') {{ randomMonth() }}
-          menu-parent
-            menu-trigger.is-icon-small(:aria-label='L("Show payment menu")')
-              i.icon-ellipsis-v
-
-            menu-content.c-actions-content
-              ul
-                menu-item(
-                  v-if='paymentsType === "sent" || needsIncome'
-                  tag='button'
-                  item-id='message'
-                  icon='info'
-                  @click='openModal("PaymentDetail")'
-                )
-                  i18n Payment details
-
-                menu-item(
-                  v-if='paymentsType === "todo"'
-                  tag='button'
-                  item-id='message'
-                  icon='times'
-                  @click='cancelPayment(payment.to, payment.hash)'
-                )
-                  i18n Dismiss this payment
-
-                menu-item(
-                  v-if='paymentsType === "sent"'
-                  tag='button'
-                  item-id='message'
-                  icon='times'
-                  @click='cancelPayment(payment.to, payment.hash)'
-                )
-                  i18n Cancel this payment
-
-                menu-item(
-                  v-if='needsIncome'
-                  tag='button'
-                  item-id='message'
-                  icon='times'
-                  @click='cancelPayment(payment.to, payment.hash)'
-                )
-                  i18n I did not receive this
+          payments-list-menu(
+            :paymentsType='paymentsType'
+            :needsIncome='needsIncome'
+          )
 </template>
 
 <script>
-import sbp from '~/shared/sbp.js'
 import { mapGetters } from 'vuex'
 import AvatarUser from '@components/AvatarUser.vue'
 import Tooltip from '@components/Tooltip.vue'
-import { OPEN_MODAL } from '@utils/events.js'
-import { MenuParent, MenuTrigger, MenuContent, MenuItem } from '@components/menu/index.js'
-import { PAYMENT_CANCELLED } from '@model/contracts/payments/index.js'
-// import L from '@view-utils/translations.js'
+import PaymentsListMenu from '@containers/payments/PaymentsListMenu.vue'
 
 export default {
   name: 'PaymentTable',
   components: {
     AvatarUser,
     Tooltip,
-    MenuParent,
-    MenuTrigger,
-    MenuContent,
-    MenuItem
+    PaymentsListMenu
   },
   props: {
     titles: {
@@ -151,24 +107,6 @@ export default {
     // TEMP
     randomMonth () {
       return this.monthsNames[new Date(+(new Date()) - Math.floor(Math.random() * 10000000000)).getMonth()]
-    },
-    openModal (name) {
-      sbp('okTurtles.events/emit', OPEN_MODAL, name)
-    },
-    // TODO: make multiple payments
-    async cancelPayment (username, paymentHash) {
-      try {
-        const paymentMessage = await sbp('gi.contracts/group/paymentUpdate/create', {
-          paymentHash,
-          updatedProperties: {
-            status: PAYMENT_CANCELLED
-          }
-        }, this.$store.state.currentGroupId)
-        await sbp('backend/publishLogEntry', paymentMessage)
-      } catch (e) {
-        console.error(e)
-        alert(e.message)
-      }
     }
   }
 }
@@ -208,6 +146,10 @@ export default {
   }
 }
 
+.c-actions {
+  justify-content: space-between;
+}
+
 .c-actions,
 .c-user {
   display: flex;
@@ -231,10 +173,6 @@ export default {
   overflow: hidden;
 }
 
-.c-actions {
-  justify-content: space-between;
-}
-
 .c-user-month {
   display: none;
   @include phone {
@@ -247,54 +185,40 @@ export default {
   padding: 0 $spacer-xs;
   text-transform: uppercase;
   font-size: $size_5;
-}
 
-.c-payments-amount-info {
-  display: inline-flex;
-  align-items: center;
-}
+  @each $name in $colors {
+    &-#{$name} {
+      &.c-status,
+      .c-status{
+        background: var(--#{$name}_2);
+        color: var(--#{$name}_0);
+      }
 
-.c-status-primary {
-  &.c-status,
-  .c-status{
-    background: $primary_2;
-    color: $primary_0;
-  }
-
-  .c-tip {
-    background: $primary_0;
+      .c-tip {
+        background: var(--#{$name}_0);
+      }
+    }
   }
 }
 
-.c-status-danger {
-  &.c-status,
-  .c-status{
-    background: $danger_2;
-    color: $danger_0;
+.has-text-1 + .c-status {
+  margin-left: $spacer-sm;
+}
+
+.c-payments-amount {
+  &-info {
+    display: inline-flex;
+    align-items: center;
   }
 
-  .c-tip {
-    background: $danger_0;
+  &-text {
+    @include phone {
+      display: block;
+    }
   }
 }
 
-.c-status-warning {
-  &.c-status,
-  .c-status {
-    background: $warning_2;
-    color: $warning_0;
-  }
-  .c-tip {
-    background: $warning_0;
-  }
-}
-
-.c-content {
-  width: 13.375rem;
-  left: -12rem;
-  top: 2rem;
-}
-
+// Tooltip
 .c-tip {
   width: 0.875rem;
   height: 0.875rem;
@@ -307,15 +231,5 @@ export default {
   &:hover {
     color: #fff;
   }
-}
-
-.c-partial-info {
-  @include phone {
-    display: block;
-  }
-}
-
-.has-text-1 + .c-status {
-  margin-left: $spacer-sm;
 }
 </style>
---
+++ frontend/views/containers/payments/PaymentsListMenu.vue
@@ -0,0 +1,100 @@
+<template lang='pug'>
+menu-parent
+  menu-trigger.is-icon-small(:aria-label='L("Show payment menu")')
+    i.icon-ellipsis-v
+
+  menu-content.c-actions-content
+    ul
+      menu-item(
+        v-if='paymentsType === "sent" || needsIncome'
+        tag='button'
+        item-id='message'
+        icon='info'
+        @click='openModal("PaymentDetail")'
+      )
+        i18n Payment details
+
+      menu-item(
+        v-if='paymentsType === "todo"'
+        tag='button'
+        item-id='message'
+        icon='times'
+        @click='cancelPayment(payment.to, payment.hash)'
+      )
+        i18n Dismiss this payment
+
+      menu-item(
+        v-if='paymentsType === "sent"'
+        tag='button'
+        item-id='message'
+        icon='times'
+        @click='cancelPayment(payment.to, payment.hash)'
+      )
+        i18n Cancel this payment
+
+      menu-item(
+        v-if='needsIncome'
+        tag='button'
+        item-id='message'
+        icon='times'
+        @click='cancelPayment(payment.to, payment.hash)'
+      )
+        i18n I did not receive this
+</template>
+
+<script>
+import sbp from '~/shared/sbp.js'
+import { OPEN_MODAL } from '@utils/events.js'
+import { MenuParent, MenuTrigger, MenuContent, MenuItem } from '@components/menu/index.js'
+import { PAYMENT_CANCELLED } from '@model/contracts/payments/index.js'
+
+export default {
+  name: 'PaymentTable',
+  props: {
+    paymentsType: {
+      type: String,
+      required: true
+    },
+    needsIncome: {
+      type: Boolean,
+      required: true
+    }
+  },
+  components: {
+    MenuParent,
+    MenuTrigger,
+    MenuContent,
+    MenuItem
+  },
+  methods: {
+    openModal (name) {
+      sbp('okTurtles.events/emit', OPEN_MODAL, name)
+    },
+    // TODO: make multiple payments
+    async cancelPayment (username, paymentHash) {
+      try {
+        const paymentMessage = await sbp('gi.contracts/group/paymentUpdate/create', {
+          paymentHash,
+          updatedProperties: {
+            status: PAYMENT_CANCELLED
+          }
+        }, this.$store.state.currentGroupId)
+        await sbp('backend/publishLogEntry', paymentMessage)
+      } catch (e) {
+        console.error(e)
+        alert(e.message)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+
+.c-content {
+  width: 13.375rem;
+  left: -12rem;
+  top: 2rem;
+}
+</style>
