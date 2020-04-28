2aebeba3cba3f660757d9f3464d23b598226e4c8
sandrina-p <sandrin4pereir4@gmail.com>
2020-04-28 10:24:52 +0100

    Hardcoded banner error

--- frontend/views/components/banners/BannerGeneral.vue
+++ frontend/views/components/banners/BannerGeneral.vue
@@ -1,8 +1,12 @@
 <template lang='pug'>
   transition-expand
-    .l-banner(v-if='ephemeral.message' data-test='bannerGeneral')
+    .l-banner(
+      v-if='ephemeral.message'
+      data-test='bannerGeneral'
+      :class='`is-${ephemeral.severity}`'
+    )
       i(:class='`icon-${ephemeral.icon} is-prefix`')
-      | {{ ephemeral.message }}
+      span(v-html='ephemeral.message')
 </template>
 
 <script>
@@ -16,7 +20,8 @@ export default {
   data: () => ({
     ephemeral: {
       message: null,
-      icon: null
+      icon: null,
+      severity: null
     }
   }),
   methods: {
@@ -25,11 +30,37 @@ export default {
     clean () {
       this.ephemeral.message = ''
       this.ephemeral.icon = ''
+      this.ephemeral.severity = ''
     },
     show (message, icon) {
       this.ephemeral.message = message
       this.ephemeral.icon = icon
+      this.ephemeral.severity = 'neutral'
+    },
+    danger (message, icon) {
+      this.ephemeral.message = message
+      this.ephemeral.icon = icon
+      this.ephemeral.severity = 'danger'
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+
+.l-banner ::v-deep .link {
+  color: inherit;
+  border-bottom-color: inherit;
+  font-weight: inherit;
+}
+
+.is-neutral {
+  background-color: $text_0;
+  color: $background;
+}
+
+.is-danger {
+  background-color: $danger_0;
+  color: $background;
+}
+</style>
