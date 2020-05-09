68c836ade096fa9879673c9b57772b17474c662b
Sandrina Pereira <sandrin4pereir4@gmail.com>
2020-05-09 21:31:50 +0100

    #763 Payments - dry payment row (#917)
    * DRY paymentRow* and related data
    
    * Make Progress.vue work and animate correctly. Tested on chrome, firefox and safari
    
    * Self review - delete unused code and fix typos
    
    * Reorganize PaymentRow* files
    
    * optimize humanDate - locale
    
    * move locate out of function - global scope. it will break on travis
    
    * rollback locale to where it was before. see travis build: https://travis-ci.org/github/okTurtles/group-income-simple/builds/684907167
    
    * add locale fallback value
    
    * add locale fallback value

--- frontend/views/containers/payments/PaymentRowRecord.vue
+++ frontend/views/containers/payments/PaymentRowRecord.vue
@@ -1,47 +1,39 @@
 <template lang='pug'>
-tr
-  td
-    label.checkbox
-      input.input(type='checkbox' v-model='payment.checked')
-      span
-  td
-    .c-user
-      avatar-user.c-avatar(:username='payment.username' size='xs')
-      strong.c-name {{ payment.displayName }}
+  // Note: .cpr- is from payment-row
+  payment-row(:payment='payment')
+    template(slot='cellPrefix')
+      label.checkbox.c-check
+        input.input(type='checkbox' v-model='payment.checked')
+        span
+          i18n.sr-only Mark as sent
 
-    // TODO: replace condition to indicate whether or not the payment date is < or > than the current date using payment.paymentStatusText
-    i18n.c-user-month(
-      :class='Math.round(Math.random()) ? "has-text-1" : "pill is-danger"'
-      :args='{date: dueDate(payment.date) }'
-    ) Due {date}
-
-  td
-    .c-actions
-      .c-actions-month.has-text-1 {{ dueDate(payment.date) }}
+    template(slot='cellActions')
+      .cpr-date.pill.is-danger(v-if='Math.round(Math.random())') {{ humanDate(payment.date) }}
       i18n.is-unstyled.is-link-inherit.link.c-reset(
         tag='button'
         type='button'
+        v-if='payment.amount !== config.initialAmount'
         @click='reset'
       ) Reset
 
-  td
-    label.field
-      .inputgroup
-        input.input(inputmode='decimal' pattern='[0-9]*' v-model='payment.amount')
-        .suffix.hide-phone {{symbolWithCode}}
-        .suffix.hide-tablet {{symbol}}
+    template(slot='cellSuffix')
+      label.field
+        .inputgroup
+          input.input(inputmode='decimal' pattern='[0-9]*' v-model='payment.amount')
+          .suffix.hide-phone {{symbolWithCode}}
+          .suffix.hide-tablet {{symbol}}
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import AvatarUser from '@components/AvatarUser.vue'
 import currencies from '@view-utils/currencies.js'
-import { humanDate } from '@view-utils/humanDate.js'
+import { humanDate } from '@utils/time.js'
+import PaymentRow from './payment-row/PaymentRow.vue'
 
 export default {
   name: 'PaymentRowRecord',
   components: {
-    AvatarUser
+    PaymentRow
   },
   props: {
     payment: {
@@ -51,6 +43,9 @@ export default {
   },
   data () {
     return {
+      config: {
+        initialAmount: this.payment.amount
+      },
       form: {
         amount: this.payment.amount
       }
@@ -68,13 +63,9 @@ export default {
     }
   },
   methods: {
-    // TEMP
-    async reset () {
-      console.log('Todo: Implement reset payment')
-    },
-    dueDate (datems) {
-      // TODO: move humanDate and everything associated with it into frontend/utils/time.js !
-      return humanDate(datems, { month: 'short', day: 'numeric' })
+    humanDate,
+    reset () {
+      this.payment.amount = this.config.initialAmount
     }
   }
 }
@@ -83,147 +74,12 @@ export default {
 <style lang="scss" scoped>
 @import "@assets/style/_variables.scss";
 
-.c-payments {
-  margin-top: 1rem;
-
-  th, td {
-    &:first-child {
-      width: 41%;
-    }
-
-    &:last-child {
-      width: 10%;
-      padding-left: 1rem;
-
-      @include tablet {
-        width: 22%;
-      }
-    }
-  }
-
-  .c-header-checkbox {
-    line-height: 23px;
-  }
-
-  &-date {
-    @include phone {
-      display: none;
-    }
-  }
-
-  &-amount {
-    @include phone {
-      text-align: right;
-    }
-  }
-}
-
-.is-editing {
-  th, td {
-    &:first-child {
-      width: 10%;
-      @include phone {
-        width: 3rem;
-      }
-    }
-
-    &:last-child {
-      width: 40%;
-      min-width: 9.375rem;
-      display: table-cell;
-    }
-  }
-
-  .c-actions {
-    justify-content: flex-end;
-  }
-
-  .c-reset {
-    margin-left: 1rem;
-  }
-
-  .c-payments-date {
-    @include phone {
-      display: block;
-    }
-  }
-}
-
-.checkbox {
+.c-check {
   margin-right: 0.5rem;
 }
 
-.c-actions {
-  justify-content: space-between;
-}
-
-.c-actions,
-.c-user {
-  display: flex;
-  align-items: center;
-}
-
-.c-avatar {
-  margin-right: 0.5rem;
-}
-
-.c-avatar,
-.c-actions-month {
-  @include phone {
-    display: none;
-  }
-}
-
-.c-actions-month {
-  margin-left: 0;
-  white-space: nowrap;
-}
-
-.c-actions-menu {
+.c-reset {
   margin-left: 1rem;
-}
-
-.c-name {
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
-
-.c-user-month {
-  display: none;
-  @include phone {
-    display: inline-block;
-    margin-left: 0;
-  }
-}
-
-.c-payments-amount-text {
-  @include tablet {
-    margin-right: 0.5rem;
-  }
-}
-
-.c-payments-amount {
-  &-info {
-    display: inline-flex;
-    align-items: center;
-
-    @include phone {
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-    }
-  }
-
-  &-text {
-    @include phone {
-      display: block;
-    }
-  }
-
-  .is-small {
-    margin: 0;
-  }
 }
 
 .inputgroup .input {
@@ -231,5 +87,4 @@ export default {
     padding-right: 2rem;
   }
 }
-
 </style>
