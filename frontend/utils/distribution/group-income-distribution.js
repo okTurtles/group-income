002ff51f063ce032583e4f00578e9aaeb745f854
Steven Degutis <sbdegutis@gmail.com>
2020-10-16 15:28:26 -0500

    [#763] Extracting non-Vue version of groupIncomeDistribution().

--- frontend/utils/distribution/group-income-distribution.js
+++ frontend/utils/distribution/group-income-distribution.js
@@ -1,15 +1,21 @@
 import { saferFloat } from '~/frontend/views/utils/currencies.js'
 import incomeDistribution from '~/frontend/utils/distribution/mincome-proportional.js'
+import paymentTotalFromUserToUser from '~/frontend/model/contracts/payments/totals.js'
 
-export default function groupIncomeDistribution ({ state, getters, monthstamp, adjusted }) {
+export function groupIncomeDistributionLogic ({
+  monthstamp,
+  adjusted,
+  mincomeAmount,
+  groupProfiles,
+  payments,
+  monthlyPayments
+}) {
   // the monthstamp will always be for the current month. the alternative
   // is to allow the re-generation of the distribution for previous months,
   // but that approach requires also storing the historical mincomeAmount
   // and historical groupProfiles. Since together these change across multiple
   // locations in the code, it involves less 'code smell' to do it this way.
   // see historical/group.js for the ugly way of doing it.
-  const mincomeAmount = getters.groupMincomeAmount
-  const groupProfiles = getters.groupProfiles
   const currentIncomeDistribution = []
   for (const username in groupProfiles) {
     const profile = groupProfiles[username]
@@ -31,7 +37,14 @@ export default function groupIncomeDistribution ({ state, getters, monthstamp, a
     // needs to be paid (if it was a partial payment).
     const carried = Object.create(null)
     for (const p of dist) {
-      const alreadyPaid = getters.paymentTotalFromUserToUser(p.from, p.to, monthstamp)
+      const alreadyPaid = paymentTotalFromUserToUser({
+        fromUser: p.from,
+        toUser: p.to,
+        paymentMonthstamp: monthstamp,
+        payments,
+        monthlyPayments
+      })
+
       const carryAmount = p.amount - alreadyPaid
       // ex: it wants us to pay $2, but we already paid $3, thus: carryAmount = -$1 (all done paying)
       // ex: it wants us to pay $3, but we already paid $2, thus: carryAmount = $1 (remaining to pay)
@@ -52,3 +65,70 @@ export default function groupIncomeDistribution ({ state, getters, monthstamp, a
   }
   return dist
 }
+
+export default function groupIncomeDistribution ({ getters, monthstamp, adjusted }) {
+  return groupIncomeDistributionLogic({
+    monthstamp,
+    adjusted,
+    mincomeAmount: getters.groupMincomeAmount,
+    groupProfiles: Object.fromEntries(Object.entries(getters.groupProfiles).map(([username, profile]) => [username, {
+      incomeDetailsType: profile.incomeDetailsType,
+      pledgeAmount: profile.pledgeAmount,
+      incomeAmount: profile.incomeAmount
+    }])),
+    payments: Object.fromEntries(Object.entries(getters.currentGroupState.payments).map(([id, payment]) => [id, {
+      amount: payment.data.amount,
+      exchangeRate: payment.data.exchangeRate,
+      status: payment.data.status,
+      createdDate: payment.meta.createdDate
+    }])),
+    monthlyPayments: Object.fromEntries(Object.entries(getters.currentGroupState.paymentsByMonth).map(([key, payments]) => [key, {
+      mincomeExchangeRate: payments.mincomeExchangeRate,
+      paymentsFrom: payments.paymentsFrom
+    }]))
+  })
+}
+
+/*
+
+groupMincomeAmount = 12
+
+groupProfiles = {
+  "u1": {
+    "globalUsername": "",
+    "contractID": "21XWnNKFgXGSigVTkZ9iAmD4X1dbhvxyFPDY9nTEkWtfW6QgaU",
+    "joinedDate": "2020-10-16T18:57:24.277Z",
+    "nonMonetaryContributions": [],
+    "status": "active",
+    "departedDate": null,
+    "incomeDetailsType": "pledgeAmount",
+    "pledgeAmount": 10,
+    "paymentMethods": []
+  },
+  "u2": {
+    "globalUsername": "",
+    "contractID": "21XWnNK5sQHid4iJwSVEPqbrEpckRaT2zNwcHwXnjzYbBbUAmU",
+    "joinedDate": "2020-10-16T18:57:33.867Z",
+    "nonMonetaryContributions": [],
+    "status": "active",
+    "departedDate": null,
+    "incomeDetailsType": "incomeAmount",
+    "incomeAmount": 10,
+    "paymentMethods": []
+  }
+}
+
+What we actually use:
+
+{
+  "u1": {
+    "incomeDetailsType": "pledgeAmount",
+    "pledgeAmount": 10,
+  },
+  "u2": {
+    "incomeDetailsType": "incomeAmount",
+    "incomeAmount": 10,
+  }
+}
+
+*/
