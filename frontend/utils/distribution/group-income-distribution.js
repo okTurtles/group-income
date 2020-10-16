928a0e5d59c22aa7d46a774b0a5ef422b4ec04f3
Steven Degutis <sbdegutis@gmail.com>
2020-10-16 17:40:42 -0500

    [#763] Splitting out group income distribution and adjustment logic.

--- frontend/utils/distribution/group-income-distribution.js
+++ frontend/utils/distribution/group-income-distribution.js
@@ -4,21 +4,10 @@ import paymentTotalFromUserToUser from '~/frontend/model/contracts/payments/tota
 import { remapObject } from '~/frontend/utils/giLodash.js'
 import { ISOStringToMonthstamp } from '~/frontend/utils/time.js'
 
-export function groupIncomeDistributionLogic ({
-  monthstamp,
-  adjusted,
+export function groupIncomeDistributionBaseLogic ({
   mincomeAmount,
-  groupProfiles,
-  payments,
-  monthlyPayments
+  groupProfiles
 }) {
-  // the monthstamp will always be for the current month. the alternative
-  // is to allow the re-generation of the distribution for previous months,
-  // but that approach requires also storing the historical mincomeAmount
-  // and historical groupProfiles. Since together these change across multiple
-  // locations in the code, it involves less 'code smell' to do it this way.
-  // see historical/group.js for the ugly way of doing it.
-
   const currentIncomeDistribution = (Object.entries(groupProfiles)
     // filter out users without a profile or without income details
     .filter(([name, profile]) => profile && profile.incomeDetailsType)
@@ -30,64 +19,74 @@ export function groupIncomeDistributionLogic ({
       return { name, amount }
     }))
 
-  var dist = incomeDistribution(currentIncomeDistribution, mincomeAmount)
-  if (adjusted) {
-    // if this user has already made some payments to other users this
-    // month, we need to take that into account and adjust the distribution.
-    // this will be used by the Payments page to tell how much still
-    // needs to be paid (if it was a partial payment).
-    const carried = Object.create(null)
-    for (const p of dist) {
-      const alreadyPaid = paymentTotalFromUserToUser({
-        fromUser: p.from,
-        toUser: p.to,
-        paymentMonthstamp: monthstamp,
-        payments,
-        monthlyPayments
-      })
+  return incomeDistribution(currentIncomeDistribution, mincomeAmount)
+}
 
-      const carryAmount = p.amount - alreadyPaid
-      // ex: it wants us to pay $2, but we already paid $3, thus: carryAmount = -$1 (all done paying)
-      // ex: it wants us to pay $3, but we already paid $2, thus: carryAmount = $1 (remaining to pay)
-      // if we "overpaid" because we sent late payments, remove us from consideration
-      p.amount = saferFloat(Math.max(0, carryAmount))
-      // calculate our carried adjustment (used when distribution changes due to new users)
-      if (!carried[p.from]) carried[p.from] = { carry: 0, total: 0 }
-      carried[p.from].total += p.amount
-      if (carryAmount < 0) carried[p.from].carry += -carryAmount
-    }
-    // we loop through and proportionally subtract the amount that we've already paid
-    dist = dist.filter(p => p.amount > 0)
-    for (const p of dist) {
-      const c = carried[p.from]
-      p.amount = saferFloat(p.amount - (c.carry * p.amount / c.total))
-    }
-    // console.debug('adjustedDist', adjustedDist, 'carried', carried)
+// Note: this mutates the objects inside dist
+export function groupIncomeDistributionAdjustmentLogic (dist, {
+  monthstamp,
+  payments,
+  monthlyPayments
+}) {
+  // if this user has already made some payments to other users this
+  // month, we need to take that into account and adjust the distribution.
+  // this will be used by the Payments page to tell how much still
+  // needs to be paid (if it was a partial payment).
+  const carried = Object.create(null)
+  for (const p of dist) {
+    const alreadyPaid = paymentTotalFromUserToUser({
+      fromUser: p.from,
+      toUser: p.to,
+      paymentMonthstamp: monthstamp,
+      payments,
+      monthlyPayments
+    })
+
+    const carryAmount = p.amount - alreadyPaid
+    // ex: it wants us to pay $2, but we already paid $3, thus: carryAmount = -$1 (all done paying)
+    // ex: it wants us to pay $3, but we already paid $2, thus: carryAmount = $1 (remaining to pay)
+    // if we "overpaid" because we sent late payments, remove us from consideration
+    p.amount = saferFloat(Math.max(0, carryAmount))
+    // calculate our carried adjustment (used when distribution changes due to new users)
+    if (!carried[p.from]) carried[p.from] = { carry: 0, total: 0 }
+    carried[p.from].total += p.amount
+    if (carryAmount < 0) carried[p.from].carry += -carryAmount
   }
+  // we loop through and proportionally subtract the amount that we've already paid
+  dist = dist.filter(p => p.amount > 0)
+  for (const p of dist) {
+    const c = carried[p.from]
+    p.amount = saferFloat(p.amount - (c.carry * p.amount / c.total))
+  }
+  // console.debug('adjustedDist', adjustedDist, 'carried', carried)
   return dist
 }
 
 export default function groupIncomeDistribution ({ getters, monthstamp, adjusted }) {
-  return groupIncomeDistributionLogic({
-    monthstamp,
-    adjusted,
+  let dist = groupIncomeDistributionLogic({
     mincomeAmount: getters.groupMincomeAmount,
     groupProfiles: remapObject(getters.groupProfiles, (profile) => ({
       incomeDetailsType: profile.incomeDetailsType,
       pledgeAmount: profile.pledgeAmount,
       incomeAmount: profile.incomeAmount
     })),
-    payments: remapObject(getters.currentGroupState.payments, (payment) => ({
-      amount: payment.data.amount,
-      exchangeRate: payment.data.exchangeRate,
-      status: payment.data.status,
-      creationMonthstamp: ISOStringToMonthstamp(payment.meta.createdDate)
-    })),
-    monthlyPayments: remapObject(getters.currentGroupState.paymentsByMonth, (payments) => ({
-      mincomeExchangeRate: payments.mincomeExchangeRate,
-      paymentsFrom: payments.paymentsFrom
-    }))
   })
+  if (adjusted) {
+    dist = groupIncomeDistributionAdjustmentLogic(dist, {
+      monthstamp,
+      payments: remapObject(getters.currentGroupState.payments, (payment) => ({
+        amount: payment.data.amount,
+        exchangeRate: payment.data.exchangeRate,
+        status: payment.data.status,
+        creationMonthstamp: ISOStringToMonthstamp(payment.meta.createdDate)
+      })),
+      monthlyPayments: remapObject(getters.currentGroupState.paymentsByMonth, (payments) => ({
+        mincomeExchangeRate: payments.mincomeExchangeRate,
+        paymentsFrom: payments.paymentsFrom
+      }))
+    })
+  }
+  return dist
 }
 
 /*
--- frontend/utils/distribution/group-income-distribution.test.js
+++ frontend/utils/distribution/group-income-distribution.test.js
@@ -5,66 +5,50 @@
 
 import should from 'should'
 import incomeDistribution from './mincome-default.js'
-import { groupIncomeDistributionLogic } from './group-income-distribution.js'
+import { groupIncomeDistributionBaseLogic, groupIncomeDistributionAdjustmentLogic } from './group-income-distribution.js'
 
 describe('group income distribution logic', function () {
   it('can distribute income evenly with two users', function () {
-    should(groupIncomeDistributionLogic({
-      monthstamp: "2020-10",
-      adjusted: false,
+    const dist = groupIncomeDistributionBaseLogic({
       mincomeAmount: 12,
       groupProfiles: {
         "u1": { incomeDetailsType: "pledgeAmount", pledgeAmount: 10 },
         "u2": { incomeDetailsType: "incomeAmount", incomeAmount: 10 }
-      },
+      }
+    })
+    should(dist).eql([{ amount: 2, from: 'u1', to: 'u2' }])
+
+    const adjustedDist = groupIncomeDistributionAdjustmentLogic(dist, {
+      monthstamp: "2020-10",
       payments: {},
       monthlyPayments: {}
-    })).eql([
-      { amount: 2, from: 'u1', to: 'u2' }
-    ])
+    })
+    should(adjustedDist).eql([{ amount: 2, from: 'u1', to: 'u2' }])
   })
 
   it('ignores existing payments when not adjusted', function () {
-    should(groupIncomeDistributionLogic({
-      monthstamp: "2020-10",
-      adjusted: false,
+    const dist = groupIncomeDistributionBaseLogic({
       mincomeAmount: 12,
       groupProfiles: {
         "u1": { incomeDetailsType: "pledgeAmount", pledgeAmount: 10 },
         "u2": { incomeDetailsType: "incomeAmount", incomeAmount: 10 }
       },
-      payments: {
-        "payment1": {
-          amount: 2,
-          exchangeRate: 1,
-          status: "completed",
-          creationMonthstamp: "2020-10"
-        }
-      },
-      monthlyPayments: {
-        "2020-10": {
-          mincomeExchangeRate: 1,
-          paymentsFrom: {
-            "u1": {
-              "u2": ["payment1"]
-            }
-          }
-        }
-      }
-    })).eql([
-      { amount: 2, from: 'u1', to: 'u2' }
-    ])
+    })
+    should(dist).eql([{ amount: 2, from: 'u1', to: 'u2' }])
   })
 
   it('takes into account payments from this month when adjusted', function () {
-    should(groupIncomeDistributionLogic({
-      monthstamp: "2020-10",
-      adjusted: true,
+    const dist = groupIncomeDistributionBaseLogic({
       mincomeAmount: 12,
       groupProfiles: {
         "u1": { incomeDetailsType: "pledgeAmount", pledgeAmount: 10 },
         "u2": { incomeDetailsType: "incomeAmount", incomeAmount: 10 }
       },
+    })
+    should(dist).eql([{ amount: 2, from: 'u1', to: 'u2' }])
+
+    const adjustedDist = groupIncomeDistributionAdjustmentLogic(dist, {
+      monthstamp: "2020-10",
       payments: {
         "payment1": {
           amount: 2,
@@ -83,6 +67,7 @@ describe('group income distribution logic', function () {
           }
         }
       }
-    })).eql([])
+    })
+    should(adjustedDist).eql([])
   })
 })
