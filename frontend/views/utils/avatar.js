fe08e8edf3c0b12e3f51a409b4fe499b3311ec0d
SebinSong <songxevin@gmail.com>
2020-11-25 14:41:27 +1300

    add try catch block to the sbp function

--- frontend/views/utils/avatar.js
+++ frontend/views/utils/avatar.js
@@ -9,17 +9,24 @@ const theme = Colors.light
 
 sbp('sbp/selectors/register', {
   'gi.utils/avatar/create': function () {
-    // creates a random avatar image as a blob
+    // creates a random avatar image and upload it
+    let avatarBase64, avatarBlob
 
     const palette = randomFromArray(colorOptions)
-    const avatarBase64 = blockies.create({
-      bgcolor: theme[`${palette}_0`], // darkest
-      color: theme[`${palette}_1`], // medium
-      spotcolor: theme[`${palette}_2`], // lightest
-      size: 6,
-      scale: 12
-    }).toDataURL('image/png')
-    const avatarBlob = imageDataURItoBlob(avatarBase64)
+
+    try {
+      avatarBase64 = blockies.create({
+        bgcolor: theme[`${palette}_0`], // darkest
+        color: theme[`${palette}_1`], // medium
+        spotcolor: theme[`${palette}_2`], // lightest
+        size: 6,
+        scale: 12
+      }).toDataURL('image/png')
+      avatarBlob = imageDataURItoBlob(avatarBase64)
+    } catch (e) {
+      // This may fail in old browsers (e.g. IE, Opera Mini, etc...)
+      console.warn("Avatar creation process didn't go well.", e)
+    }
 
     return imageUpload(avatarBlob)
   }
