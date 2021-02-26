07c3a189c42c4e90a3af9e00155b8c5b6c278d7b
snowteamer <64228468+snowteamer@users.noreply.github.com>
2021-02-26 18:23:28 +0100

    Upload new pubsub utilities

---
+++ backend/utils/pubsub.js
@@ -0,0 +1,261 @@
+/* globals logger */
+'use strict'
+
+import {
+  NOTIFICATION_TYPE,
+  REQUEST_TYPE,
+  RESPONSE_TYPE,
+  createClient,
+  createMessage,
+  messageParser
+} from '~/shared/pubsub.js'
+
+import type { Message, SubMessage, UnsubMessage } from '~/shared/pubsub.js'
+import type { JSONType } from '~/shared/types.js'
+
+const { bold } = require('chalk')
+const WebSocket = require('ws')
+
+// ====== API ====== //
+
+const { PUB, SUB, UNSUB } = NOTIFICATION_TYPE
+const { ERROR, SUCCESS } = RESPONSE_TYPE
+
+// Re-export some useful things from the shared module.
+export { createClient, createMessage, NOTIFICATION_TYPE, REQUEST_TYPE, RESPONSE_TYPE }
+
+export function createNotification (
+  type: string,
+  data: {| contractID: string, socketID: number |}
+): string {
+  return JSON.stringify({ type, ...data })
+}
+
+export function createResponse (type: string, data: JSONType): string {
+  return JSON.stringify({ type, data })
+}
+
+/**
+ * Creates a pubsub server instance.
+ *
+ * @param {(http.Server|https.Server)} server - A Node.js HTTP/S server to attach to.
+ * @param {Object?} options
+ * {object?} clientHandlers - Custom handlers for socket events.
+ * {object?} messageHandlers - Custom handlers for different message types.
+ * {object?} serverHandlers - Custom handlers for server events.
+ *
+ * {number} backlog - The maximum length of the queue of pending connections.
+ * {boolean} clientTracking - Specifies whether or not to track clients.
+ * {Function} handleProtocols - A function which can be used to handle the WebSocket subprotocols.
+ * {number} maxPayload - The maximum allowed message size in bytes.
+ * {string} path - Accept only connections matching this path.
+ * {(boolean|object)} perMessageDeflate - Enables/disables per-message deflate.
+ * @returns {Object}
+ */
+export function createServer (httpServer: Object, options?: Object = {}): Object {
+  const server = new WebSocket.Server({
+    ...defaultOptions,
+    ...options,
+    server: httpServer
+  })
+  server.customClientHandlers = { ...options.clientHandlers }
+  server.customServerHandlers = { ...options.serverHandlers }
+  server.messageHandlers = { ...defaultMessageHandlers, ...options.messageHandlers }
+  server.pingInterval = undefined
+  server.subscribersByContractID = Object.create(null)
+
+  // Create and attach server-side WebSocket event listeners.
+  ;['close', 'connection', 'error', 'headers', 'listening'].forEach((name) => {
+    server.on(name, (...args) => {
+      console.log('[pubsub] Server event:', name, ...args)
+      try {
+        const customHandler = server.customServerHandlers['on' + name]
+        const defaultHandler = defaultServerHandlers['on' + name]
+        // Always call the default handler first.
+        if (defaultHandler) {
+          defaultHandler.call(server, ...args)
+        }
+        if (customHandler) {
+          customHandler.call(server, ...args)
+        }
+      } catch (error) {
+        logger(error)
+      }
+    })
+  })
+  // Setup a ping interval if required.
+  if (server.options.pingInterval > 0) {
+    server.pingInterval = setInterval(() => {
+      server.clients.forEach((client) => {
+        if (!client.isAlive) {
+          return client.terminate()
+        }
+        client.isAlive = false
+        client.ping()
+      })
+    }, server.options.pingInterval)
+  }
+  return server
+}
+
+const defaultOptions = {
+  clientTracking: true,
+  maxPayload: 6 * 1024 * 1024,
+  pingInterval: undefined
+}
+
+const generateSocketID = (() => {
+  let counter = 0
+
+  return () => counter++
+})()
+
+// ====== Server socket event handlers ====== //
+
+const defaultServerHandlers = Object.create(null)
+
+/**
+ * Emitted when a connection handshake completes.
+ *
+ * @param socket - The client socket that connected.
+ * @param request - The http GET request sent by the socket.
+ */
+defaultServerHandlers.onconnection = function onconnection (socket, request) {
+  socket.id = generateSocketID()
+  socket.isAlive = true
+  socket.server = this
+  socket.subscriptions = new Set()
+
+  const xForwardedFor = request.headers['x-forwarded-for']
+
+  const remoteAddress = (
+    typeof xForwardedFor === 'string' && xForwardedFor !== ''
+      ? xForwardedFor.split(/\s*,\s*/)[0]
+      : request.socket.remoteAddress
+  )
+  console.log(bold(`[pubsub] Socket ${socket.id} connected from ${remoteAddress}`))
+
+  // Create and attach socket event listeners.
+  ;['close', 'error', 'message', 'ping', 'pong'].forEach((eventName) => {
+    socket.on(eventName, (...args) => {
+      console.log(`[pubsub] Event '${eventName}' on socket ${socket.id}`, ...args)
+      const customHandler = socket.server.customClientHandlers['on' + eventName]
+      const defaultHandler = defaultClientHandlers['on' + eventName]
+
+      try {
+        if (defaultHandler) {
+          defaultHandler.call(socket, ...args)
+        }
+        if (customHandler) {
+          customHandler.call(socket, ...args)
+        }
+      } catch (error) {
+        logger(error)
+      }
+    })
+  })
+}
+
+// ====== Client socket event handlers ====== //
+
+const defaultClientHandlers = {
+  onclose (code: string, reason: string) {
+    // Notify other client sockets that this one has left any room they shared.
+    for (const contractID of this.subscriptions) {
+      const notification = createNotification(UNSUB, { contractID, socketID: this.id })
+      for (const client of this.server.clients) {
+        // This should exclude this client.
+        if (client.readyState === WebSocket.OPEN && client.subscriptions.has(contractID)) {
+          client.send(notification)
+        }
+      }
+    }
+    this.alive = false
+    this.subscriptions.clear()
+  },
+
+  onmessage (data: string) {
+    let msg: Message = { type: '' }
+    try {
+      msg = messageParser(data)
+    } catch (error) {
+      console.log(bold.red(`[pubsub] Malformed message: ${error.message}`))
+      return this.terminate()
+    }
+    const handler = this.server.messageHandlers[msg.type]
+
+    if (handler) {
+      try {
+        handler.call(this, msg)
+      } catch (error) {
+        // Log the error message and stack trace but do not send it to the client.
+        logger(error)
+        const failure = createResponse(ERROR, { ...msg })
+        // Should we call 'this.terminate()' instead?
+        this.send(failure, () => this.close())
+      }
+    } else {
+      console.error(`[pubsub] Unhandled message type: ${msg.type}`)
+      this.terminate()
+    }
+  },
+
+  onpong () {
+    this.isAlive = true
+  }
+}
+
+// ====== Server-side message handlers ====== //
+
+const defaultMessageHandlers = {
+  [PUB] (msg: Object) {
+    // Currently unused.
+  },
+
+  [SUB] ({ contractID, type }: SubMessage) {
+    const socketID = this.id
+    const { server, subscriptions } = this
+    const { subscribersByContractID } = this.server
+
+    if (!subscriptions.has(contractID)) {
+      // Add the given contract ID to our subscriptions.
+      subscriptions.add(contractID)
+      if (!subscribersByContractID[contractID]) {
+        subscribersByContractID[contractID] = new Set()
+      }
+      subscribersByContractID[contractID].add(this)
+      // Notify other subscribers of this contract that we have joined them.
+      server.clients.forEach((client) => {
+        if (client !== this && client.subscriptions.has(contractID)) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send({ type, contractID, socketID })
+          }
+        }
+      })
+    }
+    this.send(createResponse(SUCCESS, { type, contractID }))
+  },
+
+  [UNSUB] ({ contractID, type }: UnsubMessage) {
+    const socketID = this.id
+    const { server, subscriptions } = this
+    const { subscribersByContractID } = this.server
+
+    if (subscriptions.has(contractID)) {
+      // Remove the given contract ID from our subscriptions.
+      subscriptions.delete(contractID)
+      if (subscribersByContractID[contractID]) {
+        subscribersByContractID[contractID].delete(this)
+      }
+      // Notify other subscribers of this contract that we have left them.
+      server.clients.forEach((client) => {
+        if (client !== this && client.subscriptions.has(contractID)) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send({ type, contractID, socketID })
+          }
+        }
+      })
+    }
+    this.send(createResponse(SUCCESS, { type, contractID }))
+  }
+}
---
+++ shared/pubsub.js
@@ -0,0 +1,386 @@
+'use strict'
+
+import type { JSONObject, JSONType } from '~/shared/types.js'
+
+// ====== Types ====== //
+
+export type Message = {
+  [key: string]: JSONType,
+  +type: string
+}
+
+export type PubSubClient = {
+  connectionTimeoutID: TimeoutID | void,
+  +customHandlers: Object,
+  failedAttempts: number,
+  isReconnecting: boolean,
+  +listeners: Object,
+  +messageHandlers: Object,
+  +options: Object,
+  +pendingSubscriptionSet: Set<string>,
+  +pendingUnsubscriptionSet: Set<string>,
+  reconnectionDelayID: TimeoutID | void,
+  socket: WebSocket | null,
+  +subscriptionSet: Set<string>,
+  +url: string,
+  // Methods
+  connect(): void,
+  destroy(): void,
+  getNextRandomReconnectionDelay(): number,
+  pub(contractID: string, data: JSONType): void,
+  sub(contractID: string): void,
+  unsub(contractID: string): void
+}
+
+export type SubMessage = {
+  [key: string]: JSONType,
+  +type: 'sub',
+  +contractID: string
+}
+
+export type UnsubMessage = {
+  [key: string]: JSONType,
+  +type: 'unsub',
+  +contractID: string
+}
+
+// ====== Enums ====== //
+
+export const NOTIFICATION_TYPE = Object.freeze({
+  ENTRY: 'entry',
+  PUB: 'pub',
+  SUB: 'sub',
+  UNSUB: 'unsub'
+})
+
+export const REQUEST_TYPE = Object.freeze({
+  PUB: 'pub',
+  SUB: 'sub',
+  UNSUB: 'unsub'
+})
+
+export const RESPONSE_TYPE = Object.freeze({
+  ERROR: 'error',
+  SUCCESS: 'success'
+})
+
+// ====== API ====== //
+
+/**
+ * Creates a pubsub client instance.
+ *
+ * @param {string} url - A WebSocket URL to connect to.
+ * @param {Object?} options
+ * {number?} timeout - Connection timeout duration in milliseconds.
+ * {boolean?} debug
+ * {object?} handlers - Custom handlers for socket events.
+ * {object?} messageHandlers - Custom handlers for different message types.
+ * {boolean?} reconnectOnDisconnection - Whether to reconnect after a server-side disconnection.
+ * {boolean?} reconnectOnOnline - Whether to reconnect after regaining internet connectivity.
+ * {boolean?} reconnectOnTimeout - Whether to reconnect after a timeout.
+ * @returns {Object}
+ */
+export function createClient (url: string, options?: Object = {}): PubSubClient {
+  const client = {
+    customHandlers: { ...options.handlers },
+    failedAttempts: 0,
+    isReconnecting: false,
+    listeners: Object.create(null),
+    messageHandlers: { ...defaultMessageHandlers, ...options.messageHandlers },
+    options: { ...defaultOptions, ...options },
+    pendingSubscriptionSet: new Set(),
+    pendingUnsubscriptionSet: new Set(),
+    reconnectionDelayID: undefined,
+    socket: null,
+    subscriptionSet: new Set(),
+    connectionTimeoutID: undefined,
+    url: url.replace(/^http/, 'ws'),
+    // Methods
+    ...{ connect, destroy, getNextRandomReconnectionDelay, pub, sub, unsub }
+  }
+  // Create and save references to reusable event listeners.
+  for (const name of [...eventNames, ...globalEventNames]) {
+    client.listeners[name] = (event: Event, ...args: any[]) => {
+      if (client.options.debug) {
+        console.log('[pubsub] Event:', name, ...args)
+      }
+      const customHandler = client.customHandlers['on' + name]
+      const defaultHandler = defaultHandlers['on' + name]
+
+      if (defaultHandler) {
+        defaultHandler.call(client, event)
+      }
+      if (customHandler) {
+        customHandler.call(client, event)
+      }
+    }
+  }
+  // Attach global event listeners before the first connection.
+  if (typeof window === 'object') {
+    globalEventNames.forEach((name) => {
+      window.addEventListener(name, client.listeners[name])
+    })
+  }
+  client.connect()
+  return client
+}
+
+export function createMessage (type: string, data: JSONType): string {
+  return JSON.stringify({ type, data })
+}
+
+export function createRequest (type: $Values<typeof REQUEST_TYPE>, data: JSONObject): string {
+  return JSON.stringify(Object.assign({ type }, data))
+}
+
+// ====== Event Handlers ====== //
+
+const defaultHandlers = {
+  onclose (event: CloseEvent) {
+    if (this.socket) {
+      // Remove event listeners to avoid memory leaks.
+      for (const name of eventNames) {
+        this.socket.removeEventListener(name, this.listeners[name])
+      }
+    }
+    this.socket = null
+
+    if (this.connectionTimeoutID) {
+      clearTimeout(this.connectionTimeoutID)
+      this.connectionTimeoutID = undefined
+    }
+    if (this.isReconnecting) {
+      this.failedAttempts++
+    }
+    if (event.reason === 'timeout' && this.options.reconnectOnTimeout) {
+      this.reconnectionDelayID = setTimeout(
+        this.connect,
+        this.getNextRandomReconnectionDelay()
+      )
+    }
+  },
+
+  // Do not manually close the socket here as it will be done automatically.
+  onerror (event: Event) {
+  },
+
+  onmessage (event: MessageEvent) {
+    const { data } = event
+
+    if (typeof data !== 'string') {
+      return
+    }
+
+    let msg: Message = { type: '' }
+    try {
+      msg = messageParser(data)
+    } catch (error) {
+      // TODO: place us in unrecoverable state (see state.js error handling TODOs)
+      console.log(`[pubsub] Malformed message: ${error.message}`)
+      return
+    }
+    const handler = this.messageHandlers[msg.type]
+
+    if (handler) {
+      handler.call(this, msg)
+    } else {
+      throw new Error(`Unhandled message type: ${msg.type}`)
+    }
+  },
+
+  onopen (event) {
+    this.pendingSubscriptionSet.forEach(this.sub)
+    this.pendingUnsubscriptionSet.forEach(this.unsub)
+
+    if (this.isReconnecting) {
+      console.log('[pubsub] Connection re-established')
+      this.failedAttempts = 0
+      this.isReconnecting = false
+    }
+    if (this.connectionTimeoutID) {
+      clearTimeout(this.connectionTimeoutID)
+      this.connectionTimeoutID = undefined
+    }
+  }
+}
+
+// ====== Message Handlers ====== //
+
+const defaultMessageHandlers = {
+  // PUB can be used to send ephemeral messages outside of any contract log.
+  [NOTIFICATION_TYPE.PUB] (msg) {
+    console.debug(`[pubsub] Ignoring ${msg.type} message:`, msg.data)
+  },
+  [NOTIFICATION_TYPE.SUB] (msg) {
+    console.debug(`[pubsub] Ignoring ${msg.type} message:`, msg.data)
+  },
+  [NOTIFICATION_TYPE.UNSUB] (msg) {
+    console.debug(`[pubsub] Ignoring ${msg.type} message:`, msg.data)
+  },
+  [RESPONSE_TYPE.ERROR] ({ data: { type, contractID } }) {
+    console.debug(`[pubsub] Received ERROR response for ${type} request to ${contractID}`)
+  },
+  [RESPONSE_TYPE.SUCCESS] ({ data: { type, contractID } }) {
+    const client = this
+    switch (type) {
+      case REQUEST_TYPE.SUB: {
+        console.debug(`[pubsub] Subscribed to ${contractID}`)
+        client.pendingSubscriptionSet.delete(contractID)
+        client.subscriptionSet.add(contractID)
+        break
+      }
+      case REQUEST_TYPE.UNSUB: {
+        console.debug(`[pubsub] Unsubscribed from ${contractID}`)
+        client.pendingUnsubscriptionSet.delete(contractID)
+        client.subscriptionSet.delete(contractID)
+        break
+      }
+      default: {
+        console.debug(`[pubsub] Malformed response: invalid request type ${type}`)
+      }
+    }
+  }
+}
+
+const defaultOptions = {
+  debug: false,
+  maxReconnectionDelay: Infinity,
+  minReconnectionDelay: 500,
+  maxRetries: 10,
+  reconnectOnDisconnection: false,
+  reconnectOnOnline: true,
+  reconnectOnTimeout: false,
+  reconnectionDelayGrowFactor: 2,
+  timeout: 5000
+}
+
+const eventNames = ['close', 'error', 'message', 'open']
+const globalEventNames = ['offline', 'online']
+
+// Parses and validates a received message.
+export const messageParser = (data: string): Message => {
+  const msg = JSON.parse(data)
+
+  if (typeof msg !== 'object' || msg === null) {
+    throw new TypeError('Message is null or not an object')
+  }
+  const { type } = msg
+
+  if (typeof type !== 'string' || type === '') {
+    throw new TypeError('Message type must be a non-empty string')
+  }
+  return msg
+}
+
+// ====== Client methods ====== //
+
+function connect () {
+  if (this.socket !== null) {
+    throw new Error('connect() can only be called if there is no current socket.')
+  }
+  this.socket = new WebSocket(this.url)
+
+  if (this.options.timeout) {
+    this.connectionTimeoutID = setTimeout(() => {
+      this.connectionTimeoutID = undefined
+      if (this.socket) {
+        this.socket.close(1000, 'timeout')
+      }
+    }, this.options.timeout)
+  }
+  this.reconnectionDelayID = undefined
+  // Attach WebSocket event listeners.
+  for (const name of eventNames) {
+    this.socket.addEventListener(name, this.listeners[name])
+  }
+}
+
+/**
+ * Immediately remove every event listener and free up ressources.
+ * This method is used in unit tests.
+ *
+ * - No 'close' event will be emitted.
+ * - Any incoming or outgoing buffered data will be discarded.
+ * - Any pending request will be discarded.
+ */
+function destroy () {
+  // Clear all timers.
+  clearTimeout(this.connectionTimeoutID)
+  clearTimeout(this.reconnectionDelayID)
+  // Update property values.
+  // Note: do not clear 'this.options'.
+  this.connectionTimeoutID = undefined
+  this.failedAttempts = 0
+  this.isReconnecting = false
+  this.pendingSubscriptionSet.clear()
+  this.pendingUnsubscriptionSet.clear()
+  this.reconnectionDelayID = undefined
+  this.subscriptionSet.clear()
+  // Remove global event listeners.
+  if (typeof window === 'object') {
+    for (const name of globalEventNames) {
+      window.removeEventListener(name, this.listeners[name])
+    }
+  }
+  // Remove WebSocket event listeners.
+  if (this.socket) {
+    for (const name of eventNames) {
+      this.socket.removeEventListener(name, this.listeners[name])
+    }
+    this.socket.close()
+  }
+  this.socket = null
+}
+
+function getNextRandomReconnectionDelay (): number {
+  const {
+    maxReconnectionDelay,
+    minReconnectionDelay,
+    reconnectionDelayGrowFactor
+  } = this.options
+
+  const minDelay = (
+    minReconnectionDelay * reconnectionDelayGrowFactor ** this.failedAttempts
+  )
+  const maxDelay = Math.min(
+    minReconnectionDelay * reconnectionDelayGrowFactor ** (this.failedAttempts + 1),
+    maxReconnectionDelay
+  )
+  return minDelay + Math.random() * (maxDelay - minDelay)
+}
+
+function pub (contractID: string, data: JSONType) {
+  const { socket } = this
+
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    const request = createRequest(REQUEST_TYPE.PUB, { contractID, data })
+    socket.send(request)
+  }
+  // Maybe add this request to a pending list.
+}
+
+function sub (contractID: string) {
+  const { socket } = this
+
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    socket.send(createRequest(REQUEST_TYPE.SUB, { contractID }))
+  }
+  this.pendingSubscriptionSet.add(contractID)
+}
+
+function unsub (contractID: string) {
+  const { socket } = this
+
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    socket.send(createRequest(REQUEST_TYPE.UNSUB, { contractID }))
+  }
+  this.pendingUnsubscriptionSet.add(contractID)
+}
+
+export default {
+  NOTIFICATION_TYPE,
+  REQUEST_TYPE,
+  RESPONSE_TYPE,
+  createClient,
+  createRequest
+}
