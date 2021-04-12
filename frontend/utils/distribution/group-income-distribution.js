84966dbd5730e78425c82c4832fae453d98c7049
CrazedCoding <CrazedCoding@gmail.com>
2021-04-12 13:21:06 +0200

    Removed unnecessary sorting by object keys.

--- frontend/utils/distribution/group-income-distribution.js
+++ frontend/utils/distribution/group-income-distribution.js
@@ -3,6 +3,5 @@ import minimizeTotalPaymentsCount from '~/frontend/utils/distribution/payments-m
 
 export default function groupIncomeDistribution (distributionEvents: Array<Object>, { mincomeAmount, adjusted = false, minimizeTxns = false, monthlyRated = true }: Object): any {
   const dist = parseMonthlyDistributionFromEvents(distributionEvents, mincomeAmount, monthlyRated, adjusted)
-  const sortObject = obj => Object.keys(obj).sort().reverse().reduce((res, key) => (res[key] = obj[key], res), {})
-  return (minimizeTxns ? minimizeTotalPaymentsCount(dist) : dist).map(sortObject)
+  return minimizeTxns ? minimizeTotalPaymentsCount(dist) : dist
 }
