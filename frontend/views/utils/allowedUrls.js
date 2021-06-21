86c6eb51e3e92a875d4c72caa1e4fc8e956c9575
snowteamer <64228468+snowteamer@users.noreply.github.com>
2021-06-21 08:10:07 +0200

    Add allowedUrls.js

---
+++ frontend/views/utils/allowedUrls.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+/**
+ * Table of all well-known safe URLs used in the app UI.
+ *
+ * - Use it e.g. to avoid typing URLs.
+ * - It has been frozen as an extra safety measure, to prevent any alteration even in case of a bug.
+ */
+const ALLOWED_URLS = Object.freeze(Object.fromEntries([
+  ['ISSUE_PAGE', 'https://github.com/okTurtles/group-income-simple/issues'],
+  ['BLOG_PAGE', 'https://groupincome.org/blog'],
+  ['DONATE_PAGE', 'https://groupincome.org/donate'],
+  ['FAQ_PAGE', 'https://groupincome.org/faq']
+]))
+
+Vue.prototype.ALLOWED_URLS = ALLOWED_URLS
+
+export default ALLOWED_URLS
