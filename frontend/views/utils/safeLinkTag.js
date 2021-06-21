6fea5e1bb3f0dfea170451e55eeb8be924dd11dd
snowteamer <64228468+snowteamer@users.noreply.github.com>
2021-06-21 08:11:31 +0200

    Update safeLinkTag.js to use the ALLOWED_URLS map

--- frontend/views/utils/safeLinkTag.js
+++ frontend/views/utils/safeLinkTag.js
@@ -1,17 +1,10 @@
-// Table of all well-known safe URLs used in the app UI.
-// The resulting object is frozen as an extra safety measure, to prevent any alteration even in case of a bug.
-const urlsByKey = Object.freeze(Object.fromEntries([
-  ['ISSUE_PAGE', 'https://github.com/okTurtles/group-income-simple/issues'],
-  ['BLOG_PAGE', 'https://groupincome.org/blog'],
-  ['DONATE_PAGE', 'https://groupincome.org/donate'],
-  ['FAQ_PAGE', 'https://groupincome.org/faq]']
-]))
+import allowedUrlsByKey from '@view-utils/allowedUrls.js'
 
 export default function safeLinkTag (key: string): string {
-  if (!Object.prototype.hasOwnProperty.call(urlsByKey, key)) {
+  if (!Object.prototype.hasOwnProperty.call(allowedUrlsByKey, key)) {
     throw new Error(`Unknown URL key: ${key}`)
   }
   // Make sure to include `noopener` and `noreferrer` in the `rel` attribute,
   // to prevent reverse tabnabbing attacks.
-  return `<a class="link" href="${urlsByKey[key]}" target="_blank" rel="noopener noreferrer">`
+  return `<a class="link" href="${allowedUrlsByKey[key]}" target="_blank" rel="noopener noreferrer">`
 }
