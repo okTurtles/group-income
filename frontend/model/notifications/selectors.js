4c6a1999e5a0b0049b10def511a5fd1d2926f5e0
snowteamer <64228468+snowteamer@users.noreply.github.com>
2021-12-31 22:09:19 +0100

    Remove unused 'gi.notifications/save' for now

--- frontend/model/notifications/selectors.js
+++ frontend/model/notifications/selectors.js
@@ -1,8 +1,6 @@
 import type { Notification, NotificationData } from './types.flow.js'
 
 import sbp from '~/shared/sbp.js'
-
-import { applyStorageRules } from './utils.js'
 import * as keys from './mutationKeys.js'
 import templates from './templates.js'
 
@@ -32,21 +30,5 @@ sbp('sbp/selectors/register', {
 
   'gi.notifications/markAllAsRead' (notification: Notification) {
     sbp('state/vuex/commit', keys.MARK_ALL_NOTIFICATIONS_AS_READ, notification)
-  },
-
-  /*
-   * Persists in-memory notifications to local offline storage,
-   * possibly discarding some of them according to predefined storage rules.
-   */
-  async 'gi.notifications/save' () {
-    const rootState = sbp('state/vuex/state')
-
-    if (!rootState.loggedIn) throw new Error('Must be logged in to use this selector.')
-    if (!rootState.loggedIn.username) throw new Error('Invalid or missing username.')
-
-    const notificationsToSave = applyStorageRules(rootState.notifications)
-
-    // TODO: encrypt this?
-    await sbp('gi.db/notifications/save', rootState.loggedIn.username, notificationsToSave)
   }
 })
