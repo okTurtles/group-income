7ddd224b6923d17da3878b7524376149e049f519
silver-it <alex.jin220@gmail.com>
2022-01-20 22:50:32 +0800

    fix: errors regarding the shortening chatroom contract actions

--- frontend/controller/actions/chatroom.js
+++ frontend/controller/actions/chatroom.js
@@ -1,6 +1,8 @@
 'use strict'
 import sbp from '~/shared/sbp.js'
 import type { GIActionParams } from './types.js'
+import L from '@view-utils/translations.js'
+import { encryptedAction } from './utils.js'
 
 export default (sbp('sbp/selectors/register', {
   'gi.actions/chatroom/create': async function (params: GIActionParams) {
@@ -14,5 +16,5 @@ export default (sbp('sbp/selectors/register', {
   ...encryptedAction('gi.actions/chatroom/join', L('Failed to join chat channel.')),
   ...encryptedAction('gi.actions/chatroom/rename', L('Failed to rename chat channel.')),
   ...encryptedAction('gi.actions/chatroom/changeDescription', L('Failed to change chat channel description.')),
-  ...encryptedAction('gi.actions/chatroom/leave', L('Failed to leave chat channel.')),
+  ...encryptedAction('gi.actions/chatroom/leave', L('Failed to leave chat channel.'))
 }): string[])
