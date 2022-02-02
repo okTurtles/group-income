7f04b3eda25d425046e5d65d911c74d5b25e9880
CrazedCoding <CrazedCoding@gmail.com>
2022-02-02 09:46:02 -0500

    Back to proof-of-concept code.

--- frontend/utils/distribution/payments-minimizer.js
+++ frontend/utils/distribution/payments-minimizer.js
@@ -6,24 +6,14 @@ type Payment = {| amount: number; total: number; partial: boolean; isLate: boole
 type Distribution = Array<Payment>;
 
 function untangleDistribution (distribution, havers, needers) {
-  const paymentsByFrom = distribution.reduce((acc, p) => {
-    acc[p.from] ? acc[p.from].push[p] : acc[p.from] = [p];
-    return acc
-  }, {})
-  const paymentsByTo = distribution.reduce((acc, p) => { 
-    acc[p.to] ? acc[p.to].push[p] : acc[p.to] = [p]
-    return acc
-  }, {})
   for (const haver of havers) {
-    const outgoing = paymentsByFrom[haver.name]
-    if(!outgoing) continue
+    const outgoing = distribution.filter((p) => p.from === haver.name)
     for (const paymentfrom of outgoing) {
       const needer = paymentfrom.to
       for (const otherNeeder of needers) {
         if (needer !== otherNeeder.name) {
-          const incoming = paymentsByTo[otherNeeder.name]
-          if(!incoming) continue
-          const filteredByFrom = incoming.filter((p) => p.dueOn === paymentfrom.dueOn)
+          const incoming = distribution.filter((p) => p.to === otherNeeder.name)
+          const filteredByFrom = incoming.filter((p) => p.from === haver.name && p.dueOn === paymentfrom.dueOn)
           if (filteredByFrom.length === 1) {
             const crossOver = filteredByFrom[0]
             for (const paymentTo of incoming) {
@@ -65,56 +55,59 @@ function minimizeTotalPaymentsCount (distribution: Distribution, groupMembers: A
   let havers = groupMembers.filter((m) => m.haveNeed > 0).sort((a, b) => a.haveNeed - b.haveNeed)
   let needers = groupMembers.filter((m) => m.haveNeed < 0).sort((a, b) => a.haveNeed - b.haveNeed)
 
-  const maxGroupSize = Math.max(havers.length, needers.length)
+  const tangledHavers = []
+  const tangledNeeders = []
+
+  havers.forEach((haver, index) => (index % 2) ? tangledHavers.push(haver) : tangledHavers.unshift(haver))
+  needers.forEach((needer, index) => (index % 2) ? tangledNeeders.push(needer) : tangledNeeders.unshift(needer))
+
+  havers = tangledHavers
+  needers = tangledNeeders
 
   // You can think of this algorithm as a method of untangling
-  // a closed-loop of string; you don't try to untangle the
+  // a closed-loop on string; you don't try to untangle the
   // entire string at once: instead you untangle it in larger
   // and larger sections, and eventually it comes undone.
-  for(let subdivisions = Math.min(2, maxGroupSize); subdivisions <= maxGroupSize; subdivisions++)
-  {
-    const paymentsByFrom = distribution.reduce((acc, p) => {
-      acc[p.from] ? acc[p.from].push[p] : acc[p.from] = [p];
-      return acc
-    }, {})
 
-    havers = havers.filter((haver)=>!paymentsByFrom[haver] || paymentsByFrom[haver].length <= 1)
+  const maxKnot = 10 // This is the maximum number of knots we will untangle at a time.
 
-    const subHavers = [] // Stores the havers into group sizes of subdivisions
-    const subNeeders = [] // Stores the needers into group sizes of subdivisions
+  const subHavers = [] // Stores the havers into group sizes of maxKnot
+  const subNeeders = [] // Stores the needers into group sizes of maxKnot
 
-    const maxSubHavers = Math.ceil(havers.length / subdivisions)
-    for (let step = 0; step < maxSubHavers; step++) {
-      const subArray = havers.slice(step * subdivisions, Math.min((step + 1) * subdivisions, havers.length))
-      subHavers.push(subArray)
-    }
+  const maxSubHavers = Math.ceil(havers.length / maxKnot)
+  for (let step = 0; step < maxSubHavers; step++) {
+    const subArray = havers.slice(step * maxKnot, Math.min((step + 1) * maxKnot, havers.length))
+    subHavers.push(subArray)
+  }
 
-    const maxSubNeeders = Math.ceil(needers.length / subdivisions)
-    for (let step = 0; step < maxSubNeeders; step++) {
-      const subArray = needers.slice(step * subdivisions, Math.min((step + 1) * subdivisions, needers.length))
-      subNeeders.push(subArray)
-    }
-    const maxGroupCount = Math.max(subHavers.length, subNeeders.length);
-    // Divide & conquer the problem:
-    for (let group = 0; group < maxGroupCount; group++) {
-        const subHaves = subHavers[group%subHavers.length]
-        const subNeeds = subNeeders[group%subNeeders.length]
+  const maxSubNeeders = Math.ceil(needers.length / maxKnot)
+  for (let step = 0; step < maxSubNeeders; step++) {
+    const subArray = needers.slice(step * maxKnot, Math.min((step + 1) * maxKnot, needers.length))
+    subNeeders.push(subArray)
+  }
 
-        function filterIrrelivant (payment) {
-          return subHaves.find((haver) => haver.name === payment.from) &&
-                subNeeds.find((needer) => needer.name === payment.to)
-        }
+  // Divide & conquer the problem:
+  for (let haverGroup = 0; haverGroup < maxSubHavers; haverGroup++) {
+    for (let neederGroup = 0; neederGroup < maxSubNeeders; neederGroup++) {
+      const subHaves = subHavers[haverGroup]
+      const subNeeds = subNeeders[neederGroup]
 
-        const minimizable = distribution.filter((payment) => filterIrrelivant(payment))
-        untangleDistribution(minimizable, subHaves, subNeeds)
+      function filterIrrelivant (payment) {
+        return subHaves.find((haver) => haver.name === payment.from) &&
+              subNeeds.find((needer) => needer.name === payment.to)
+      }
 
-        // WITHOUT this line: we get no boosts in computational efficiency
-        // in the final conquering call of untangleDistribution.
-        distribution = distribution.filter((payment) => payment.amount > 0)
+      const minimizable = distribution.filter((payment) => filterIrrelivant(payment))
+
+      untangleDistribution(minimizable, subHaves, subNeeds)
+
+      // WITHOUT this line: we get no boosts in computational efficiency
+      // in the final conquering call of untangleDistribution.
+      distribution = distribution.filter((payment) => payment.amount > 0)
     }
   }
-  untangleDistribution(distribution, havers, needers)
-  distribution = distribution.filter((payment) => payment.amount > 0)
+
+  untangleDistribution(distribution, havers, needers) // Conquer
   return distribution
 }
 
