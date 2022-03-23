b4987baca365061ecbb775bc40ae1a8a70ed8ad3
Pierre Schweiger <schweiger.pierre@gmail.com>
2022-03-23 11:52:11 +0700

    Refactor prompt call in one function with promise

---
+++ shared/domains/gi/ui.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import sbp from '~/shared/sbp.js'
+import { OPEN_MODAL, MODAL_RESPONSE } from '@utils/events.js'
+
+export default (sbp('sbp/selectors/register', {
+  'gi.ui/prompt': function (params: object): Promise<*> {
+    sbp('okTurtles.events/emit', OPEN_MODAL, 'Prompt', null, params)
+
+    return new Promise((resolve, reject) => {
+      sbp('okTurtles.events/on', MODAL_RESPONSE, function (response) {
+        sbp('okTurtles.events/off', MODAL_RESPONSE)
+        resolve(response)
+      })
+    })
+  }
+}))
