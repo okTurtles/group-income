5c0ee09107affa52b6dc4aa2e62a52dfeb4751a7
silver-it <alex.jin220@gmail.com>
2022-03-26 06:03:23 +0800

    chore: removed fake data

--- frontend/views/pages/Messages.vue
+++ frontend/views/pages/Messages.vue
@@ -29,58 +29,6 @@ import chatroom from '@containers/chatroom/chatroom.js'
 import GroupsShortcut from '@containers/chatroom/GroupsShortcut.vue'
 import ConversationsList from '@containers/chatroom/ConversationsList.vue'
 
-const individualMessagesSorted = ['GIBot', 555, 333, 444, 111, 222]
-const users = {
-  'GIBot': {
-    id: 'GIBot',
-    name: 'gibot',
-    displayName: 'GIBot',
-    picture: '/assets/images/group-income-icon-transparent-circle.png',
-    unreadCount: 1,
-    description: 'I’m here to keep you update while you are away'
-  },
-  '111': {
-    id: '111',
-    name: 'johnn',
-    displayName: 'John Mars',
-    picture: '/assets/images/user-avatar-default.png',
-    unreadCount: 0,
-    description: 'You and John are both part of Dreamers group'
-  },
-  '222': {
-    id: '222',
-    name: 'hlenon',
-    displayName: 'Hugo Lenon',
-    picture: '/assets/images/user-avatar-default.png',
-    unreadCount: 0,
-    description: null
-  },
-  '333': {
-    id: '333',
-    name: 'liliabt',
-    displayName: 'Lilia Bouvet',
-    picture: '/assets/images/user-avatar-default.png',
-    unreadCount: 0,
-    description: null
-  },
-  '444': {
-    id: '444',
-    name: 'rickyricky',
-    displayName: 'Rick Eggs',
-    picture: '/assets/images/user-avatar-default.png',
-    unreadCount: 0,
-    description: null
-  },
-  '555': {
-    id: '555',
-    name: 'ericrock',
-    displayName: 'Eric Rock',
-    picture: '/assets/images/user-avatar-default.png',
-    unreadCount: 1,
-    description: 'You and Eric are both part of Dreamers group'
-  }
-}
-
 export default ({
   name: 'Messages',
   mixins: [
@@ -107,8 +55,8 @@ export default ({
     ]),
     messages () {
       return {
-        order: individualMessagesSorted,
-        conversations: users
+        order: [],
+        conversations: []
       }
     },
     type () {
