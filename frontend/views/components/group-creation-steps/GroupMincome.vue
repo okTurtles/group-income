86a1440157441bc450a9aa96215b77a85825fa1c
Pierre Schweiger <schweiger.pierre@gmail.com>
2022-04-25 21:00:42 +0800

    Add selectbox

--- frontend/views/components/group-creation-steps/GroupMincome.vue
+++ frontend/views/components/group-creation-steps/GroupMincome.vue
@@ -38,21 +38,59 @@
 
       i18n.helper This value can be adjusted in the future.
 
+    fieldset.field
+      .label.c-label-tooltip
+        i18n In what day should the first payment distribution be calculated?
+        tooltip(direction='bottom-end')
+          span.button.is-icon-small
+            i.icon-question-circle
+          template(slot='tooltip')
+            i18n(tag='p') Select which day of the month should the distribution be calculated. Every group member will need to update their mincome details before this date to ensure that the algorythm can fairly distribute available funds between group members.
+
+      .selectbox
+        select.select(
+          :aria-label='L("Choose day")'
+          name='distributionDate'
+          required=''
+          :value='group.distributionDate.toString()'
+          @change='update'
+        )
+          i18n(tag='option' disabled value='') Choose day
+          option(
+            v-for='(item, index) in ephemeral.distributionDayRange'
+            :key='index'
+            :value='item'
+          ) {{ new Date(item).getDate() }}
+
+      i18n.helper Payment distribution will be calculated every 30 days.
     slot
 </template>
 
 <script>
 import currencies from '@view-utils/currencies.js'
+import Tooltip from '@components/Tooltip.vue'
+import { dateToPeriodStamp, addTimeToDate, DAYS_MILLIS } from '~/frontend/utils/time.js'
 
 export default ({
   name: 'GroupMincome',
+  components: {
+    Tooltip
+  },
   props: {
     group: { type: Object },
     $v: { type: Object }
   },
   data () {
     return {
-      currencies
+      currencies,
+      ephemeral: {
+        distributionDayRange: []
+      }
+    }
+  },
+  beforeMount () {
+    for (let index = 0; index < 30; index++) {
+      this.ephemeral.distributionDayRange.push(dateToPeriodStamp(addTimeToDate(new Date().setUTCHours(0, 0, 0, 0), index * DAYS_MILLIS)))
     }
   },
   mounted () {
@@ -76,3 +114,10 @@ export default ({
   }
 }: Object)
 </script>
+
+<style lang="scss" scoped>
+.c-label-tooltip {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
