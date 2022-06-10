3b8c558be25c236500ea0d8b072a776875925fe9
silver-it <alex.jin220@gmail.com>
2022-06-10 08:48:45 +0800

    hack: error in console when leave group

--- frontend/views/containers/group-settings/GroupLeaveModal.vue
+++ frontend/views/containers/group-settings/GroupLeaveModal.vue
@@ -94,7 +94,7 @@ export default ({
     code () {
       return L('LEAVE {GROUP_NAME}', {
         // HACK: this.groupSettings is empty object {} when user leaved the group but not left this modal yet
-        GROUP_NAME: this.groupSettings.groupName ? this.groupSettings.groupName.toUpperCase() : ''
+        GROUP_NAME: (this.groupSettings.groupName || '').toUpperCase()
       })
     }
   },
