a99b8762f202f3395ba2d58d605f4dafb38a6280
silver-it <alex.jin220@gmail.com>
2022-07-05 17:47:44 +0800

    feat: added init test case for group chat scrolling

---
+++ test/cypress/integration/group-chat-scrolling.spec.js
@@ -0,0 +1,100 @@
+import { CHATROOM_GENERAL_NAME } from '../../../frontend/model/contracts/constants.js'
+
+const groupName1 = 'Dreamers'
+const groupName2 = 'Donuts'
+const userId = Math.floor(Math.random() * 10000)
+const user1 = `user1-${userId}`
+const user2 = `user2-${userId}`
+let invitationLinkAnyone
+let me
+
+describe('Send/edit/remove messages & add/remove emoticons inside group chat', () => {
+  function switchUser (username) {
+    cy.giSwitchUser(username)
+    me = username
+  }
+
+  function checkIfJoined (channelName, inviter, invitee) {
+    // Attention: need to check just after joined
+    // not after making other activities
+    inviter = inviter || me
+    invitee = invitee || me
+    const selfJoin = inviter === invitee
+    const selfCheck = me === invitee
+    if (selfCheck) {
+      cy.getByDT('messageInputWrapper').within(() => {
+        cy.get('textarea').should('exist')
+      })
+    }
+    cy.getByDT('conversationWapper').within(() => {
+      cy.get('.c-message:last-child .c-who > span:first-child').should('contain', inviter)
+      const message = selfJoin ? `Joined ${channelName}` : `Added a member to ${channelName}: ${invitee}`
+      cy.get('.c-message:last-child .c-notification').should('contain', message)
+    })
+  }
+
+  function sendMessage (message) {
+    cy.getByDT('messageInputWrapper').within(() => {
+      cy.get('textarea').clear().type(`${message}{enter}`)
+      cy.get('textarea').should('be.empty')
+    })
+    cy.getByDT('conversationWapper').within(() => {
+      cy.get('.c-message:last-child .c-who > span:first-child').should('contain', me)
+      cy.get('.c-message.sent:last-child .c-text').should('contain', message)
+    })
+  }
+
+  it(`user1 creats '${groupName1}' group and joins "${CHATROOM_GENERAL_NAME}" channel by default`, () => {
+    cy.visit('/')
+    cy.giSignup(user1)
+    me = user1
+
+    cy.giCreateGroup(groupName1, { bypassUI: true })
+    cy.giGetInvitationAnyone().then(url => {
+      invitationLinkAnyone = url
+    })
+    cy.giRedirectToGroupChat()
+    cy.getByDT('channelName').should('contain', CHATROOM_GENERAL_NAME)
+    cy.getByDT('channelsList').within(() => {
+      cy.get('ul').children().should('have.length', 1)
+    })
+    checkIfJoined(CHATROOM_GENERAL_NAME)
+
+    cy.giLogout()
+  })
+
+  it(`user2 joins ${groupName1} group`, () => {
+    cy.giAcceptGroupInvite(invitationLinkAnyone, {
+      username: user2,
+      groupName: groupName1,
+      shouldLogoutAfter: false,
+      bypassUI: true
+    })
+    me = user2
+    cy.giRedirectToGroupChat()
+
+    cy.getByDT('channelName').should('contain', CHATROOM_GENERAL_NAME)
+    checkIfJoined(CHATROOM_GENERAL_NAME)
+
+    cy.getByDT('channelsList').find('ul>li:first-child').within(() => {
+      cy.get('[data-test]').should('contain', CHATROOM_GENERAL_NAME)
+    })
+  })
+
+  it('user2 sends test messages from 1 to 20', () => {
+    for (let i = 0; i < 20; i++) {
+      sendMessage(`${i + 1}`)
+    }
+  })
+
+  it('user1 sends test messages from 21 to 50', () => {
+    switchUser(user1);
+    cy.giRedirectToGroupChat()
+
+    for (let i = 20; i < 50; i++) {
+      sendMessage(`${i + 1}`)
+    }
+
+    cy.giLogout()
+  })
+})
