631915983dde1f005678ada789faa179d6e63717
SebinSong <songxevin@gmail.com>
2022-08-11 13:08:20 +0900

    remove Boolean()

--- frontend/model/contracts/group.js
+++ frontend/model/contracts/group.js
@@ -120,12 +120,12 @@ function memberLeaves ({ username, dateLeft }, { meta, state, getters }) {
   updateCurrentDistribution({ meta, state, getters })
 }
 
-function isActionYoungerThanUser (actionMeta: Object, userProfile: ?Object): boolean {
+function isActionYoungerThanUser (actionMeta: Object, userProfile: Object): boolean {
   // A util function that checks if an action (or event) in a group occurred after a particular user joined a group.
   // This is used mostly for checking if a notification should be sent for that user or not.
   // e.g.) user-2 who joined a group later than user-1 (who is the creator of the group) doesn't need to receive
   // 'MEMBER_ADDED' notification for user-1.
-  return Boolean(userProfile) &&
+  return userProfile &&
     compareISOTimestamps(actionMeta.createdDate, userProfile.joinedDate) > 0
 }
 
