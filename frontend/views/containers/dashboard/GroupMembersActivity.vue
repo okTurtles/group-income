d932831006972b0039436125f498d0cbb364b620
SebinSong <songxevin@gmail.com>
2023-01-20 13:51:36 +1300

    some bug-fixes in GroupMembersAcitvity.vue

--- frontend/views/containers/dashboard/GroupMembersActivity.vue
+++ frontend/views/containers/dashboard/GroupMembersActivity.vue
@@ -88,7 +88,7 @@ export default ({
     onTimePayments () {
       return Object.entries(this.groupStreaks.onTimePayments || {})
         .filter(([username, streak]) => streak >= STREAK_ON_TIME_PAYMENTS)
-        .map(([username]) => this.userDisplayName(username))
+        .map(([username, streak]) => L('{user} - {count} month streak', { user: this.userDisplayName(username), count: streak }))
     },
     missedPayments () {
       return Object.entries(this.groupStreaks.missedPayments || {})
@@ -139,20 +139,20 @@ export default ({
       }
 
       return {
-        'onTimePayments': this.onTimePayments.length <= 1
-          ? L('{span_}{membercount} member{_span} have {strong_} on-time payment streaks{_strong}', argsMap['onTimePayments'])
+        'onTimePayments': this.onTimePayments.length == 1
+          ? L('{span_}{membercount} member{_span} has {strong_} on-time payment streaks{_strong}', argsMap['onTimePayments'])
           : L('{span_}{membercount} members{_span} have {strong_} on-time payment streaks{_strong}', argsMap['onTimePayments']),
-        'haventLoggedIn': this.haventLoggedIn.length <= 1
-          ? L('{span_}{membercount} member{_span} haven\'t {strong_} logged in past {days} days or more {_strong}', argsMap['haventLoggedIn'])
+        'haventLoggedIn': this.haventLoggedIn.length == 1
+          ? L('{span_}{membercount} member{_span} hasn\'t {strong_} logged in past {days} days or more {_strong}', argsMap['haventLoggedIn'])
           : L('{span_}{membercount} members{_span} haven\'t {strong_} logged in past {days} days or more {_strong}', argsMap['haventLoggedIn']),
-        'noIncomeDetails': this.noIncomeDetails.length <= 1
-          ? L('{span_}{membercount} member{_span} haven\'t {strong_} entered income details{_strong}', argsMap['noIncomeDetails'])
+        'noIncomeDetails': this.noIncomeDetails.length == 1
+          ? L('{span_}{membercount} member{_span} hasn\'t {strong_} entered income details{_strong}', argsMap['noIncomeDetails'])
           : L('{span_}{membercount} members{_span} haven\'t {strong_} entered income details{_strong}', argsMap['noIncomeDetails']),
-        'missedPayments': this.missedPayments.length <= 1
-          ? L('{span_}{membercount} member{_span} have {strong_} missed payments {_strong}', argsMap['missedPayments'])
+        'missedPayments': this.missedPayments.length == 1
+          ? L('{span_}{membercount} member{_span} has {strong_} missed payments {_strong}', argsMap['missedPayments'])
           : L('{span_}{membercount} members{_span} have {strong_} missed payments {_strong}', argsMap['missedPayments']),
-        'noVotes': this.noVotes.length <= 1
-          ? L('{span_}{membercount} member{_span} haven\'t {strong_} voted in the last {proposalcount} proposals {_strong}', argsMap['noVotes'])
+        'noVotes': this.noVotes.length == 1
+          ? L('{span_}{membercount} member{_span} hsn\'t {strong_} voted in the last {proposalcount} proposals {_strong}', argsMap['noVotes'])
           : L('{span_}{membercount} members{_span} haven\'t {strong_} voted in the last {proposalcount} proposals {_strong}', argsMap['noVotes'])
       }
     }
