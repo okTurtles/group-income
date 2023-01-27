ba4631ce29eb8660a7cfdbce6112c72d077206fe
Sebin Song <songxevin@gmail.com>
2023-01-28 12:43:17 +1300

    Implement support history todo tab (#1473)
    * add tooltip to the graph-bar
    
    * implement group's total distribution amount
    
    * fix the lint error
    
    * git cm 'grunt pin version 0.0.12'
    
    * update the contract v0.0.12
    
    * display support history only for those months that are after the group creation date
    
    * remove the wrong contract v12 folder
    
    * work on the CRs
    
    * random comment change for flaky cypress
    
    * random comment change
    
    * delete contracts/0.0.12
    
    * correct the percentage calculation in SupportHistory.vue
    
    * delete contracts v0.0.13 for merge
    
    * grunt pin v0.0.14
    
    * some bug-fixes in GroupMembersAcitvity.vue
    
    * fix linter issue
    
    * grunt pin v14 overwrite
    
    * random comment for flaky cypress
    
    * display >= 1 streaks for on-time payments
    
    * some adjustment to the todo history tab
    
    * work on change requests Ro. 2
    
    * link the real data with TodoHistory.vue
    
    * update the column UI in the BarGraph.vue
    
    * implement 'fullMonthlySupport' streak in group contract

--- frontend/views/containers/payments/PaymentsMixin.js
+++ frontend/views/containers/payments/PaymentsMixin.js
@@ -2,6 +2,7 @@ import sbp from '@sbp/sbp'
 import { mapState, mapGetters } from 'vuex'
 import { PAYMENT_COMPLETED } from '@model/contracts/shared/payments/index.js'
 import { createPaymentInfo, paymentHashesFromPaymentPeriod } from '@model/contracts/shared/functions.js'
+import { humanDate, dateFromPeriodStamp } from '@model/contracts/shared/time.js'
 import { cloneDeep } from '@model/contracts/shared/giLodash.js'
 
 // NOTE: this mixin combines payment information
@@ -14,7 +15,8 @@ const PaymentsMixin: Object = {
       'ourUsername',
       'ourPayments',
       'groupPeriodPayments',
-      'paymentHashesForPeriod'
+      'paymentHashesForPeriod',
+      'dueDateForPeriod'
     ])
   },
   methods: {
@@ -55,7 +57,7 @@ const PaymentsMixin: Object = {
       let detailedPayments = {}
       if (Object.keys(this.groupPeriodPayments).includes(period)) {
         const paymentHashes = this.paymentHashesForPeriod(period) || []
-        detailedPayments = paymentHashes.map(hash => this.currentGroupState.payments[hash])
+        detailedPayments = Object.fromEntries(paymentHashes.map(hash => [hash, this.currentGroupState.payments[hash]]))
       } else {
         const paymentsByPeriodKey = `paymentsByPeriod/${this.ourUsername}/${this.currentGroupId}`
         const paymentsByPeriod = await sbp('gi.db/archive/load', paymentsByPeriodKey) || {}
@@ -84,6 +86,48 @@ const PaymentsMixin: Object = {
       const paymentsKey = `payments/${this.ourUsername}/${this.currentGroupId}`
       const payments = await sbp('gi.db/archive/load', paymentsKey) || {}
       return payments[hash]
+    },
+    async getHaveNeedsSnapshotByPeriod (period: string) {
+      if (Object.keys(this.groupPeriodPayments).includes(period)) {
+        return this.groupPeriodPayments[period].haveNeedsSnapshot || []
+      }
+
+      const paymentsByPeriodKey = `paymentsByPeriod/${this.ourUsername}/${this.currentGroupId}`
+      const paymentsByPeriod = await sbp('gi.db/archive/load', paymentsByPeriodKey) || {}
+      return Object.keys(paymentsByPeriod).includes(period)
+        ? paymentsByPeriod[period].haveNeedsSnapshot || []
+        : []
+    },
+    async getTotalTodoAmountForPeriod (period: string) {
+      const haveNeeds = await this.getHaveNeedsSnapshotByPeriod(period)
+      let total = 0
+
+      for (const { haveNeed } of haveNeeds) {
+        if (haveNeed < 0) { total += -1 * haveNeed }
+      }
+      return total
+    },
+    async getTotalPledgesDoneForPeriod (period: string) {
+      const payments = await this.getPaymentsByPeriod(period)
+      let total = 0
+
+      for (const { amount } of payments) {
+        total += amount
+      }
+      return total
+    },
+    async getPeriodPayment (period: string) {
+      if (Object.keys(this.groupPeriodPayments).includes(period)) {
+        return this.groupPeriodPayments[period] || {}
+      }
+
+      const archPaymentsByPeriodKey = `paymentsByPeriod/${this.ourUsername}/${this.currentGroupId}`
+      const archPaymentsByPeriod = await sbp('gi.db/archive/load', archPaymentsByPeriodKey) || {}
+      return archPaymentsByPeriod[period] || {}
+    },
+    getPeriodFromStartToDueDate (period) {
+      const dueDate = this.dueDateForPeriod(period)
+      return `${humanDate(dateFromPeriodStamp(period))} - ${humanDate(dateFromPeriodStamp(dueDate))}`
     }
   }
 }
