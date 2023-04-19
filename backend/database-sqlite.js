21ac3e07edf672b972e052a996bf8113c4ba24e4
snowteamer <64228468+snowteamer@users.noreply.github.com>
2023-04-20 00:11:09 +0200

    Re-add SQLite support (#1527)
    * Add sqlite support
    
    * Apply review feedback
    
    * Remove unnecessary try statements
    
    * Use readFile and writeFile from fs/promises
    
    * Fix an inconsistency and some Flow typings
    
    * Make 'chelonia/db/set' return Promise<void>
    
    * Add storage backend unit tests
    
    * Use prepared statements for performance
    
    * Update .travis.yml to use Ubuntu Xenial
    
    * fixup! Use prepared statements for performance
    
    * Use path.join in readString
    
    * Drop 'backend/db/readFile' and '-writeFile'
    
    * Rename to x-backend.js files to database-x.js
    
    * Update Node to v18 LTS
    
    * Store uploads in Chelonia DB
    
    * Validate args in 'chelonia/db/set'
    
    * Bump Ubuntu to v20 LTS in Travis
    
    * Fix Flow errors
    
    * Remove initDB() call in backend.test.js
    
    * Add DB import and export functions
    
    * Remove 'GET contract/' route
    
    * Use primary key
    
    * Improve Chelonia DB preloading
    
    * Disallow unprintable characters in keys
    
    * Use sequential preloading and skip avatars

---
+++ backend/database-sqlite.js
@@ -0,0 +1,108 @@
+'use strict'
+
+import { mkdir } from 'node:fs/promises'
+import { join, resolve } from 'node:path'
+import sqlite3 from 'sqlite3'
+
+import { checkKey } from './database.js'
+
+let db: any = null
+let readStatement: any = null
+let writeStatement: any = null
+
+const run = (sql, args) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, args, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+export async function initStorage (options: Object = {}): Promise<void> {
+  const { dirname, filename } = options
+  const dataFolder = resolve(dirname)
+
+  await mkdir(dataFolder, { mode: 0o750, recursive: true })
+
+  await new Promise((resolve, reject) => {
+    if (db) {
+      reject(new Error(`The ${filename} SQLite database is already open.`))
+    }
+    db = new sqlite3.Database(join(dataFolder, filename), (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+  await run('CREATE TABLE IF NOT EXISTS Data(key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)')
+  console.log('Connected to the %s SQLite database.', filename)
+  readStatement = db.prepare('SELECT value FROM Data WHERE key = ?')
+  writeStatement = db.prepare('REPLACE INTO Data(key, value) VALUES(?, ?)')
+}
+
+// Useful in test hooks.
+export function clear (): Promise<void> {
+  return run('DELETE FROM Data')
+}
+
+export function exportToJSON (): Promise<Object> {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT key, value FROM Data', [], (err, rows) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(Object.fromEntries(rows.map(row => ([row.key, row.value]))))
+      }
+    })
+  })
+}
+
+export function importFromJSON (json: Object): Promise<void> {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run('BEGIN')
+      Object.keys(json).forEach(key => writeData(key, json[key]))
+      db.run('COMMIT', [], (err) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    })
+  })
+}
+
+export function readData (key: string): Promise<Buffer | string | void> {
+  return new Promise((resolve, reject) => {
+    checkKey(key)
+    readStatement.get([key], (err, row) => {
+      if (err) {
+        reject(err)
+      } else {
+        // Note: sqlite remembers the type of every stored value, therefore we
+        // automatically get back the same JS value that has been inserted.
+        resolve(row?.value)
+      }
+    })
+  })
+}
+
+export function writeData (key: string, value: Buffer | string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    checkKey(key)
+    writeStatement.run([key, value], (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
