a860fb0822db9ceeffc74706ede77fdd0bd29e9d
SebinSong <songxevin@gmail.com>
2023-05-11 12:52:36 +1200

    create a skeleton component CreatePoll.vue

---
+++ frontend/views/containers/chatroom/CreatePoll.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.c-create-poll(:class='{ "is-active": ephemeral.isActive }')
+  .c-create-poll-wrapper
+</template>
+
+<script>
+export default {
+  name: 'CreatePoll',
+  data () {
+    return {
+      ephemeral: {
+        isActive: false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+
+.c-create-poll {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 0;
+  pointer-events: none;
+  overflow: hidden;
+  z-index: $zindex-tooltip;
+
+  &.is-active {
+    pointer-events: initial;
+    height: 100%;
+  }
+}
+
+.c-create-poll-wrapper {
+  position: absolute;
+  pointer-events: inherit;
+}
+</style>
