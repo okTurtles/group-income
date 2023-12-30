3e1fe790b2f595ac8a3b9e3041ebbb3f80781b3f
snowteamer <64228468+snowteamer@users.noreply.github.com>
2023-12-30 09:16:11 +0100

    Remove shared/logger.js

--- shared/logger.js
+++
@@ -1,7 +0,0 @@
-export default (tag) => ({
-  log: console.log.bind(console, tag),
-  debug: console.debug.bind(console, tag),
-  error: console.error.bind(console, tag),
-  info: console.debug.bind(console, tag),
-  warn: console.debug.bind(console, tag)
-})
