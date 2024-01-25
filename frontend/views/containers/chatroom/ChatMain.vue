9d93819692ac14c4d23f186de68d67b14d25c5b6
SebinSong <songxevin@gmail.com>
2024-01-25 18:06:57 +0900

    increase the top-padding of the message-body area

--- frontend/views/containers/chatroom/ChatMain.vue
+++ frontend/views/containers/chatroom/ChatMain.vue
@@ -1012,7 +1012,7 @@ export default ({
 
 .c-body-conversation {
   margin-right: 1rem;
-  padding: 2rem 0 1rem 0;
+  padding: 10rem 0 1rem 0;
   overflow: hidden auto;
   -webkit-overflow-scrolling: touch;
 }
