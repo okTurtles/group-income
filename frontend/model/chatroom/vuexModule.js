7d24ef79328495d0f0092e5caddf6a29a1ac63de
SebinSong <songxevin@gmail.com>
2024-02-15 13:05:14 +1300

    add chatroom/vuexModule.js with some boilerplate code in there

---
+++ frontend/model/chatroom/vuexModule.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import { cloneDeep } from '@model/contracts/shared/giLodash.js'
+
+const defaultState = {
+  currentChatRoomIDs: {}, // { [groupId]: currentChatRoomId }
+  chatRoomScrollPosition: {}, // [chatRoomId]: messageHash
+  chatRoomUnread: {}, // [chatRoomId]: { readUntil: { messageHash, createdDate }, messages: [{ messageHash, createdDate, type, deletedDate? }]}
+  chatNotificationSettings: {}, // { messageNotification: MESSAGE_NOTIFY_SETTINGS, messageSound: MESSAGE_NOTIFY_SETTINGS }
+}
+
+// getters
+const getters = {}
+
+export default ({
+  state: () => cloneDeep(defaultState),
+  getters
+}: Object)
