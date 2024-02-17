91a8b73e58a1b35dd476f547054de25df09f4a98
SebinSong <songxevin@gmail.com>
2024-02-18 12:37:36 +1300

    fix a linter error

--- frontend/model/chatroom/vuexModule.js
+++ frontend/model/chatroom/vuexModule.js
@@ -6,7 +6,7 @@ const defaultState = {
   currentChatRoomIDs: {}, // { [groupId]: currentChatRoomId }
   chatRoomScrollPosition: {}, // [chatRoomId]: messageHash
   chatRoomUnread: {}, // [chatRoomId]: { readUntil: { messageHash, createdDate }, messages: [{ messageHash, createdDate, type, deletedDate? }]}
-  chatNotificationSettings: {}, // { messageNotification: MESSAGE_NOTIFY_SETTINGS, messageSound: MESSAGE_NOTIFY_SETTINGS }
+  chatNotificationSettings: {} // { messageNotification: MESSAGE_NOTIFY_SETTINGS, messageSound: MESSAGE_NOTIFY_SETTINGS }
 }
 
 // getters
