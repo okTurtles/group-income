2044db5e3f313e009b233a1ee8abb335c10354a5
SebinSong <songxevin@gmail.com>
2024-02-18 14:06:45 +1300

    move chat&DM related getters in state.js to the chatroom vuexModule

--- frontend/model/chatroom/vuexModule.js
+++ frontend/model/chatroom/vuexModule.js
@@ -1,7 +1,7 @@
 'use strict'
 
-import { cloneDeep } from '@model/contracts/shared/giLodash.js'
-
+import { merge, cloneDeep, union } from '@model/contracts/shared/giLodash.js'
+import { MESSAGE_NOTIFY_SETTINGS, MESSAGE_TYPES } from '@model/contracts/shared/constants.js'
 const defaultState = {
   currentChatRoomIDs: {}, // { [groupId]: currentChatRoomId }
   chatRoomScrollPosition: {}, // [chatRoomId]: messageHash
@@ -10,9 +10,153 @@ const defaultState = {
 }
 
 // getters
-const getters = {}
+const getters = {
+  currentChatRoomId (state, getters, rootState) {
+    return state.currentChatRoomIDs[rootState.currentGroupId] || null
+  },
+  currentChatRoomState (state, getters, rootState) {
+    return rootState[getters.currentChatRoomId] || {} // avoid "undefined" vue errors at inoportune times
+  },
+  chatNotificationSettings (state) {
+    return Object.assign({
+      default: {
+        messageNotification: MESSAGE_NOTIFY_SETTINGS.DIRECT_MESSAGES,
+        messageSound: MESSAGE_NOTIFY_SETTINGS.DIRECT_MESSAGES
+      }
+    }, state.chatNotificationSettings || {})
+  },
+  ourGroupDirectMessages (state, getters, rootState) {
+    const currentGroupDirectMessages = {}
+    for (const chatRoomId of Object.keys(getters.ourDirectMessages)) {
+      const chatRoomState = rootState[chatRoomId]
+      const directMessageSettings = getters.ourDirectMessages[chatRoomId]
+
+      // NOTE: skip DMs whose chatroom contracts are not synced yet
+      if (!chatRoomState || !chatRoomState.members?.[getters.ourIdentityContractId]) {
+        continue
+      }
+      // NOTE: direct messages should be filtered to the ones which are visible and of active group members
+      const members = Object.keys(chatRoomState.members)
+      const partners = members
+        .filter(memberID => memberID !== getters.ourIdentityContractId)
+        .sort((p1, p2) => {
+          const p1JoinedDate = new Date(chatRoomState.members[p1].joinedDate).getTime()
+          const p2JoinedDate = new Date(chatRoomState.members[p2].joinedDate).getTime()
+          return p1JoinedDate - p2JoinedDate
+        })
+      const hasActiveMember = partners.some(memberID => Object.keys(getters.groupProfiles).includes(memberID))
+      if (directMessageSettings.visible && hasActiveMember) {
+        // NOTE: lastJoinedParter is chatroom member who has joined the chatroom for the last time.
+        //       His profile picture can be used as the picture of the direct message
+        //       possibly with the badge of the number of partners.
+        const lastJoinedPartner = partners[partners.length - 1]
+        currentGroupDirectMessages[chatRoomId] = {
+          ...directMessageSettings,
+          members,
+          partners,
+          lastJoinedPartner,
+          // TODO: The UI should display display names, usernames and (in the future)
+          // identity contract IDs differently in some way (e.g., font, font size,
+          // prefix (@), etc.) to make it impossible (or at least obvious) to impersonate
+          // users (e.g., 'user1' changing their display name to 'user2')
+          title: partners.map(cID => getters.userDisplayNameFromID(cID)).join(', '),
+          picture: getters.ourContactProfiles[lastJoinedPartner]?.picture
+        }
+      }
+    }
+    return currentGroupDirectMessages
+  },
+  // NOTE: this getter is used to find the ID of the direct message in the current group
+  //       with the name[s] of partner[s]. Normally it's more useful to find direct message
+  //       by the partners instead of contractID
+  ourGroupDirectMessageFromUserIds (state, getters) {
+    return (partners) => { // NOTE: string | string[]
+      if (typeof partners === 'string') {
+        partners = [partners]
+      }
+      const currentGroupDirectMessages = getters.ourGroupDirectMessages
+      return Object.keys(currentGroupDirectMessages).find(chatRoomId => {
+        const cPartners = currentGroupDirectMessages[chatRoomId].partners
+        return cPartners.length === partners.length && union(cPartners, partners).length === partners.length
+      })
+    }
+  },
+  isDirectMessage (state, getters) {
+    // NOTE: identity contract could not be synced at the time of calling this getter
+    return chatRoomId => !!getters.ourGroupDirectMessages[chatRoomId || getters.currentChatRoomId]
+  },
+  isJoinedChatRoom (state, getters) {
+    return (chatRoomId: string, memberID?: string) => !!state[chatRoomId]?.members?.[memberID || getters.ourIdentityContractId]
+  },
+  currentChatVm (state, getters) {
+    return state?.[getters.currentChatRoomId]?._vm || null
+  },
+  currentChatRoomScrollPosition (state, getters) {
+    return state.chatRoomScrollPosition[getters.currentChatRoomId] // undefined means to the latest
+  },
+  ourUnreadMessages (state, getters) {
+    return state.chatRoomUnread
+  },
+  currentChatRoomReadUntil (state, getters) {
+    // NOTE: Optional Chaining (?) is necessary when user viewing the chatroom which he is not part of
+    return getters.ourUnreadMessages[getters.currentChatRoomId]?.readUntil // undefined means to the latest
+  },
+  chatRoomUnreadMessages (state, getters) {
+    return (chatRoomId: string) => {
+      // NOTE: Optional Chaining (?) is necessary when user tries to get mentions of the chatroom which he is not part of
+      return getters.ourUnreadMessages[chatRoomId]?.messages || []
+    }
+  },
+  chatRoomUnreadMentions (state, getters) {
+    return (chatRoomId: string) => {
+      // NOTE: Optional Chaining (?) is necessary when user tries to get mentions of the chatroom which he is not part of
+      return (getters.ourUnreadMessages[chatRoomId]?.messages || []).filter(m => m.type === MESSAGE_TYPES.TEXT)
+    }
+  },
+  groupUnreadMessages (state, getters, rootState) {
+    return (groupID: string) => Object.keys(getters.ourUnreadMessages)
+      .filter(cID => getters.isDirectMessage(cID) || Object.keys(rootState[groupID]?.chatRooms || {}).includes(cID))
+      .map(cID => getters.ourUnreadMessages[cID].messages.length)
+      .reduce((sum, n) => sum + n, 0)
+  },
+  groupIdFromChatRoomId (state, getters, rootState) {
+    return (chatRoomId: string) => Object.keys(rootState.contracts)
+      .find(cId => rootState.contracts[cId].type === 'gi.contracts/group' &&
+        Object.keys(rootState[cId].chatRooms).includes(chatRoomId))
+  },
+  chatRoomsInDetail (state, getters, rootState) {
+    const chatRoomsInDetail = merge({}, getters.getGroupChatRooms)
+    for (const contractID in chatRoomsInDetail) {
+      const chatRoom = rootState[contractID]
+      if (chatRoom && chatRoom.attributes &&
+        chatRoom.members[rootState.loggedIn.identityContractID]) {
+        chatRoomsInDetail[contractID] = {
+          ...chatRoom.attributes,
+          id: contractID,
+          unreadMessagesCount: getters.chatRoomUnreadMessages(contractID).length,
+          joined: true
+        }
+      } else {
+        const { name, privacyLevel } = chatRoomsInDetail[contractID]
+        chatRoomsInDetail[contractID] = { id: contractID, name, privacyLevel, joined: false }
+      }
+    }
+    return chatRoomsInDetail
+  },
+  chatRoomMembersInSort (state, getters) {
+    return getters.groupMembersSorted
+      .map(member => ({ contractID: member.contractID, username: member.username, displayName: member.displayName }))
+      .filter(member => !!getters.chatRoomMembers[member.contractID]) || []
+  }
+}
+
+// mutations
+const mutations = {
+
+}
 
 export default ({
   state: () => cloneDeep(defaultState),
-  getters
+  getters,
+  mutations
 }: Object)
