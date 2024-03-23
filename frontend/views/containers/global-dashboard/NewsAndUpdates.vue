42f4a29df00a7dcf02a5770f821bb3247fea983e
SebinSong <songxevin@gmail.com>
2024-03-23 14:58:30 +1300

    configure GlobalDashboard.vue to update the content based on the route path

---
+++ frontend/views/containers/global-dashboard/NewsAndUpdates.vue
@@ -0,0 +1,14 @@
+<template lang='pug'>
+.c-news-and-updates-container
+  i18n(tag='p') News And Updates: Coming soon!
+</template>
+
+<script>
+export default ({
+  name: 'NewAndUpdates'
+}: Object)
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+</style>
--- frontend/views/pages/GlobalDashboard.vue
+++ frontend/views/pages/GlobalDashboard.vue
@@ -4,13 +4,20 @@ page(
   pageTestHeaderName='pageHeaderName'
 )
   template(#title='') {{ currentTabSetting.title }}
-  .c-global-dashboard-content
-    i18n(tag='p') Coming soon!
+
+  component(:is='currentContent' :key='$route.params.id')
 </template>
 
 <script>
 import Page from '@components/Page.vue'
+import NewsAndUpdates from '@containers/global-dashboard/NewsAndUpdates.vue'
+import DirectMessages from '@containers/global-dashboard/DirectMessages.vue'
 import { GLOBAL_DASHBOARD_SETTINGS } from '@controller/utils/misc.js'
+
+const contentComponentsMap = {
+  'news-and-updates': NewsAndUpdates,
+  'direct-messages': DirectMessages
+}
 
 export default ({
   name: 'GlobalDashboard',
@@ -20,9 +27,12 @@ export default ({
   computed: {
     currentTabSetting () {
       return GLOBAL_DASHBOARD_SETTINGS[this.$route.params.id || 'news-and-updates']
+    },
+    currentContent () {
+      return contentComponentsMap[this.$route.params.id || 'news-and-updates']
     }
   }
-})
+}: Object)
 </script>
 
 <style lang="scss" scoped>
