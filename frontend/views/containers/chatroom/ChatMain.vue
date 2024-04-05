7f59cb4086605e606e7ce913f9b67a5cf3afdca0
Alex Jin <57976479+Silver-IT@users.noreply.github.com>
2024-04-05 16:51:19 +0800

    feat: set skeleton size of image attachments (#1906)

--- frontend/views/containers/chatroom/ChatMain.vue
+++ frontend/views/containers/chatroom/ChatMain.vue
@@ -135,7 +135,7 @@ import {
 import { CHATROOM_EVENTS } from '@utils/events.js'
 import { findMessageIdx, createMessage } from '@model/contracts/shared/functions.js'
 import { proximityDate, MINS_MILLIS } from '@model/contracts/shared/time.js'
-import { cloneDeep, debounce, throttle } from '@model/contracts/shared/giLodash.js'
+import { omit, cloneDeep, debounce, throttle } from '@model/contracts/shared/giLodash.js'
 import { EVENT_HANDLED } from '~/shared/domains/chelonia/events.js'
 import { objectURLtoBlob } from '@utils/image.js'
 
@@ -473,13 +473,13 @@ export default ({
       const uploadAttachments = async () => {
         try {
           const attachmentsToSend = await Promise.all(attachments.map(async (attachment) => {
-            const { mimeType, url, name } = attachment
+            const { mimeType, url } = attachment
             // url here is an instance of URL.createObjectURL(), which needs to be converted to a 'Blob'
             const attachmentBlob = await objectURLtoBlob(url)
             const { download: downloadData } = await sbp('chelonia/fileUpload', attachmentBlob, {
               type: mimeType, cipher: 'aes256gcm'
             }, { billableContractID: contractID })
-            return { name, mimeType, downloadData }
+            return { ...omit(attachment, ['url']), downloadData }
           }))
           data.attachments = attachmentsToSend
 
