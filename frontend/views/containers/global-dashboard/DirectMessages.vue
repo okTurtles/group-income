61c6a9f066ee742b7ae2b8834547465b2610bc18
SebinSong <songxevin@gmail.com>
2024-04-20 15:25:41 +1200

    write intial logic for loading all DMs of a user

--- frontend/views/containers/global-dashboard/DirectMessages.vue
+++ frontend/views/containers/global-dashboard/DirectMessages.vue
@@ -2,6 +2,7 @@
 .c-news-and-updates-container
   .card.c-search-input-card
     search.c-search(
+      :label='config.searchLabel'
       :placeholder='config.searchPlaceholder'
       v-model='form.search'
     )
@@ -12,11 +13,26 @@
 </template>
 
 <script>
+import sbp from '@sbp/sbp'
+import { mapGetters } from 'vuex'
 import { L } from '@common/common.js'
 import Search from '@components/Search.vue'
+import { CHATROOM_ACTIONS_PER_PAGE } from '@model/contracts/shared/constants.js'
+// DEV_NOTE - fetching DM message data
+// 1. refer to 'loadMessagesFromStorage' method in ChatMain.vue
+// 2. L805
 
-// DEV_NOTE:
-// fetching DM message data - refer to 'loadMessagesFromStorage' method in ChatMain.vue
+const collectEventStream = async (s: ReadableStream) => {
+  const reader = s.getReader()
+  const r = []
+  for (;;) {
+    const { done, value } = await reader.read()
+    if (done) break
+    r.push(value)
+  }
+  return r
+}
+
 export default ({
   name: 'DirectMessages',
   components: {
@@ -28,9 +44,41 @@ export default ({
         search: ''
       },
       config: {
-        searchPlaceholder: L('Find a DM')
+        searchPlaceholder: L('Find a DM'),
+        searchLabel: L('Search a DM')
       }
     }
+  },
+  computed: {
+    ...mapGetters([
+      'ourDirectMessages',
+      'ourIdentityContractId'
+    ])
+  },
+  methods: {
+    async loadDMs () {
+      const chatroomIds = Object.keys(this.ourDirectMessages)
+        .filter(id => this.ourDirectMessages[id].visible)
+      const data = await Promise.all(chatroomIds.map(this.getEventsByChatroomId))
+
+      console.log('All DM data: ', data)
+    },
+    async getEventsByChatroomId (chatroomId) {
+      const storageKeyFromChatroomId = (id) => `messages/${this.ourIdentityContractId}/${id}`
+      const eventsFromStorage = await sbp('gi.db/archive/load', storageKeyFromChatroomId(chatroomId))
+
+      if (eventsFromStorage) {
+        return JSON.parse(eventsFromStorage)
+      } else {
+        const { height: latestHeight } = await sbp('chelonia/out/latestHEADInfo', chatroomId)
+        const events = await collectEventStream(sbp('chelonia/out/eventsBefore', chatroomId, latestHeight, CHATROOM_ACTIONS_PER_PAGE))
+
+        return events
+      }
+    }
+  },
+  created () {
+    this.loadDMs()
   }
 }: Object)
 </script>
