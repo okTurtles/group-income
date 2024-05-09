486f9ac0b0d6ce8ef196ad0bba264987e6842e54
silver-it <alex.jin220@gmail.com>
2024-05-09 09:13:21 +0800

    feat: display profile card when click user in messages

--- frontend/views/containers/chatroom/MessageNotification.vue
+++ frontend/views/containers/chatroom/MessageNotification.vue
@@ -24,6 +24,7 @@ export default ({
     id: String,
     messageHash: String,
     type: String,
+    from: String,
     text: String,
     notification: Object, // { type, params }
     who: String,
