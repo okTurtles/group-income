810ddf0c3a53adceaddf78a11c365152e9495866
Alex Jin <57976479+Silver-IT@users.noreply.github.com>
2024-05-21 00:37:20 +0800

    Fix proposal issues (#1986)
    * chore: do not open new tab when user is signed in
    
    * feat: added displayName in proposals and notifications
    
    * fix: error in wrong icon for MINCOME_CHANGED notification
    
    * feat: make one proposal when the proposal is accepted
    
    * feat: make proposal after proposal is closed
    
    * chore: do not open new tab when user is signed in
    
    * feat: updated function name to have precise meaning
    
    * fix: errors in notification options
    
    * chore: do not open new tab when user is signed in
    
    * chore: revert unrelated changes
    
    * chore: removed console.log
    
    * chore: removed blank line
    
    * chore: added comment for better understanding
    
    * chore: updated variable names
    
    * chore: do not open new tab when user is signed in
    
    * chore: updated message interactive templates and cypress retry
    
    * feat: utility function to emit notification after syncing contracts
    
    * chore: do not open new tab when user is signed in
    
    * chore: reverted change which is unrelated to this Pr
    
    * use 'chelonia/contract/wait' instead of queueInvocation + noop
    
    ---------
    
    Co-authored-by: Greg Slepak <contact@taoeffect.com>

--- frontend/views/containers/chatroom/ChatMain.vue
+++ frontend/views/containers/chatroom/ChatMain.vue
@@ -313,7 +313,6 @@ export default ({
       'isDirectMessage',
       'currentChatRoomScrollPosition',
       'currentChatRoomReadUntil',
-      'currentGroupNotifications',
       'currentChatVm',
       'chatRoomUnreadMentions'
     ]),
