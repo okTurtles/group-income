ecb063c3439fde863620348888e36ccf1bfe0ec7
silver-it <alex.jin220@gmail.com>
2024-06-12 17:02:36 +0800

    chore: add comment and Travis retry

--- frontend/views/containers/chatroom/ChatMain.vue
+++ frontend/views/containers/chatroom/ChatMain.vue
@@ -884,6 +884,7 @@ export default ({
       const chatRoomID = this.renderingChatRoomId
       if (chatRoomID && this.isJoinedChatRoom(chatRoomID)) {
         if (this.currentChatRoomReadUntil?.createdHeight >= createdHeight) {
+          // NOTE: skip adding useless invocations in UNREAD_MESSAGES_QUEUE queue
           return
         }
         sbp('gi.actions/identity/setChatRoomReadUntil', {
