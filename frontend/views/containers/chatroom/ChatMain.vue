okTurtles/group-income

commit 51e0985841e0276c3788f4ace7b06a3894ae4b26

Only update readUntilMessageHash for non-pending messages (#2123)
* Only update readUntilMessageHash for non-pending messages

Fix #2119

* Fix #2104

* Correct pending check

frontend/views/containers/chatroom/ChatMain.vue
@@ -181,7 +181,7 @@ const onChatScroll = function () {
     if (offsetTop + height <= curScrollBottom) {
       const bottomMessageCreatedHeight = msg.height
       const latestMessageCreatedHeight = this.currentChatRoomReadUntil?.createdHeight
-      if (!latestMessageCreatedHeight || latestMessageCreatedHeight <= bottomMessageCreatedHeight) {
+      if ((!latestMessageCreatedHeight || latestMessageCreatedHeight <= bottomMessageCreatedHeight) && !msg.pending) {
         this.updateReadUntilMessageHash({
           messageHash: msg.hash,
           createdHeight: msg.height
@@ -998,11 +998,13 @@ export default ({
               if (!fromOurselves && isScrollable) {
                 this.updateScroll()
               } else {
-                const msg = this.messages[this.messages.length - 1]
-                this.updateReadUntilMessageHash({
-                  messageHash: msg.hash,
-                  createdHeight: msg.height
-                })
+                const msg = this.messages.filter(m => !m.pending).slice(-1)
+                if (msg) {
+                  this.updateReadUntilMessageHash({
+                    messageHash: msg.hash,
+                    createdHeight: msg.height
+                  })
+                }
               }
             }
           }
@@ -1047,7 +1049,7 @@ export default ({
             $state.complete()
             if (!this.$refs.conversation ||
             this.$refs.conversation.scrollHeight === this.$refs.conversation.clientHeight) {
-              const msg = this.messages[this.messages.length - 1]
+              const msg = this.messages.filter(m => !m.pending).slice(-1)
               if (msg) {
                 this.updateReadUntilMessageHash({
                   messageHash: msg.hash,

frontend/common/translations.js
@@ -135,7 +135,7 @@ export function LError (error: Error): {|reportError: any|} {
   let url = 'https://github.com/okTurtles/group-income/issues'
   if (sbp('state/vuex/state').loggedIn) {
     const baseRoute = document.location.origin + sbp('controller/router').options.base
-    url = `${baseRoute}?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURI(error.message)}`
+    url = `${baseRoute}?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURIComponent(error.message)}`
   }
   return {
     reportError: L('"{errorMsg}". You can {a_}report the error{_a}.', {

frontend/model/notifications/templates.js
@@ -46,7 +46,7 @@ export default ({
       }),
       icon: 'exclamation-triangle',
       level: 'danger',
-      linkTo: `/app/dashboard?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURI(error.message)}`,
+      linkTo: `/app/dashboard?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURIComponent(error.message)}`,
       scope: 'app'
     }
   },
@@ -73,7 +73,7 @@ export default ({
       body: data.message,
       icon: 'exclamation-triangle',
       level: 'danger',
-      linkTo: `/app/dashboard?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURI(data.message)}`,
+      linkTo: `/app/dashboard?modal=UserSettingsModal&tab=application-logs&errorMsg=${encodeURIComponent(data.message)}`,
       scope: 'app'
     }
   },

commit 0516b454cf6bfc90ad0463fc07b95b9efd37255b

Fix regressions introduced in PR #2123

frontend/views/containers/chatroom/ChatMain.vue
@@ -181,7 +181,8 @@ const onChatScroll = function () {
     if (offsetTop + height <= curScrollBottom) {
       const bottomMessageCreatedHeight = msg.height
       const latestMessageCreatedHeight = this.currentChatRoomReadUntil?.createdHeight
-      if ((!latestMessageCreatedHeight || latestMessageCreatedHeight <= bottomMessageCreatedHeight) && !msg.pending) {
+      // No need to check for pending here as it's checked above
+      if (!latestMessageCreatedHeight || latestMessageCreatedHeight <= bottomMessageCreatedHeight) {
         this.updateReadUntilMessageHash({
           messageHash: msg.hash,
           createdHeight: msg.height
@@ -998,7 +999,9 @@ export default ({
               if (!fromOurselves && isScrollable) {
                 this.updateScroll()
               } else {
-                const msg = this.messages.filter(m => !m.pending).slice(-1)
+                // If there are any temporary messages that do not exist in the
+                // contract, they should not used for updateReadUntilMessageHash
+                const msg = this.messages.filter(m => !m.pending && !m.hasFailed).pop()
                 if (msg) {
                   this.updateReadUntilMessageHash({
                     messageHash: msg.hash,
@@ -1049,7 +1052,9 @@ export default ({
             $state.complete()
             if (!this.$refs.conversation ||
             this.$refs.conversation.scrollHeight === this.$refs.conversation.clientHeight) {
-              const msg = this.messages.filter(m => !m.pending).slice(-1)
+              // updateReadUntilMessageHash should only use messages that exist
+              // in the contract
+              const msg = this.messages.filter(m => !m.pending && !m.hasFailed).pop()
               if (msg) {
                 this.updateReadUntilMessageHash({
                   messageHash: msg.hash,