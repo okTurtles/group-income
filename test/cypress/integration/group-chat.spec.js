45c6925572aa86136085b8e89e5b21547c6d8267
silver-it <alex.jin220@gmail.com>
2024-07-08 20:38:19 +0800

    chore: enabled all test cases for group-chat spec

--- test/cypress/integration/group-chat.spec.js
+++ test/cypress/integration/group-chat.spec.js
@@ -459,7 +459,7 @@ describe('Group Chat Basic Features (Create & Join & Leave & Close)', () => {
 
   // TODO: can not rejoin the group by himself unless he uses the link made by proposal
   // so the scenario could be updated later when e2e protocol would be ready
-  it.skip(`user3 joins the ${groupName1} group and ${CHATROOM_GENERAL_NAME} channel again`, () => {
+  it(`user3 joins the ${groupName1} group and ${CHATROOM_GENERAL_NAME} channel again`, () => {
     cy.giLogin(user3, {
       bypassUI: true,
       toGroupDashboardUponSuccess: false // user-3 has no group to this account at this point.
@@ -483,7 +483,7 @@ describe('Group Chat Basic Features (Create & Join & Leave & Close)', () => {
     cy.giLogout()
   })
 
-  it.skip(`user2 joins the ${groupName1} group and ${CHATROOM_GENERAL_NAME} again and logout`, () => {
+  it(`user2 joins the ${groupName1} group and ${CHATROOM_GENERAL_NAME} again and logout`, () => {
     cy.giLogin(user2, { bypassUI: true, toGroupDashboardUponSuccess: false })
     me = user2
 
