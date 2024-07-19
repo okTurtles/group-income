8ddde8d3f4aea8e5a5cdf55b368b3490ef88df83
silver-it <alex.jin220@gmail.com>
2024-07-19 15:39:06 +0800

    chore: tiny update and Cypress retry

--- test/cypress/integration/group-paying.spec.js
+++ test/cypress/integration/group-paying.spec.js
@@ -55,7 +55,7 @@ function assertMonthOverview (items) {
 }
 
 function openNotificationCard ({
-  notificationsCount = 0, // 0 means not to check notifications count
+  notificationsCount = 0, // 0 means not to check number of notifications
   messageToAssert = '',
   clickItem = true
 } = {}) {
