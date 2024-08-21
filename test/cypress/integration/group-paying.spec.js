25ae13be1590a1a0c10be99511f1fbb361a7bcef
SebinSong <songxevin@gmail.com>
2024-08-21 18:25:32 +1200

    comment update

--- test/cypress/integration/group-paying.spec.js
+++ test/cypress/integration/group-paying.spec.js
@@ -326,7 +326,7 @@ describe('Group Payments', () => {
     cy.log('user1 receives a notification for a thank you note')
     cy.giSwitchUser(`user1-${userId}`)
     openNotificationCard({
-      // no need to check the exact number of notifications but do check if the list contains the item for thank-you message.
+      // no need to check the exact number of notifications but do check if the list contains the item for thank-you message
       notificationsCount: 0,
       messageToAssert: `user3-${userId} sent you a thank you note for your contribution.`
     })
