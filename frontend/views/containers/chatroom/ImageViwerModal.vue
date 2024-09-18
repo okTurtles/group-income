96dae8e7efb7c175f71209bc28a60d64e94150a6
SebinSong <songxevin@gmail.com>
2024-09-19 10:13:14 +1200

    add blurry bg / add modal-btn / implement onLoad handler

--- frontend/views/containers/chatroom/ImageViwerModal.vue
+++ frontend/views/containers/chatroom/ImageViwerModal.vue
@@ -1,7 +1,19 @@
 <template lang="pug">
 .c-image-viewer-modal
-  p Content placeholder
-  button(type='button' @click='close') Close
+  .c-image-viewer-content
+    button.is-icon-small.c-close-btn(
+      type='button'
+      @click.stop='close'
+    )
+      i.icon-times
+
+    .c-image-blurry-background(:style='blurryBgStyles')
+    .c-image-view-area
+      img.c-preview-image(ref='previewImg'
+        :src='testImgSrc'
+        v-bind='ephemeral.previewImgAttrs'
+        @load='onImgLoad')
+    p Content placeholder
 </template>
 
 <script>
@@ -13,6 +25,24 @@ export default {
   // NOTE: gave this component a generic name in case this is used outside the chatroom area. (eg. instead of 'ChatImageViewer' etc.)
   name: 'ImageViewerModal',
   mixins: [trapFocus],
+  data () {
+    return {
+      testImgSrc: '/assets/images/home-bg.jpeg',
+      ephemeral: {
+        previewImgAttrs: {
+          width: undefined,
+          height: undefined
+        }
+      }
+    }
+  },
+  computed: {
+    blurryBgStyles () {
+      return {
+        backgroundImage: `url(${this.testImgSrc})`
+      }
+    }
+  },
   mounted () {
     document.addEventListener('keydown', this.trapFocus)
   },
@@ -20,6 +50,11 @@ export default {
     document.removeEventListener('keydown', this.trapFocus)
   },
   methods: {
+    onImgLoad () {
+      const imgEl = this.$refs.previewImg
+      this.ephemeral.previewImgAttrs.width = imgEl.naturalWidth
+      this.ephemeral.previewImgAttrs.height = imgEl.naturalHeight
+    },
     close () {
       sbp('okTurtles.events/emit', CLOSE_MODAL, 'ImageViewerModal')
     }
@@ -31,7 +66,6 @@ export default {
 @import "@assets/style/_variables.scss";
 
 .c-image-viewer-modal {
-  display: flex;
   position: fixed;
   z-index: $zindex-modal;
   top: 0;
@@ -39,6 +73,54 @@ export default {
   bottom: 0;
   right: 0;
   overflow: auto;
-  background-color: $general_0;
+  background-color: rgba(10, 10, 10, 0.86);
+}
+
+.c-image-viewer-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: $general_1;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  @include from($tablet) {
+    border-radius: 0.375rem;
+    width: 92.5vw;
+    height: 90vh;
+  }
+}
+
+.c-close-btn {
+  position: absolute;
+  z-index: 2;
+  right: 0.75rem;
+  top: 0.75rem;
+}
+
+.c-image-view-area {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+img.c-preview-image {
+  max-width: unset;
+}
+
+.c-image-blurry-background {
+  position: absolute;
+  pointer-events: none;
+  filter: blur(40px) brightness(0.4);
+  background-position: 50%;
+  background-size: cover;
+  inset: -100px;
 }
 </style>
