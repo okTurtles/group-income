f8ec52b2ab00be25a3549b100b6986de538aadb0
Sebin Song <songxevin@gmail.com>
2024-10-29 07:13:53 +1300

    #2392 - No need to send a notification when the chatroom is open (#2394)
    * initial fix
    
    * add comment

--- frontend/controller/actions/chatroom.js
+++ frontend/controller/actions/chatroom.js
@@ -24,6 +24,9 @@ sbp('okTurtles.events/on', MESSAGE_RECEIVE_RAW, ({
   const getters = sbp('state/vuex/getters')
   const rootState = sbp('state/vuex/state')
   const targetChatroomState = rootState[contractID]
+  const currentRoute = sbp('controller/router').history.current || ''
+  const isTargetChatroomCurrentlyActive = currentRoute.path.includes('/group-chat') &&
+    getters.currentChatRoomId === contractID // when the target chatroom is currently open/active on the browser, No need to send a notification.
   const mentions = makeMentionFromUserID(getters.ourIdentityContractId)
   const msgData = newMessage || data
   const isMentionedMe = (!!newMessage || data.type === MESSAGE_TYPES.TEXT) && msgData.text &&
@@ -39,7 +42,7 @@ sbp('okTurtles.events/on', MESSAGE_RECEIVE_RAW, ({
     if (isAlreadyAdded) return
   }
 
-  messageReceivePostEffect({
+  !isTargetChatroomCurrentlyActive && messageReceivePostEffect({
     contractID,
     messageHash: msgData.hash,
     height: msgData.height,
