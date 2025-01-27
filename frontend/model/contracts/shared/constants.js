b1c8ae5eb356d7283532e12d9c4df0e6550aa34c
SebinSong <songxevin@gmail.com>
2025-01-28 07:47:45 +0900

    change the constant to 1

--- frontend/model/contracts/shared/constants.js
+++ frontend/model/contracts/shared/constants.js
@@ -53,7 +53,7 @@ export const STATUS_EXPIRED = 'expired'
 export const STATUS_CANCELLED = 'cancelled'
 
 export const STREAK_ON_TIME_PAYMENTS = 1
-export const STREAK_MISSED_PAYMENTS = 2
+export const STREAK_MISSED_PAYMENTS = 1
 export const STREAK_MISSED_PROPSAL_VOTE = 2
 export const STREAK_NOT_LOGGED_IN_DAYS = 14
 
