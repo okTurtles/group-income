005d2ecf2c20996961650ca0bc405de2a1a0b76d
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-02-12 19:37:09 +0000

    Fix #2616. Wait until Chelonia is ready before logging in

--- frontend/main.js
+++ frontend/main.js
@@ -354,35 +354,11 @@ async function startApp () {
       // happened (an example where things can happen this quickly is in the
       // tests).
       let oldIdentityContractID = null
-      sbp('gi.db/settings/load', SETTING_CURRENT_USER).then(async (identityContractID) => {
-        oldIdentityContractID = identityContractID
-        if (!identityContractID || this.ephemeral.finishedLogin === 'yes') return
-        // Calling login could result in a prompt in case of an error; if the
-        // loading animation is visible, it'll hide the prompt. We remove it,
-        // so that it's possible to interact with the prompt.
-        const removeHandler = sbp('okTurtles.events/once', OPEN_MODAL, () => {
-          this.removeLoadingAnimation()
-        })
-        await sbp('gi.app/identity/login', { identityContractID })
-        removeHandler()
-        await sbp('chelonia/contract/wait', identityContractID)
-      }).catch(async e => {
-        this.removeLoadingAnimation()
-        oldIdentityContractID && sbp('appLogs/clearLogs', oldIdentityContractID).catch(e => {
-          console.error('[main] Error clearing logs for old session', oldIdentityContractID, e)
-        }) // https://github.com/okTurtles/group-income/issues/2194
-        console.error(`[main] caught ${e?.name} while fetching settings or handling a login error: ${e?.message || e}`, e)
-        await sbp('gi.app/identity/logout')
-        await sbp('gi.ui/prompt', {
-          heading: L('Failed to login'),
-          question: L('Error details: {reportError}', LError(e)),
-          primaryButton: L('Close')
-        })
-      }).finally(() => {
+      Promise.all([sbp('gi.db/settings/load', SETTING_CURRENT_USER), (() => {
         // Wait for SW to be ready
         console.debug('[app] Waiting for SW to be ready')
         const sw = ((navigator.serviceWorker: any): ServiceWorkerContainer)
-        Promise.race([
+        return Promise.race([
           sw.ready,
           new Promise((resolve, reject) => setTimeout(() => reject(new Error('SW ready timeout')), 10000))
         ]).then(() => {
@@ -405,6 +381,31 @@ async function startApp () {
           // Fallback behavior
           this.removeLoadingAnimation()
           alert(L('Error while setting up service worker'))
+          throw e
+        })
+      })()]).then(async ([identityContractID]) => {
+        oldIdentityContractID = identityContractID
+        if (!identityContractID || this.ephemeral.finishedLogin === 'yes') return
+        // Calling login could result in a prompt in case of an error; if the
+        // loading animation is visible, it'll hide the prompt. We remove it,
+        // so that it's possible to interact with the prompt.
+        const removeHandler = sbp('okTurtles.events/once', OPEN_MODAL, () => {
+          this.removeLoadingAnimation()
+        })
+        await sbp('gi.app/identity/login', { identityContractID })
+        removeHandler()
+        await sbp('chelonia/contract/wait', identityContractID)
+      }).catch(async e => {
+        this.removeLoadingAnimation()
+        oldIdentityContractID && sbp('appLogs/clearLogs', oldIdentityContractID).catch(e => {
+          console.error('[main] Error clearing logs for old session', oldIdentityContractID, e)
+        }) // https://github.com/okTurtles/group-income/issues/2194
+        console.error(`[main] caught ${e?.name} while fetching settings or handling a login error: ${e?.message || e}`, e)
+        await sbp('gi.app/identity/logout')
+        await sbp('gi.ui/prompt', {
+          heading: L('Failed to login'),
+          question: L('Error details: {reportError}', LError(e)),
+          primaryButton: L('Close')
         })
       })
     },
