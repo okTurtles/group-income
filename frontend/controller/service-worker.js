3684654809136756b9b062732980e1b1a574e602
Greg Slepak <contact@taoeffect.com>
2025-02-20 15:43:34 -0800

    ensure local Cypress tests don't get tripped by by push issues

--- frontend/controller/service-worker.js
+++ frontend/controller/service-worker.js
@@ -184,7 +184,7 @@ sbp('sbp/selectors/register', {
   // have this function there. However, most examples perform this outside of the
   // SW, and private testing showed that it's more reliable doing it here.
   'service-worker/setup-push-subscription': async function (attemptNumber = 1) {
-    if (process.env.CI) {
+    if (window.Cypress || process.env.CI) {
       throw new Error('Push disabled in CI mode')
     }
     let retryAttemptsPromise = null
