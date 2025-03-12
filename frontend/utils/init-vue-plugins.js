ac0853a30091258c89b70c873fc49b343ea09408
Sebin Song <songxevin@gmail.com>
2025-03-12 10:40:58 +0900

    #2476 - Weird iOS bug related to `position:fixed` (#2715)
    * install/setup portal-vue plugin, add the portal-target to ChatMain.vue
    
    * provide/inject for config.isPhone / ensure its reactivity works
    
    * build MessageActionsMobile.vue / make sure it works and fixes the issue #2476
    
    * fix the visual bugs / use built-in transtion component
    
    * add comments to detail why portal-vue is used
    
    * update comment
    
    * delete an unused wather

--- frontend/utils/init-vue-plugins.js
+++ frontend/utils/init-vue-plugins.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import PortalVue from 'portal-vue'
 
+// 1. Setup vue2-touch-events
 if ('ontouchstart' in window || 'msMaxTouchPoints' in navigator) {
   import('vue2-touch-events').then(Vue2TouchEvents => Vue.use(Vue2TouchEvents.default))
 } else Vue.directive('touch', {})
+
+// 2. Setup portal-vue (https://v2.portal-vue.linusb.org/guide/getting-started.html)
+Vue.use(PortalVue)
