ece503244bdd6a33b0d283928ae9e53f3c142468
snowteamer <64228468+snowteamer@users.noreply.github.com>
2025-04-26 21:31:30 +0200

    Add a few config validation tests

---
+++ backend/database-router.test.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+
+import assert from 'node:assert'
+import RouterBackend from './database-router.js'
+import { cloneDeep, omit } from 'turtledash'
+
+const validConfig = {
+  'abc': {
+    name: 'sqlite',
+    options: {
+      dirname: './data'
+    }
+  },
+  '*': {
+    name: 'fs',
+    options: {
+      dirname: './data'
+    }
+  }
+}
+
+describe('DatabaseRouter::validateConfig', () => {
+  const db = new RouterBackend()
+
+  it('should accept a valid config', () => {
+    const errors = db.validateConfig(validConfig)
+    assert.equal(errors.length, 0)
+  })
+
+  it('should reject configs missing a * key', () => {
+    const config = omit(validConfig, '*')
+    const errors = db.validateConfig(config)
+    assert.equal(errors.length, 1)
+  })
+
+  it('should reject config entries missing a name', () => {
+    const config = cloneDeep(validConfig)
+    delete config['*'].name
+    const errors = db.validateConfig(config)
+    assert.equal(errors.length, 1)
+  })
+})
