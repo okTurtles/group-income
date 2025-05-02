109627663b92feeac39f2da23f70cedaa40da305
SebinSong <songxevin@gmail.com>
2025-05-02 18:02:26 +0900

    update design-system menus

---
+++ backend/dashboard/views/pages/design-system/design-system-content/ChelTypography.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+content-outlet(title='Typography')
+  .content-unit
+    h4.unit-name Typography
+
+    .unit-description
+      | Explains about the typography used in the app
+
+    .c-dropdowns-container dropdowns...
+</template>
+
+<script>
+import ContentOutlet from './ContentOutlet.vue'
+
+export default {
+  name: 'ChelForms',
+  components: {
+    ContentOutlet
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+</style>
