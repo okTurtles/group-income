9c451526babc8d5037562421245897117b5c4102
snowteamer <64228468+snowteamer@users.noreply.github.com>
2025-05-08 21:44:22 +0200

    Add tests for lookupBackend

--- backend/database-router.test.js
+++ backend/database-router.test.js
@@ -1,27 +1,33 @@
 import assert from 'node:assert'
+import crypto from 'node:crypto'
 import RouterBackend from './database-router.js'
 import { cloneDeep, omit } from 'turtledash'
 import { describe, it } from 'node:test'
 
+// CID for shelter-contract-text.
+const CID = '\x51\x1e\x01'
+
+const randomKeyWithPrefix = (prefix) => `${prefix}${crypto.randomUUID().replaceAll('-', '')}`
+
 const validConfig = {
-  // CID for shelter-contract-text.
-  '\x51\x1e\x01': {
+  [CID]: {
     name: 'sqlite',
     options: {
-      dirname: './data'
+      dirname: './test/data',
+      filename: 'sqlite.db'
     }
   },
   '*': {
     name: 'fs',
     options: {
-      dirname: './data'
+      dirname: './test/data'
     }
   }
 }
 
-describe('DatabaseRouter::validateConfig', () => {
-  const db = new RouterBackend()
+const db = new RouterBackend({ config: validConfig })
 
+describe('DatabaseRouter::validateConfig', () => {
   it('should accept a valid config', () => {
     const errors = db.validateConfig(validConfig)
     assert.equal(errors.length, 0)
@@ -38,5 +44,39 @@ describe('DatabaseRouter::validateConfig', () => {
     delete config['*'].name
     const errors = db.validateConfig(config)
     assert.equal(errors.length, 1)
+  })
+})
+
+describe('DatabaseRouter::lookupBackend', async () => {
+  await db.init()
+  const { backends, config } = db
+
+  it('should find the right backend for keys starting with configured prefixes', () => {
+    for (const keyPrefix of Object.keys(config)) {
+      if (keyPrefix === '*') continue
+      const key = randomKeyWithPrefix(keyPrefix)
+
+      const actual = db.lookupBackend(key)
+      const expected = backends[keyPrefix]
+      assert.equal(actual, expected)
+    }
+  })
+
+  it('should find the right backend for keys equal to configured prefixes', () => {
+    for (const keyPrefix of Object.keys(config)) {
+      const key = keyPrefix
+
+      const actual = db.lookupBackend(key)
+      const expected = backends[keyPrefix]
+      assert.equal(actual, expected)
+    }
+  })
+
+  it('should return the fallback backend for keys not matching any configured prefix', () => {
+    const key = 'foo'
+
+    const actual = db.lookupBackend(key)
+    const expected = backends['*']
+    assert.equal(actual, expected)
   })
 })
