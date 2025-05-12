6a4339985cf1b60aafcd9abbdd3a9dbb403aceac
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-05-12 03:01:30 +0000

    Contract deletion bugfixes backport

--- backend/server.js
+++ backend/server.js
@@ -417,6 +417,7 @@ sbp('sbp/selectors/register', {
       await sbp('chelonia.db/delete', `_private_hidx=${cid}#0`)
       await sbp('chelonia.db/delete', `_private_keyop_idx_${cid}_0`)
       await sbp('chelonia.db/set', cid, '')
+      sbp('chelonia/private/removeImmediately', cid)
 
       await sbp('chelonia.db/delete', `_private_cheloniaState_${cid}`)
       await removeFromIndexFactory('_private_cheloniaState_index')(cid)
