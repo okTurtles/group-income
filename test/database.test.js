c7542ee57831d50213df2bfa9188620e6f9008e9
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-05-16 05:29:51 +0000

    Simplify SQLite options to just take filepath

--- test/database.test.js
+++ test/database.test.js
@@ -11,8 +11,7 @@ const options = {
     dirname: './test/data/fs'
   },
   sqlite: {
-    dirname: './test/data/sqlite',
-    filename: 'groupincome.db'
+    filepath: './test/data/sqlite/groupincome.db'
   }
 }
 
