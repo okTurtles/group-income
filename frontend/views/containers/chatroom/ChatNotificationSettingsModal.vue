48060839b9ac3163f62370b9d2b631769e211917
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-05-16 09:23:42 +0000

    Change default

--- frontend/views/containers/chatroom/ChatNotificationSettingsModal.vue
+++ frontend/views/containers/chatroom/ChatNotificationSettingsModal.vue
@@ -76,7 +76,7 @@ import { mapGetters } from 'vuex'
 import { validationMixin } from 'vuelidate'
 import { required } from 'vuelidate/lib/validators'
 import ModalTemplate from '@components/modal/ModalTemplate.vue'
-import { MESSAGE_NOTIFY_SETTINGS } from '@model/contracts/shared/constants.js'
+import { CHATROOM_PRIVACY_LEVEL, MESSAGE_NOTIFY_SETTINGS } from '@model/contracts/shared/constants.js'
 import { NEW_CHATROOM_NOTIFICATION_SETTINGS } from '@utils/events.js'
 
 export default ({
@@ -102,7 +102,9 @@ export default ({
     if (this.chatNotificationSettings[this.currentChatRoomId]) {
       settingsFromState = this.chatNotificationSettings[this.currentChatRoomId]
     } else {
-      settingsFromState = this.chatNotificationSettings.default
+      const rootState = sbp('state/vuex/state')
+      const privacyLevelPrivate = rootState[this.currentChatRoomId]?.attributes?.privacyLevel === CHATROOM_PRIVACY_LEVEL.PRIVATE
+      settingsFromState = privacyLevelPrivate ? this.chatNotificationSettings.privateDefault : this.chatNotificationSettings.publicDefault
     }
     this.form.messageNotification = settingsFromState.messageNotification
     this.form.messageSound = settingsFromState.messageSound
