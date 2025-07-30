dd439a0f915079b6773c5ee658791ecce856b5de
Greg Slepak <contact@taoeffect.com>
2025-07-30 13:24:55 -0700

    feedback

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,17 +6,14 @@ set -e
 set -x
 
 VERSION=$(grep '"version"' package.json | sed 's/.*"version": *"\([^"]*\)".*/\1/')
-TAR_FLAGS="--no-acls"
-
-# prevent ._ files from being included in the archive on macOS
-[ "$(uname -s)" = "Darwin" ] && TAR_FLAGS+=" --no-mac-metadata"
 
 grunt clean
 grunt deploy
-tar -czv $TAR_FLAGS -f gi-v${VERSION}.debug.tgz dist
+# prevent ._ files from being included in the archive on macOS
+find dist -not -name '._*' | cpio -H ustar -o | gzip -9 > gi-v${VERSION}.debug.tgz
 
 grunt clean
 NODE_ENV=production grunt deploy
-tar -czv $TAR_FLAGS -f gi-v${VERSION}.tgz dist
+find dist -not -name '._*' | cpio -H ustar -o | gzip -9 > gi-v${VERSION}.tgz
 
 sha256sum gi-*
