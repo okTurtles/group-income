6e5f75edc0971a225612580ad1ae3ad3e33b1ae4
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-09-07 01:58:04 +0000

    Preliminary stubs

---
+++ frontend/views/components/vue-virtual-scroller/DynamicScroller.vue
@@ -0,0 +1,109 @@
+<template>
+  <div
+    ref="scroller"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <span ref="startSentinel"></span>
+    <div
+      v-if="$slots.before"
+      ref="before"
+      class="vue-recycle-scroller__slot"
+    >
+      <slot
+        name="before"
+      />
+    </div>
+
+    <component
+      :is="listTag"
+      ref="wrapper"
+      class="vue-recycle-scroller__item-wrapper"
+    >
+        <component
+            :is="itemTag"
+            class="vue-recycle-scroller__item-view"
+            :key="item.hash"
+            v-for="(item, index) of items"
+        >
+            <slot v-bind="{
+                item,
+                index,
+                active: true
+            }" />
+        </component>
+      <slot
+        name="empty"
+      />
+    </component>
+
+    <div
+      v-if="$slots.after"
+      ref="after"
+      class="vue-recycle-scroller__slot"
+    >
+      <slot
+        name="after"
+      />
+    </div>
+    <span ref="endSentinel"></span>
+  </div>
+</template>
+
+<script>
+// import RecycleScroller from './RecycleScroller.vue'
+
+export default {
+  // components: { RecycleScroller },
+  name: 'DynamicScroller',
+  inheritAttrs: false,
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    keyField: {
+      type: String,
+      default: 'id'
+    },
+    direction: {
+      type: String,
+      default: 'vertical',
+      validator: (value) => ['vertical', 'horizontal'].includes(value)
+    },
+    listTag: {
+      type: String,
+      default: 'div'
+    },
+    itemTag: {
+      type: String,
+      default: 'div'
+    },
+    minItemSize: {
+      type: [Number, String],
+      required: true
+    }
+  },
+  mounted () {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return
+        if (entry.target === this.$refs.startSentinel) this.$emit('scroll-start')
+        if (entry.target === this.$refs.endSentinel) this.$emit('scroll-end')
+      })
+    }, {
+      root: this.$el
+    })
+    this.observer.observe(this.$refs.startSentinel)
+    this.observer.observe(this.$refs.endSentinel)
+  },
+  destroyed () {
+    this.observer?.disconnect()
+  },
+  methods: {
+    scrollToItem (index: number) {
+      this.$el.querySelector('.vue-recycle-scroller__item-view:nth-child(' + index + ')')?.scrollIntoView()
+    }
+  }
+}
+</script>
