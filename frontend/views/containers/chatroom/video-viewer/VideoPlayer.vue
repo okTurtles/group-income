32294bf1ef8a6bdd8464ad7260caf247993bda1f
SebinSong <songxevin@gmail.com>
2025-09-18 09:35:40 +1200

    install plyr / build VideoPlayer.vue

---
+++ frontend/views/containers/chatroom/video-viewer/VideoPlayer.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+.video-player-container
+  video.c-video-el(ref='videoEl' playsinline controls)
+    source(:src='src' :type='mimeType')
+</template>
+
+<script>
+import Plyr from 'plyr'
+
+const defaultOptions = {
+  // reference: https://www.npmjs.com/package/plyr#options
+  debug: false
+}
+
+export default {
+  name: 'VideoPlayer',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    mimeType: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data () {
+    return {
+      config: {
+        player: null
+      }
+    }
+  },
+  methods: {
+    initPlayer () {
+      const opts = { ...defaultOptions, ...this.options }
+      this.config.player = new Plyr(this.$refs.videoEl, opts)
+    }
+  },
+  mounted () {
+    this.initPlayer()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+@import "@assets/style/components/plyr/_plyr_override.scss";
+</style>
