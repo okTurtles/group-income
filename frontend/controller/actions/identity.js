926a14fe8116e3b92dc86b410f1125ca7810bfd8
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-09-23 17:44:52 +0000

    Fix flow

--- frontend/controller/actions/identity.js
+++ frontend/controller/actions/identity.js
@@ -1150,7 +1150,7 @@ export default (sbp('sbp/selectors/register', {
 
         const updatedGroupFKTuple = verifyUpdates()
         if (updatedGroupFKTuple) {
-          groupFKTuple = (updatedGroupFKTuple: [string, string, string][])
+          groupFKTuple = ((updatedGroupFKTuple: any): [string, string, string][])
         }
 
         if (groupFKTuple.length === 0) return
