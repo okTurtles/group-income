4e66ba876e1a3d81a2170e09d490307f8ac61175
snowteamer <64228468+snowteamer@users.noreply.github.com>
2025-10-23 19:44:29 +0200

    Drop getters.withGroupCurrency from IncomeDetails.vue

--- frontend/views/containers/contributions/IncomeDetails.vue
+++ frontend/views/containers/contributions/IncomeDetails.vue
@@ -121,7 +121,6 @@ export default ({
       'groupMincomeAmount',
       'groupMincomeSymbolWithCode',
       'ourIdentityContractId',
-      'withGroupCurrency',
       'ourGroupProfile',
       'usernameFromID'
     ]),
@@ -269,7 +268,7 @@ export default ({
           [L('Your income must be lower than the group mincome')]: function (value) {
             return !this.needsIncome || normalizeCurrency(value) < this.groupSettings.mincomeAmount
           },
-          [L('Pledge amount cannot exceed {max}', { max: this.withGroupCurrency(GROUP_MAX_PLEDGE_AMOUNT) })]: function (value) {
+          [L('Pledge amount cannot exceed {max}', { max: withGroupCurrency(GROUP_MAX_PLEDGE_AMOUNT) })]: function (value) {
             return !this.isPledging || normalizeCurrency(value) < GROUP_MAX_PLEDGE_AMOUNT
           }
         }
