6379bda918b6028eba832290109d68c23fa1b7bb
SebinSong <songxevin@gmail.com>
2025-11-07 13:10:04 +1300

    organsize plyr style override / build boilterplate AudioPlayer.vue

---
+++ frontend/views/containers/chatroom/audio-player/AudioPlayer.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.c-audio-player.plyr_override
+  audio(ref='audioEl' controls playsinline)
+    source(:src='src' :type='mimeType')
+</template>
+
+<script>
+export default {
+  name: 'AudioPlayer',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    mimeType: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+
+.c-audio-player {
+  position: relative;
+  width: 100%;
+  height: auto;
+}
+</style>
