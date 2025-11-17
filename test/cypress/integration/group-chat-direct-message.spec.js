003e3ed14b98e96e499372e8721ad7af02f16174
SebinSong <songxevin@gmail.com>
2025-11-18 11:58:22 +1300

    another attempt

--- test/cypress/integration/group-chat-direct-message.spec.js
+++ test/cypress/integration/group-chat-direct-message.spec.js
@@ -199,7 +199,7 @@ describe('Create/Join direct messages and orders of direct message channels', ()
 
     openDMByMembers([user3])
 
-    // This is a fix for a cypress heisenbug - https://github.com/okTurtles/group-income/issues/2931
+    // This is a fix for a cypress heisenbug: https://github.com/okTurtles/group-income/issues/2931
     cy.wait(4 * 1000) // eslint-disable-line cypress/no-unnecessary-waiting
 
     cy.getByDT('channelName').within(() => {
