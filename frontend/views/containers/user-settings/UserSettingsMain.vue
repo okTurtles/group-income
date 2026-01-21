0bf3e7e3ec280fa6aacdfde5cb8c60173010af8d
SebinSong <songxevin@gmail.com>
2026-01-21 16:04:14 +1300

    add menu items to UserSettingsMain.vue / design adjustments to danger style tile etc

--- frontend/views/containers/user-settings/UserSettingsMain.vue
+++ frontend/views/containers/user-settings/UserSettingsMain.vue
@@ -7,15 +7,67 @@
 
     menu.c-menu
       MenuItem(tabId='my-profile')
+      MenuItem(tabId='logout')
+
+  .c-menu-block
+    legend.tab-legend
+      i.icon-cog.legend-icon
+      i18n.legend-text App settings
+    
+    menu.c-menu
+      MenuItem(tabId='notifications')
+      MenuItem(tabId='appearance')
+
+  .c-menu-block
+    legend.tab-legend
+      i.icon-search.legend-icon
+      i18n.legend-text Advanced
+
+    menu.c-menu
+      MenuItem(tabId='application-logs')
+      MenuItem(tabId='troubleshooting')
+
+  .c-menu-block
+    legend.tab-legend
+      i.icon-exclamation-triangle.legend-icon
+      i18n.legend-text Danger Zone
+
+    menu.c-menu
+      MenuItem(
+        tabId='delete-account'
+        variant='danger'
+        :isExpandable='true'
+      )
+        template(#lower='')
+          .c-menu-item-lower-section-container
+            p.c-delete-account-text
+              i18n.has-text-1 Deleting your account will erase all your data, and remove you from the groups you belong to.
+              | {{ ' ' }}
+              i18n.has-text-danger This action cannot be undone.
+
+            .buttons
+              i18n.button.is-danger.is-outlined(
+                tag='button'
+                type='button'
+                data-test='deleteAccount'
+                @click='handleDeleteAccount'
+              ) Delete account
 </template>
 
 <script>
+import sbp from '@sbp/sbp'
+import { OPEN_MODAL } from '@utils/events.js'
 import UserSettingsTabMenuItem from './UserSettingsTabMenuItem.vue'
 
 export default {
   name: 'UserSettingsMain',
   components: {
     MenuItem: UserSettingsTabMenuItem
+  },
+  methods: {
+    handleDeleteAccount () {
+      sbp('okTurtles.events/emit', OPEN_MODAL, 'AccountRemovalModal')
+    }
   }
 }
 </script>
@@ -34,5 +86,27 @@ export default {
 .c-menu-block {
   width: 100%;
   margin-bottom: 2.5rem;
+}
+
+.c-menu {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.75rem;
+  width: 100%;
+}
+
+.c-menu-item-lower-section-container {
+  position: relative;
+  width: 100%;
+  padding-top: 1.5rem;
+
+  > * {
+    white-space: normal;
+  }
+}
+
+.c-delete-account-text {
+  margin-bottom: 1.5rem;
 }
 </style>
