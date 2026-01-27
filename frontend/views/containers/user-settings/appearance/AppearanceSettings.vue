9a204f0c41167ca382ea6d9bf685d5decf754bc9
SebinSong <songxevin@gmail.com>
2026-01-28 07:34:21 +1300

    add reduced-motion tile to appearance settings page

--- frontend/views/containers/user-settings/appearance/AppearanceSettings.vue
+++ frontend/views/containers/user-settings/appearance/AppearanceSettings.vue
@@ -4,10 +4,22 @@
     menu
       ThemeTile
       TextSizeTile
-      MenuItem(tabId='reduced-motion')
+      MenuItem(
+        tabId='reduced-motion'
+        :isExpandable='true'
+      )
+        template(#info='')
+          input.switch.is-small.c-switch(
+            type='checkbox'
+            name='switch'
+            :checked='$store.state.settings.reducedMotion'
+            @change='handleReducedMotion'
+            @click.stop=''
+          )
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 import UserSettingsTabMenuItem from '../UserSettingsTabMenuItem.vue'
 import ThemeTile from './ThemeTile.vue'
 import TextSizeTile from './TextSizeTile.vue'
@@ -19,6 +31,14 @@ export default {
     MenuItem: UserSettingsTabMenuItem,
     ThemeTile,
     TextSizeTile
+  },
+  methods: {
+    ...mapMutations([
+      'setReducedMotion'
+    ]),
+    handleReducedMotion (e) {
+      this.setReducedMotion(e.target.checked)
+    }
   }
 }
 </script>
@@ -28,5 +48,9 @@ export default {
 
 .c-appearance-settings {
   position: relative;
+}
+
+input.c-switch {
+  vertical-align: middle;
 }
 </style>
