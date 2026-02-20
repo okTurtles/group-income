8195409b66fcd2d5b758e01e376f234a1eb0f90c
Sebin Song <songxevin@gmail.com>
2026-02-20 13:27:57 +1300

    #3023 - Save draft message (#3027)
    * create a localforge instance for chat-msgs
    
    * implement save/delete msg draft logics
    
    * implement loading msg draft in SendArea.vue
    
    * fix the broken load-draft functionality
    
    * add icon for draft in chat
    
    * logic for checking if a chatroom has drafy
    
    * logic to save both text/attachments
    
    * logic to load text/attachments from indexedDB
    
    * logic to show draft-icon / fix a bug where draft persists between diff chatrooms
    
    * draft indicator UI to ChatMembers.vue
    
    * use eventQueue for chat-draft indexedDB actions / fix attachment.dimension related err
    
    * no need to use string prefix for the key
    
    * ensure saved-draft is cleared when deleting channel
    
    * fix cypress failure
    
    * fix test failure
    
    * fix heisenbug...
    
    * resolve feedbacks
    
    * revert back staleObjectURL / resolve feedbacks
    
    * fix cypress heisenbug
    
    * resolve feedback
    
    * fix for when chatroom switch happens
    
    * use appSettings db instace / draft key improvements / etc..
    
    * resolve opus(no tools) reviews
    
    * some fixes for opus reviews
    
    * resolve some GH copilot feedbacks
    
    * fix gi.db/chatDrafts/getAllChatroomIds related bugs
    
    * resolve llm review item
    
    * timeoutIds should be kept per chatroom
    
    * fix an issue
    
    * make comment more clear
    
    * capture chatroomHasDraftSaved at call time
    
    * resolve additional reviews
    
    * update comment

--- frontend/views/containers/chatroom/DeleteChannelModal.vue
+++ frontend/views/containers/chatroom/DeleteChannelModal.vue
@@ -78,6 +78,7 @@ export default ({
           contractID: this.currentGroupId,
           data: { chatRoomID }
         })
+
         this.close()
       } catch (e) {
         console.error('RemoveChannelModal submit() error:', e)
