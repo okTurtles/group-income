479c62929eb00be93ed3490c2f9228f8b124035d
SebinSong <songxevin@gmail.com>
2026-04-02 11:39:41 +1300

    add _toast.scss / add global toast container / layout in ToastCard.vue

--- frontend/views/containers/toast/ToastCard.vue
+++ frontend/views/containers/toast/ToastCard.vue
@@ -1,6 +1,13 @@
 <template lang='pug'>
 .c-toast-card
-  | TODO!
+  .c-toast-content
+    i.icon-check-circle.c-toast-icon
+    .c-toast-message(v-safe-html:a='data.message')
+    button.is-unstyled.c-toast-close(type='button' @click.stop='onClose')
+      i.icon-close
+
+  .c-toast-progress-bar
+    .c-progress-bar-inner
 </template>
 
 <script>
@@ -8,6 +15,12 @@ export default {
   name: 'ToastCard',
   props: {
     data: Object
+  },
+  methods: {
+    onClose () {
+      console.log('TODO!')
+      this.$emit('close')
+    }
   }
 }
 </script>
@@ -17,10 +30,18 @@ export default {
 
 .c-toast-card {
   position: relative;
+  display: block;
+  width: 100%;
+  border-radius: $radius;
+  border: 1px solid $text_1;
+  overflow: hidden;
+}
+
+.c-toast-content {
+  position: relative;
   display: flex;
   align-items: flex-start;
-  padding: 1rem;
-  border-radius: $radius;
   column-gap: 0.5rem;
+  padding: 0.75rem;
 }
 </style>
--- frontend/views/containers/toast/ToastContainer.vue
+++ frontend/views/containers/toast/ToastContainer.vue
@@ -1,17 +1,7 @@
 <template lang='pug'>
-.c-toast-container
-  .c-toast-inner-pocket.is-bottom-left
-    toast-card(v-for='item in ephemeral.items["bottom-left"]' :key='item.id' :data='item')
-  .c-toast-inner-pocket.is-bottom-center
-    toast-card(v-for='item in ephemeral.items["bottom-center"]' :key='item.id' :data='item')
-  .c-toast-inner-pocket.is-bottom-right
-    toast-card(v-for='item in ephemeral.items["bottom-right"]' :key='item.id' :data='item')
-  .c-toast-inner-pocket.is-top-left
-    toast-card(v-for='item in ephemeral.items["top-left"]' :key='item.id' :data='item')
-  .c-toast-inner-pocket.is-top-center
-    toast-card(v-for='item in ephemeral.items["top-center"]' :key='item.id' :data='item')
-  .c-toast-inner-pocket.is-top-right
-    toast-card(v-for='item in ephemeral.items["top-right"]' :key='item.id' :data='item')
+.toast-container.c-toast-container(:class='{ "is-global": isGlobalToast }')
+  .toast-inner-pocket.is-bottom-right
+    toast-card(v-for='item in ephemeral.items' :key='item.id' :data='item')
 </template>
 
 <script>
@@ -27,29 +17,31 @@ export default {
   props: {
     containerId: {
       type: String,
-      default: 'app-global'
+      required: true
     }
   },
   data () {
     return {
       ephemeral: {
-        items: {
-          'bottom-left': [],
-          'bottom-center': [],
-          'bottom-right': [],
-          'top-left': [],
-          'top-center': [],
-          'top-right': []
-        }
+        items: [
+          {
+            id: 'random-id-1',
+            message: 'This is a test message'
+          }
+        ]
       }
+    }
+  },
+  computed: {
+    isGlobalToast () {
+      return this.containerId === 'app-global'
     }
   },
   methods: {
     onShowToast (targetContainerId = '', data = null) {
       if (targetContainerId !== this.containerId || !data) { return }
 
-      const toastPosition = data.position || 'bottom-right'
-      this.ephemeral.items[toastPosition].push(data)
+      this.ephemeral.items.push(data)
     }
   },
   mounted () {
@@ -65,22 +57,6 @@ export default {
 @import "@assets/style/_variables.scss";
 
 .c-toast-container {
-  position: absolute;
   display: block;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 10;
-  background-color: $success_0_1;
-  opacity: 0.5;
-}
-
-.c-toast-inner-pocket {
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 0.5rem;
 }
 </style>
