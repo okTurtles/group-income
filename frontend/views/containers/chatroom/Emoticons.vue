031f9b4b1dacc0fd0acc771771f1fc07c21e1f6e
SebinSong <songxevin@gmail.com>
2026-04-13 10:52:31 +1200

    fix the bug

--- frontend/views/containers/chatroom/Emoticons.vue
+++ frontend/views/containers/chatroom/Emoticons.vue
@@ -41,7 +41,8 @@ export default ({
   },
   methods: {
     handleKeyUp (e) {
-      if (this.content && e.key === 'Escape') {
+      const isComponentFocused = e.target.closest('.c-picker-wrapper') !== null
+      if (this.isActive && isComponentFocused && e.key === 'Escape') {
         e.preventDefault()
         this.closeEmoticonDlg()
       }
