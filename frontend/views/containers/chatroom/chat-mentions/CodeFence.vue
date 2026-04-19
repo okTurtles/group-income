03feaab976c630d44bad3516b9e1a2c049a38b89
SebinSong <songxevin@gmail.com>
2026-04-20 08:00:25 +1200

    build a dedicated component for code block / render it

---
+++ frontend/views/containers/chatroom/chat-mentions/CodeFence.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  pre.c-code-fence
+    code {{ content }}
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.c-code-fence {
+  position: relative;
+  display: block;
+}
+</style>
