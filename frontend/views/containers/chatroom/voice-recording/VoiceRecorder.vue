7746c16686b0ce62e8590f9dbc3164ca67728528
SebinSong <songxevin@gmail.com>
2026-05-25 10:18:44 +1200

    add boilerplate AudioRecorder.vue

---
+++ frontend/views/containers/chatroom/voice-recording/VoiceRecorder.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.c-voice-recorder
+  button.is-unstyled.c-close-btn(@click='close')
+    i.icon-times
+  .c-sound-patterns
+    .c-pattern-box(v-for='i in ephemeral.patternCount' :key='i')
+  button.is-unstyled.c-record-btn(@click='record')
+    i.icon-check
+</template>
+
+<script>
+export default {
+  name: 'VoiceRecorder',
+  data () {
+    return {
+      ephemeral: {
+        isOpen: false,
+        patternCount: 20
+      }
+    }
+  },
+  methods: {
+    close () {
+      console.log('TODO: close voice recorder UI')
+    },
+    record () {
+      console.log('TODO: record voice')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/style/_variables.scss";
+
+.c-voice-recorder {
+  position: absolute;
+}
+</style>
