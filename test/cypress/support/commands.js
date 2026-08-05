f715018851615a0f6357011d5e62b169d0feee86
SebinSong <songxevin@gmail.com>
2026-08-05 16:35:04 +0900

    comment for cy.giSendMessage() opts

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -859,7 +859,14 @@ Cypress.Commands.add('giWaitUntilMessagesLoaded', (isGroupChannel = true) => {
   }
 })
 
-Cypress.Commands.add('giSendMessage', (sender, message, { instantInput = false, checkMessage = true } = {}) => {
+Cypress.Commands.add('giSendMessage', (sender, message, {
+  // instantInput: Instead of mimicking the user typing message character by character (which can be slow for long or repeatitive message),
+  //               Just dump the text quickly into the textarea without waiting for typing animation to complete.
+  // checkMessage: In some cases, the sent message gets transformed (e.g. markdown) and the default check in this function
+  //               fails. The default check here can be skipped using this parameter.
+  instantInput = false,
+  checkMessage = true
+} = {}) => {
   // The following is to ensure the chatroom has finished loading (no spinner)
   cy.giWaitUntilMessagesLoaded(false)
   cy.getByDT('messageInputWrapper').within(() => {
